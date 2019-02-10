57c60a7c9c3c3471a21827be1749c9b91ac8db90
Moritz <moritz.kroeger@tu-dortmund.de>
2019-02-10 20:56:17 +0100

    Merge remote-tracking branch 'upstream/master' into dev/tests

--- tensorflow_addons/layers/__init__.py
+++ tensorflow_addons/layers/__init__.py
@@ -19,5 +19,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# Weight Normalization Wrapper
-from tensorflow_addons.layers.python.wrappers import WeightNorm
+from tensorflow_addons.layers.python.maxout import Maxout
+from tensorflow_addons.layers.python.wrappers import WeightNormalization
