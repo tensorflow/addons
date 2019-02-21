89eccb8cf3bd914faae548a3b2baea85e0c88a94
Sean Morgan <seanmorgan@outlook.com>
2019-02-21 09:10:33 -0500

    PEP8

--- tensorflow_addons/layers/__init__.py
+++ tensorflow_addons/layers/__init__.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""
-Ops for building neural network layers
-"""
+"""Ops for building neural network layers"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
