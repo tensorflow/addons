06694664b691b09e78106542d9b2ef1400e76950
smokrow <moritz.kroeger@tu-dortmund.de>
2019-03-05 23:11:41 +0100

    beautified

--- tensorflow_addons/layers/python/normalizations.py
+++ tensorflow_addons/layers/python/normalizations.py
@@ -30,8 +30,8 @@ class GroupNormalization(Layer):
 
     Group Normalization divides the channels into groups and computes
     within each group the mean and variance for normalization.
-    Empirically, its accuracy is more stable than batch norm in a wide 
-    range of small batch sizes, if learning rate is adjusted linearly 
+    Empirically, its accuracy is more stable than batch norm in a wide
+    range of small batch sizes, if learning rate is adjusted linearly
     with batch sizes.
 
     Relation to Layer Normalization:
@@ -119,20 +119,20 @@ class GroupNormalization(Layer):
         self.built = True
         super(GroupNormalization, self).build(input_shape)
 
-
     def call(self, inputs):
 
         input_shape = K.int_shape(inputs)
         tensor_input_shape = K.shape(inputs)
 
-        reshaped_inputs, group_shape=self._reshape_into_groups(inputs,input_shape,tensor_input_shape)
+        reshaped_inputs, group_shape = self._reshape_into_groups(
+            inputs, input_shape, tensor_input_shape)
 
-        normalized_inputs = self._apply_normalization(reshaped_inputs, input_shape)
+        normalized_inputs = self._apply_normalization(
+            reshaped_inputs, input_shape)
 
         outputs = K.reshape(normalized_inputs, tensor_input_shape)
 
         return outputs
-
 
     def get_config(self):
         config = {
@@ -151,12 +151,10 @@ class GroupNormalization(Layer):
         base_config = super(GroupNormalization, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
-
     def compute_output_shape(self, input_shape):
         return input_shape
 
-
-    def _reshape_into_groups(self,inputs,input_shape,tensor_input_shape):
+    def _reshape_into_groups(self, inputs, input_shape, tensor_input_shape):
 
         group_shape = [tensor_input_shape[i] for i in range(len(input_shape))]
         group_shape[self.axis] = input_shape[self.axis] // self.groups
@@ -165,8 +163,7 @@ class GroupNormalization(Layer):
         reshaped_inputs = K.reshape(inputs, group_shape)
         return reshaped_inputs, group_shape
 
-
-    def _apply_normalization(self, reshaped_inputs , input_shape):
+    def _apply_normalization(self, reshaped_inputs, input_shape):
 
         group_shape = K.int_shape(reshaped_inputs)
         group_reduction_axes = list(range(len(group_shape)))
@@ -174,27 +171,25 @@ class GroupNormalization(Layer):
         mean, variance = nn.moments(reshaped_inputs, group_reduction_axes[2:],
                                     keep_dims=True)
 
-        gamma,beta= self._get_reshaped_weights(input_shape)
-        normalized_inputs= nn.batch_normalization(reshaped_inputs,
-                                                mean = mean,
-                                                variance = variance,
-                                                scale = gamma,
-                                                offset = beta,
-                                                variance_epsilon = self.epsilon)
+        gamma, beta = self._get_reshaped_weights(input_shape)
+        normalized_inputs = nn.batch_normalization(reshaped_inputs,
+                                                   mean=mean,
+                                                   variance=variance,
+                                                   scale=gamma,
+                                                   offset=beta,
+                                                   variance_epsilon=self.epsilon)
         return normalized_inputs
 
-
     def _get_reshaped_weights(self, input_shape):
-        broadcast_shape=self._create_broadcast_shape(input_shape)
-        gamma=None
-        beta=None
+        broadcast_shape = self._create_broadcast_shape(input_shape)
+        gamma = None
+        beta = None
         if self.scale:
             gamma = K.reshape(self.gamma, broadcast_shape)
 
         if self.center:
             beta = K.reshape(self.beta, broadcast_shape)
         return gamma, beta
-
 
     def _check_if_input_shape_is_None(self, input_shape):
         dim = input_shape[self.axis]
@@ -204,17 +199,15 @@ class GroupNormalization(Layer):
                              'but the layer received an input with shape ' +
                              str(input_shape) + '.')
 
-
     def _set_number_of_groups_for_instance_norm(self, input_shape):
-        dim=input_shape[self.axis]
+        dim = input_shape[self.axis]
 
-        if self.groups==-1:
-            self.groups=dim
+        if self.groups == -1:
+            self.groups = dim
 
+    def _check_size_of_dimensions(self, input_shape):
 
-    def _check_size_of_dimensions(self,input_shape):
-
-        dim=input_shape[self.axis]
+        dim = input_shape[self.axis]
         if dim < self.groups:
             raise ValueError('Number of groups (' + str(self.groups) + ') cannot be '
                              'more than the number of channels (' +
@@ -227,19 +220,20 @@ class GroupNormalization(Layer):
 
     def _check_axis(self):
 
-        if self.axis==0:
-            raise ValueError("You are trying to normalize your batch axis. Do you want to use tf.layer.batch_normalization instead")
-    def _create_input_spec(self,input_shape):
+        if self.axis == 0:
+            raise ValueError(
+                "You are trying to normalize your batch axis. Do you want to use tf.layer.batch_normalization instead")
 
-        dim=input_shape[self.axis]
+    def _create_input_spec(self, input_shape):
+
+        dim = input_shape[self.axis]
         self.input_spec = InputSpec(ndim=len(input_shape),
                                     axes={self.axis: dim})
 
+    def _add_gamma_weight(self, input_shape):
 
-    def _add_gamma_weight(self,input_shape):
-
-        dim=input_shape[self.axis]
-        shape=(dim,)
+        dim = input_shape[self.axis]
+        shape = (dim,)
 
         if self.scale:
             self.gamma = self.add_weight(shape=shape,
@@ -250,10 +244,10 @@ class GroupNormalization(Layer):
         else:
             self.gamma = None
 
-    def _add_beta_weight(self,input_shape):
+    def _add_beta_weight(self, input_shape):
 
-        dim=input_shape[self.axis]
-        shape=(dim,)
+        dim = input_shape[self.axis]
+        shape = (dim,)
 
         if self.center:
             self.beta = self.add_weight(shape=shape,
@@ -264,8 +258,7 @@ class GroupNormalization(Layer):
         else:
             self.beta = None
 
-
-    def _create_broadcast_shape(self,input_shape):
+    def _create_broadcast_shape(self, input_shape):
         broadcast_shape = [1] * len(input_shape)
         broadcast_shape[self.axis] = input_shape[self.axis] // self.groups
         broadcast_shape.insert(1, self.groups)
@@ -277,8 +270,8 @@ class LayerNormalization(GroupNormalization):
 
     Layer Normalization is an specific case of ```GroupNormalization```since it
     normalizes all features of a layer. The Groupsize is 1.
-    Empirically, its accuracy is more stable than batch norm in a wide 
-    range of small batch sizes, if learning rate is adjusted linearly 
+    Empirically, its accuracy is more stable than batch norm in a wide
+    range of small batch sizes, if learning rate is adjusted linearly
     with batch sizes.
 
     Arguments
@@ -313,19 +306,22 @@ class LayerNormalization(GroupNormalization):
     References
         - [Layer Normalization](https://arxiv.org/abs/1607.06450)
     """
-    def __init__(self,**kwargs):
+
+    def __init__(self, **kwargs):
         if "groups" in kwargs:
-            tf.logging.warning("The given value for groups will be overwritten.")
-        kwargs["groups"]=1
-        super(LayerNormalization,self).__init__(**kwargs)
+            tf.logging.warning(
+                "The given value for groups will be overwritten.")
+        kwargs["groups"] = 1
+        super(LayerNormalization, self).__init__(**kwargs)
+
 
 class InstanceNormalization(GroupNormalization):
     """Instance normalization layer.
 
     Instance Normalization is an specific case of ```GroupNormalization```since it
     normalizes all features of one channel. The Groupsize is equal to the channel size.
-    Empirically, its accuracy is more stable than batch norm in a wide 
-    range of small batch sizes, if learning rate is adjusted linearly 
+    Empirically, its accuracy is more stable than batch norm in a wide
+    range of small batch sizes, if learning rate is adjusted linearly
     with batch sizes.
 
     Arguments
@@ -360,10 +356,12 @@ class InstanceNormalization(GroupNormalization):
     References
         - [Layer Normalization](https://arxiv.org/abs/1607.06450)
     """
-    def __init__(self,**kwargs):
+
+    def __init__(self, **kwargs):
 
         if "groups" in kwargs:
-            tf.logging.warning("The given value for groups will be overwritten.")
+            tf.logging.warning(
+                "The given value for groups will be overwritten.")
 
-        kwargs["groups"]=-1
-        super(InstanceNormalization,self).__init__(**kwargs)
+        kwargs["groups"] = -1
+        super(InstanceNormalization, self).__init__(**kwargs)
