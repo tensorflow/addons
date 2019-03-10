9a4c30d74d202881a3041c57e5ea0553aaf8e435
Aakash Kumar Nain <aakashnain@outlook.com>
2019-03-10 20:17:21 +0530

    removed unused imports

--- tensorflow_addons/losses/python/focal_loss.py
+++ tensorflow_addons/losses/python/focal_loss.py
@@ -18,9 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import abc
-import six
-import numpy as np
 
 import tensorflow as tf
 import tensorflow.keras.backend as K
