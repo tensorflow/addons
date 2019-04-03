e2379279f7b2e2c31e4ed424360beb376caa077d
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2019-04-03 23:50:42 +0530

    Update setup.py
    pep8 Linting

--- setup.py
+++ setup.py
@@ -14,12 +14,13 @@
 # ==============================================================================
 """TensorFlow Addons 
 
-TensorFlow Addons is a repository of contributions that conform to well-established
-API patterns,but implement new functionality not available in core TensorFlow.
-TensorFlow natively supports a large number of operators, layers, metrics, losses,and
-optimizers. However, in a fast movingfield like ML, there are many interesting new 
-developments that cannot be integrated into core TensorFlow (because their broad
-applicability is not yet clear, or it is mostly used by a smallersubset of the community).
+TensorFlow Addons is a repository of contributions that conform to
+well-established API patterns,but implement new functionality not available in
+core TensorFlow.TensorFlow natively supports a large number of operators,
+layers, metrics, losses, and optimizers. However, in a fast movingfield like
+ML, there are many interesting new developments that cannot be integrated into
+core TensorFlow (because their broad applicability is not yet clear, or it is
+mostly used by a smallersubset of the community).
 """
 
 from __future__ import absolute_import
