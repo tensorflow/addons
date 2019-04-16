be1825b99f84cfa17587df3299ce4ef81efc323d
Smokrow <moritz.kroeger@tu-dortmund.de>
2019-04-16 19:22:55 +0200

    removed layernorm

--- tensorflow_addons/layers/normalizations.py
+++ tensorflow_addons/layers/normalizations.py
@@ -274,49 +274,6 @@ class GroupNormalization(tf.keras.layers.Layer):
 
 
 @keras_utils.register_keras_custom_object
-class LayerNormalization(GroupNormalization):
-    """Layer normalization layer.
-
-    Layer Normalization is an specific case of ```GroupNormalization```since it
-    normalizes all features of a layer. The Groupsize is 1.
-    Empirically, its accuracy is more stable than batch norm in a wide
-    range of small batch sizes, if learning rate is adjusted linearly
-    with batch sizes.
-
-    Arguments
-        axis: Integer, the axis that should be normalized.
-        epsilon: Small float added to variance to avoid dividing by zero.
-        center: If True, add offset of `beta` to normalized tensor.
-            If False, `beta` is ignored.
-        scale: If True, multiply by `gamma`.
-            If False, `gamma` is not used.
-        beta_initializer: Initializer for the beta weight.
-        gamma_initializer: Initializer for the gamma weight.
-        beta_regularizer: Optional regularizer for the beta weight.
-        gamma_regularizer: Optional regularizer for the gamma weight.
-        beta_constraint: Optional constraint for the beta weight.
-        gamma_constraint: Optional constraint for the gamma weight.
-
-    Input shape
-        Arbitrary. Use the keyword argument `input_shape`
-        (tuple of integers, does not include the samples axis)
-        when using this layer as the first layer in a model.
-
-    Output shape
-        Same shape as input.
-
-    References
-        - [Layer Normalization](https://arxiv.org/abs/1607.06450)
-    """
-
-    def __init__(self, **kwargs):
-        if "groups" in kwargs:
-            logging.warning("The given value for groups will be overwritten.")
-        kwargs["groups"] = 1
-        super(LayerNormalization, self).__init__(**kwargs)
-
-
-@keras_utils.register_keras_custom_object
 class InstanceNormalization(GroupNormalization):
     """Instance normalization layer.
 
