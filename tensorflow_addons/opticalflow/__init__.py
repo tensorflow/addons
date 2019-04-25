1096a45bcf8278737f515035773ceee50d91fb74
PatWie <mail@patwie.com>
2019-04-25 15:33:23 +0200

    move files around

--- tensorflow_addons/opticalflow/__init__.py
+++ tensorflow_addons/opticalflow/__init__.py
@@ -17,3 +17,5 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+from tensorflow_addons.opticalflow.correlation_cost import correlation_cost
