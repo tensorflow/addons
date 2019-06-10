2b679c672ed1583130c2b270cba1b360af6845f0
Aakash Kumar Nain <aakashnain@outlook.com>
2019-06-10 21:35:56 +0530

    refactor tests

--- tensorflow_addons/metrics/cohens_kappa_test.py
+++ tensorflow_addons/metrics/cohens_kappa_test.py
@@ -29,66 +29,88 @@ class CohensKappaTest(tf.test.TestCase):
   def test_config(self):
     kp_obj = CohensKappa(name='cohens_kappa')
     self.assertEqual(kp_obj.name, 'cohens_kappa')
-
+  
   def test_kappa_random_score(self):
-    kp_obj = CohensKappa()
+    kp_obj1 = CohensKappa(num_classes=5)
+    kp_obj2 = CohensKappa(num_classes=5, weightage='linear')
+    kp_obj3 = CohensKappa(num_classes=5, weightage='quadratic')
+    
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj1.variables))
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj2.variables))
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj3.variables))
+    
     # random score
     actuals = np.array([4, 4, 3, 4, 2, 4, 1, 1], dtype=np.int32)
     preds = np.array([4, 4, 3, 4, 4, 2, 1, 1], dtype=np.int32)
     actuals = tf.convert_to_tensor(actuals, dtype=tf.int32)
     preds = tf.convert_to_tensor(preds, dtype=tf.int32)
 
-    score1 = kp_obj.update_state(actuals, preds, sample_weight=None)
-    score2 = kp_obj.update_state(actuals, preds, sample_weight='linear')
-    score3 = kp_obj.update_state(actuals, preds, sample_weight='quadratic')
+    update_op1 = kp_obj1.update_state(actuals, preds)
+    update_op2 = kp_obj2.update_state(actuals, preds)
+    update_op3 = kp_obj3.update_state(actuals, preds)
+    
+    self.evaluate(update_op1)
+    self.evaluate(update_op2)
+    self.evaluate(update_op3)
 
-    score1 = self.evaluate(score1)
-    score2 = self.evaluate(score2)
-    score3 = self.evaluate(score3)
-
-    self.assertAlmostEqual(score1, 0.61904, 4)
-    self.assertAlmostEqual(score2, 0.62790, 4)
-    self.assertAlmostEqual(score3, 0.68932, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj1.result()), 0.61904, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj2.result()), 0.62790, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj3.result()), 0.68932, 4)
 
   def test_kappa_perfect_score(self):
-    kp_obj = CohensKappa()
+    kp_obj1 = CohensKappa(num_classes=5)
+    kp_obj2 = CohensKappa(num_classes=5, weightage='linear')
+    kp_obj3 = CohensKappa(num_classes=5, weightage='quadratic')
+    
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj1.variables))
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj2.variables))
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj3.variables))
+    
     # perfect score
     actuals = np.array([4, 4, 3, 3, 2, 2, 1, 1], dtype=np.int32)
     preds = np.array([4, 4, 3, 3, 2, 2, 1, 1], dtype=np.int32)
     actuals = tf.convert_to_tensor(actuals, dtype=tf.int32)
     preds = tf.convert_to_tensor(preds, dtype=tf.int32)
 
-    score1 = kp_obj.update_state(actuals, preds, sample_weight=None)
-    score2 = kp_obj.update_state(actuals, preds, sample_weight='linear')
-    score3 = kp_obj.update_state(actuals, preds, sample_weight='quadratic')
-
-    score1 = self.evaluate(score1)
-    score2 = self.evaluate(score2)
-    score3 = self.evaluate(score3)
-
-    self.assertAlmostEqual(score1, 1.0, 4)
-    self.assertAlmostEqual(score2, 1.0, 4)
-    self.assertAlmostEqual(score3, 1.0, 4)
+    update_op1 = kp_obj1.update_state(actuals, preds)
+    update_op2 = kp_obj2.update_state(actuals, preds)
+    update_op3 = kp_obj3.update_state(actuals, preds)
+    
+    self.evaluate(update_op1)
+    self.evaluate(update_op2)
+    self.evaluate(update_op3)
+    
+    
+    self.assertAlmostEqual(self.evaluate(kp_obj1.result()), 1.0, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj2.result()), 1.0, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj3.result()), 1.0, 4)
 
   def test_kappa_worse_than_random(self):
-    kp_obj = CohensKappa()
-    #worse than random 
+    kp_obj1 = CohensKappa(num_classes=5)
+    kp_obj2 = CohensKappa(num_classes=5, weightage='linear')
+    kp_obj3 = CohensKappa(num_classes=5, weightage='quadratic')
+    
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj1.variables))
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj2.variables))
+    self.evaluate(tf.compat.v1.variables_initializer(kp_obj3.variables))
+    
+    # worse than random 
     actuals = np.array([4, 4, 3, 3, 2, 2, 1, 1], dtype=np.int32)
     preds = np.array([1, 2, 4, 1, 3, 3, 4, 4], dtype=np.int32)
     actuals = tf.convert_to_tensor(actuals, dtype=tf.int32)
     preds = tf.convert_to_tensor(preds, dtype=tf.int32)
 
-    score1 = kp_obj.update_state(actuals, preds, sample_weight=None)
-    score2 = kp_obj.update_state(actuals, preds, sample_weight='linear')
-    score3 = kp_obj.update_state(actuals, preds, sample_weight='quadratic')
+    update_op1 = kp_obj1.update_state(actuals, preds)
+    update_op2 = kp_obj2.update_state(actuals, preds)
+    update_op3 = kp_obj3.update_state(actuals, preds)
+    
+    self.evaluate(update_op1)
+    self.evaluate(update_op2)
+    self.evaluate(update_op3)
 
-    score1 = self.evaluate(score1)
-    score2 = self.evaluate(score2)
-    score3 = self.evaluate(score3)
-
-    self.assertAlmostEqual(score1, -0.33333, 4)
-    self.assertAlmostEqual(score2, -0.52380, 4)
-    self.assertAlmostEqual(score3, -0.72727, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj1.result()), -0.33333, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj2.result()), -0.52380, 4)
+    self.assertAlmostEqual(self.evaluate(kp_obj3.result()), -0.72727, 4)
 
 if __name__ == '__main__':
-  tf.test.main()
+  tf.test.main() 
