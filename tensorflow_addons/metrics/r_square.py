b3eb7692cf891ffb5b9b81c5158ead05ecfb11ec
saishruthi <saishruthi.tn@ibm.com>
2019-06-12 11:08:19 -0700

    rsquare

---
+++ tensorflow_addons/metrics/r_square.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow.keras.metrics import Metric
+
+
+class RSquare(Metric):
+    """
+    Compute R^2 score
+
+    This is also called as coefficient of determination.
+    It tells how close are data to the fitted regression line.
+
+    - Highest score can be 1.0 and it indicates that the predictors
+      perfectly accounts for variation in the target.
+    - Score 0.0 indicates that the predictors do not
+      account for variation in the target.
+    - It can also be negative if the model is worse.
+    """
+    def __init__(self, name='r-square'):
+        super(RSquare, self).__init__(name=name, dtype=tf.float32)
+        self.squared_sum = self.add_weight("squared_sum", initializer="zeros")
+        self.sum = self.add_weight("sum", initializer="zeros")
+        self.res = self.add_weight("residual", initializer="zeros")
+        self.count = self.add_weight("count", initializer="zeros")
+
+    def update_state(self, y_true, y_pred):
+        y_true = tf.convert_to_tensor(y_true, tf.float32)
+        y_pred = tf.convert_to_tensor(y_pred, tf.float32)
+        self.squared_sum.assign_add(tf.reduce_sum(y_true ** 2)),
+        self.sum.assign_add(tf.reduce_sum(y_true)),
+        self.res.assign_add(tf.reduce_sum(
+            tf.square(tf.subtract(y_true, y_pred)))),
+        self.count.assign_add(tf.cast(tf.shape(y_true)[0], tf.float32))
+
+    def result(self):
+        mean = self.sum / self.count
+        total = self.squared_sum - 2 * self.sum * mean + self.count * mean ** 2
+        return 1 - (self.res / total)
+
+    def reset_states(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.squared_sum.assign(0.0)
+        self.sum.assign(0.0)
+        self.res.assign(0.0)
+        self.count.assign(0.0)
