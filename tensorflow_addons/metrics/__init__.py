73a6209360c155a5bd45fef348b2c22cdad0508f
saishruthi <saishruthi.tn@ibm.com>
2019-06-13 16:53:23 -0700

    Addressing issues

--- tensorflow_addons/metrics/__init__.py
+++ tensorflow_addons/metrics/__init__.py
@@ -19,3 +19,4 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow_addons.metrics.cohens_kappa import CohenKappa
+from tensorflow_addons.metrics.r_square import RSquare
