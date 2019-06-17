2e1b867cbf3b539071f79041b08b2e10e57eb512
saishruthi <saishruthi.tn@ibm.com>
2019-06-17 10:21:09 -0700

    minor updates

--- tensorflow_addons/metrics/f1_test.py
+++ tensorflow_addons/metrics/f1_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests F1 micro metric."""
+"""Tests F1 metrics."""
 
 from __future__ import absolute_import
 from __future__ import division
