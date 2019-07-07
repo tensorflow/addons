b9bbd64ee39c7e49484ac8f9ca5d66a06890613c
Sean Morgan <seanmorgan91@gmail.com>
2019-07-07 19:33:58 -0400

    Remove numpy implementation

--- tensorflow_addons/losses/metric_test.py
+++ tensorflow_addons/losses/metric_test.py
@@ -23,39 +23,12 @@ from tensorflow_addons.losses.metric_learning import pairwise_distance
 from tensorflow_addons.utils import test_utils
 
 
-def pairwise_distance_np(feature, squared=False):
-    """Computes the pairwise distance matrix in numpy. Originally found in
-    https://github.com/omoindrot/tensorflow-triplet-loss.
-
-    Args:
-        feature: 2-D numpy array of size [number of data, feature dimension]
-        squared: Boolean. If true, output is the pairwise squared euclidean
-                 distance matrix; else, output is the pairwise euclidean
-                 distance matrix.
-    Returns:
-        pairwise_distances: 2-D numpy array of size
-                            [number of data, number of data].
-    """
-    triu = np.triu_indices(feature.shape[0], 1)
-    upper_tri_pdists = np.linalg.norm(
-        feature[triu[1]] - feature[triu[0]], axis=1)
-    if squared:
-        upper_tri_pdists **= 2.
-    num_data = feature.shape[0]
-    pairwise_distances = np.zeros((num_data, num_data))
-    pairwise_distances[np.triu_indices(num_data, 1)] = upper_tri_pdists
-    # Make symmetrical.
-    pairwise_distances = pairwise_distances + pairwise_distances.T - np.diag(
-        pairwise_distances.diagonal())
-    return pairwise_distances
-
-
 @test_utils.run_all_in_graph_and_eager_modes
 class PairWiseDistance(tf.test.TestCase):
     def test_zero_distance(self):
         """Test that equal embeddings have a pairwise distance of 0."""
-        rand_embedding = tf.random.uniform([1, 3], dtype=tf.float32)
-        equal_embeddings = tf.concat([rand_embedding, rand_embedding], axis=0)
+        equal_embeddings = tf.constant([[1.0, 0.5],
+                                     [1.0, 0.5]])
 
         distances = pairwise_distance(equal_embeddings, squared=False)
         self.assertAllClose(tf.math.reduce_sum(distances), 0)
@@ -71,27 +44,25 @@ class PairWiseDistance(tf.test.TestCase):
 
     def test_correct_distance(self):
         """Compare against numpy caluclation."""
-        tf_embeddings = tf.constant([[0.7373636, 0.6229943, 0.49961114],
-                                     [0.21469843, 0.26616073, 0.25712824]])
+        tf_embeddings = tf.constant([[0.5, 0.5],
+                                     [1.0, 1.0]])
 
-        np_embeddings = np.array([[0.7373636, 0.6229943, 0.49961114],
-                                  [0.21469843, 0.26616073, 0.25712824]])
+        expected_distance = np.array([[0, np.sqrt(2)/2],
+                                     [np.sqrt(2)/2, 0]])
 
         distances = pairwise_distance(tf_embeddings, squared=False)
-        self.assertAllClose(
-            pairwise_distance_np(np_embeddings, squared=False), distances)
+        self.assertAllClose(expected_distance, distances)
 
     def test_correct_distance_squared(self):
         """Compare against numpy caluclation for squared distances."""
-        tf_embeddings = tf.constant([[0.7373636, 0.6229943, 0.49961114],
-                                     [0.21469843, 0.26616073, 0.25712824]])
+        tf_embeddings = tf.constant([[0.5, 0.5],
+                                     [1.0, 1.0]])
 
-        np_embeddings = np.array([[0.7373636, 0.6229943, 0.49961114],
-                                  [0.21469843, 0.26616073, 0.25712824]])
+        expected_distance = np.array([[0, 0.5],
+                                      [0.5, 0]])
 
         distances = pairwise_distance(tf_embeddings, squared=True)
-        self.assertAllClose(
-            pairwise_distance_np(np_embeddings, squared=True), distances)
+        self.assertAllClose(expected_distance, distances)
 
 
 if __name__ == "__main__":
