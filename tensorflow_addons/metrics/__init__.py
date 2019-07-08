47ccd0c64a48501bd15a5041d352b7b67b2a3b54
saishruthi <saishruthi.tn@ibm.com>
2019-07-08 14:42:05 -0700

    Addressing comments

--- tensorflow_addons/metrics/__init__.py
+++ tensorflow_addons/metrics/__init__.py
@@ -19,5 +19,5 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow_addons.metrics.cohens_kappa import CohenKappa
-from tensorflow_addons.metrics.f1_scores import F1Score, FBetaScore
+from tensorflow_addons.metrics.f_scores import F1Score, FBetaScore
 from tensorflow_addons.metrics.r_square import RSquare
