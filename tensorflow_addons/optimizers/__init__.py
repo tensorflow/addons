60b155f83d814c297f74bb0beba1b2f739e8a31c
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2019-07-14 10:04:49 +0530

    CLN: moving_average_test (#351)
    * Wrap the class in `run_all_in_graph_and_eager_modes` instead of
    wrapping each method in `run_in_graph_and_eager_modes`

--- tensorflow_addons/optimizers/__init__.py
+++ tensorflow_addons/optimizers/__init__.py
@@ -19,8 +19,8 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow_addons.optimizers.lazy_adam import LazyAdam
+from tensorflow_addons.optimizers.moving_average import MovingAverage
 from tensorflow_addons.optimizers.weight_decay_optimizers import AdamW
 from tensorflow_addons.optimizers.weight_decay_optimizers import SGDW
 from tensorflow_addons.optimizers.weight_decay_optimizers import (
     extend_with_decoupled_weight_decay)
-from tensorflow_addons.optimizers.moving_average import MovingAverage
