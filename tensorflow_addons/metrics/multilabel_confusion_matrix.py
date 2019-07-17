03f1230898221e66d97901c92cf9bc5cab8da76b
saishruthi <saishruthi.tn@ibm.com>
2019-07-16 21:08:38 -0700

    minor update

--- tensorflow_addons/metrics/multilabel_confusion_matrix.py
+++ tensorflow_addons/metrics/multilabel_confusion_matrix.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implements Multilabel confusion matrix scores."""
+"""Implements Multi-label confusion matrix scores."""
 
 from __future__ import absolute_import
 from __future__ import division
