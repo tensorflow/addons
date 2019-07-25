07b633213fc77bd68e0553f378ed89ca096c0ead
Sean Morgan <seanmorgan91@gmail.com>
2019-07-25 14:57:56 -0400

    Make tests eager executed

--- tensorflow_addons/layers/correlation_cost_test.py
+++ tensorflow_addons/layers/correlation_cost_test.py
@@ -21,8 +21,6 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow_addons.layers.correlation_cost import correlation_cost
-from tensorflow.python.platform import test
-tf.compat.v1.disable_eager_execution()
 
 
 class CorrelationCostTest(tf.test.TestCase):
@@ -34,72 +32,24 @@ class CorrelationCostTest(tf.test.TestCase):
                  stride_1,
                  stride_2,
                  pad,
-                 data_format,
-                 use_gpu=False):
-        with self.test_session(use_gpu=use_gpu, force_gpu=use_gpu) as sess:
+                 data_format):
 
-            input_a_op = tf.convert_to_tensor(input_a, dtype=tf.float32)
-            input_b_op = tf.convert_to_tensor(input_b, dtype=tf.float32)
+        input_a_op = tf.convert_to_tensor(input_a, dtype=tf.float32)
+        input_b_op = tf.convert_to_tensor(input_b, dtype=tf.float32)
 
-            call_op = correlation_cost
-            actual_op = call_op(
-                input_a_op,
-                input_b_op,
-                kernel_size=kernel_size,
-                max_displacement=max_displacement,
-                stride_1=stride_1,
-                stride_2=stride_2,
-                pad=pad,
-                data_format=data_format)
-
-            return sess.run(actual_op)
-
-    def _forward_both(self, data_format='NCHW'):
-        val = [[[[0, -6, 9, 5], [1, -5, 10, 3], [2, -4, 11, 1]],
-                [[3, -3, 12, -1], [4, -2, 13, -3], [5, -1, 14, -5]]],
-               [[[6, 0, 15, -7], [7, 1, 16, -9], [8, 2, 17, -11]],
-                [[9, 3, 18, -13], [10, 4, 19, -15], [11, 5, 20, -17]]]]
-
-        input_a = tf.constant(np.array(val), dtype=tf.float32)
-        valb = np.array(val).transpose(2, 3, 0, 1).reshape(2, 2, 3, 4)
-        input_b = tf.constant(valb, dtype=tf.float32)
-
-        kernel_size = 1
-        max_displacement = 2
-        stride_1 = 1
-        stride_2 = 2
-        pad = 4
-
-        if data_format == 'NHWC':
-            input_a = tf.transpose(input_a, [0, 2, 3, 1])
-            input_b = tf.transpose(input_b, [0, 2, 3, 1])
-
-        actual_cpu = self._forward(
-            input_a,
-            input_b,
+        output = correlation_cost(
+            input_a_op,
+            input_b_op,
             kernel_size=kernel_size,
             max_displacement=max_displacement,
             stride_1=stride_1,
             stride_2=stride_2,
             pad=pad,
-            data_format=data_format,
-            use_gpu=False)
+            data_format=data_format)
 
-        actual_gpu = self._forward(
-            input_a,
-            input_b,
-            kernel_size=kernel_size,
-            max_displacement=max_displacement,
-            stride_1=stride_1,
-            stride_2=stride_2,
-            pad=pad,
-            data_format=data_format,
-            use_gpu=True)
+        return output
 
-        self.assertEqual(actual_cpu.shape, actual_gpu.shape)
-        self.assertAllClose(actual_cpu, actual_gpu)
-
-    def _forward_simple(self, data_format='NCHW', use_gpu=False):
+    def _forward_simple(self, data_format='NCHW'):
         # cumbersome calculation by hand for a fixed input
         # we just test where zeros occurs and a few entries
         val = [[[[0, -6, 9, 5], [1, -5, 10, 3], [2, -4, 11, 1]],
@@ -115,8 +65,8 @@ class CorrelationCostTest(tf.test.TestCase):
             input_a = tf.transpose(input_a, [0, 2, 3, 1])
             input_b = tf.transpose(input_b, [0, 2, 3, 1])
 
-        input_a_op = tf.convert_to_tensor(input_a, dtype=tf.float32)
-        input_b_op = tf.convert_to_tensor(input_b, dtype=tf.float32)
+        input_a_tensor = tf.convert_to_tensor(input_a, dtype=tf.float32)
+        input_b_tensor = tf.convert_to_tensor(input_b, dtype=tf.float32)
 
         kernel_size = 1
         max_displacement = 2
@@ -125,36 +75,35 @@ class CorrelationCostTest(tf.test.TestCase):
         pad = 4
 
         actual = self._forward(
-            input_a_op,
-            input_b_op,
+            input_a_tensor,
+            input_b_tensor,
             kernel_size=kernel_size,
             max_displacement=max_displacement,
             stride_1=stride_1,
             stride_2=stride_2,
             pad=pad,
-            data_format=data_format,
-            use_gpu=use_gpu)
+            data_format=data_format)
 
         if data_format == 'NHWC':
             # NHWC -> NCHW
-            actual = actual.transpose(0, 3, 1, 2)
+            actual = tf.transpose(actual, [0, 3, 1, 2])
 
         # we just need to test fixed ids, as output is NCHW independently from data_format
         expected_ids = np.concatenate([np.zeros(464,), np.ones(464,)])
-        self.assertAllClose(np.where(actual == 0)[0], expected_ids)
+        self.assertAllClose(np.where(actual.numpy() == 0)[0], expected_ids)
 
         counts = [54, 52, 54, 50, 44, 50, 54, 52, 54]
         expected_ids = np.concatenate(
             [k * np.ones(v,) for k, v in enumerate(counts)])
         expected_ids = np.concatenate([expected_ids, expected_ids])
-        self.assertAllClose(np.where(actual == 0)[1], expected_ids)
+        self.assertAllClose(np.where(actual.numpy() == 0)[1], expected_ids)
         self.assertEqual(actual.shape, (2, 9, 7, 8))
 
-    def _gradients(self, data_format='NCHW', use_gpu=False):
+    def _gradients(self, data_format='NCHW'):
 
         batch, channels, height, width = 2, 3, 5, 6
-        input_a = np.random.randn(batch, channels, height, width)
-        input_b = np.random.randn(batch, channels, height, width)
+        input_a = tf.random.normal([batch, channels, height, width])
+        input_b = tf.random.normal([batch, channels, height, width])
 
         kernel_size = 1
         max_displacement = 2
@@ -163,68 +112,40 @@ class CorrelationCostTest(tf.test.TestCase):
         pad = 4
 
         if data_format == 'NHWC':
-            input_a = input_a.transpose(0, 2, 3, 1)
-            input_b = input_b.transpose(0, 2, 3, 1)
+            input_a = tf.transpose(input_a, [0, 2, 3, 1])
+            input_b = tf.transpose(input_b, [0, 2, 3, 1])
 
-        with self.test_session(use_gpu=use_gpu, force_gpu=use_gpu):
+        input_a_op = tf.convert_to_tensor(input_a, dtype=tf.float32)
+        input_b_op = tf.convert_to_tensor(input_b, dtype=tf.float32)
 
-            input_a_op = tf.convert_to_tensor(input_a, dtype=tf.float32)
-            input_b_op = tf.convert_to_tensor(input_b, dtype=tf.float32)
-
-            # def correlation_fn(inputs):
-            #     output = correlation_cost(
-            #         inputs[0],
-            #         inputs[1],
-            #         kernel_size=kernel_size,
-            #         max_displacement=max_displacement,
-            #         stride_1=stride_1,
-            #         stride_2=stride_2,
-            #         pad=pad,
-            #         data_format=data_format)
-            #     return output
-            #
-            # theoretical, numerical = tf.test.compute_gradient(
-            #     correlation_fn, [[input_a_op, input_b_op]])
-            #
-            # self.assertAllClose(theoretical[0], numerical[0], 1e-4)
-
-            call_op = correlation_cost
-            actual_op = call_op(
-                input_a_op,
-                input_b_op,
+        def correlation_fn(inputs):
+            output = correlation_cost(
+                inputs[0],
+                inputs[1],
                 kernel_size=kernel_size,
                 max_displacement=max_displacement,
                 stride_1=stride_1,
                 stride_2=stride_2,
                 pad=pad,
                 data_format=data_format)
+            return output
 
-            err_a = test.compute_gradient_error([input_a_op, input_b_op],
-                                                [input_a.shape, input_b.shape],
-                                                actual_op,
-                                                actual_op.shape.as_list())
+        theoretical, numerical = tf.test.compute_gradient(
+            correlation_fn, [[input_a_op, input_b_op]])
 
-            self.assertLess(err_a, 1e-4)
+        self.assertAllClose(theoretical[0], numerical[0], atol=1e-3)
 
     def testForwardNCHW(self):
-        self._forward_simple(data_format='NCHW', use_gpu=False)
-        self._forward_simple(data_format='NCHW', use_gpu=True)
+        self._forward_simple(data_format='NCHW')
 
     def testForwardNHWC(self):
-        self._forward_simple(data_format='NHWC', use_gpu=False)
-        self._forward_simple(data_format='NHWC', use_gpu=True)
-
-    def testForwardSame(self):
-        self._forward_both(data_format='NCHW')
-        self._forward_both(data_format='NHWC')
+        self._forward_simple(data_format='NHWC')
 
     def testBackwardNCHW(self):
-        self._gradients(data_format='NCHW', use_gpu=False)
-        self._gradients(data_format='NCHW', use_gpu=True)
+        self._gradients(data_format='NCHW')
 
     def testBackwardNHWC(self):
-        self._gradients(data_format='NHWC', use_gpu=False)
-        self._gradients(data_format='NHWC', use_gpu=True)
+        self._gradients(data_format='NHWC')
 
 
 if __name__ == "__main__":
