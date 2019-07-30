9051ac8847012163eed7d545f1359c545b8553ac
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2019-07-30 22:11:17 +0530

    Add training parameter to step (#369)
    * Pass training from BaseDecoder.call to BasicDecoder
      and BeamSearchDecoder

--- tensorflow_addons/seq2seq/attention_wrapper_test.py
+++ tensorflow_addons/seq2seq/attention_wrapper_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for contrib.seq2seq.python.ops.attention_wrapper."""
+"""Tests for tfa.seq2seq.attention_wrapper."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
