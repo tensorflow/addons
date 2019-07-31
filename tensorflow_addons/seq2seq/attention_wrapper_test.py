ad91edb5df7f8554bb93f3024d2db4a8ec0f39af
Karmel Allison <karmel@google.com>
2019-07-31 12:16:22 -0700

    Github text editor insists on an em-dash, strangely.

--- tensorflow_addons/seq2seq/attention_wrapper_test.py
+++ tensorflow_addons/seq2seq/attention_wrapper_test.py
@@ -147,7 +147,7 @@ class AttentionMechanismTest(tf.test.TestCase, parameterized.TestCase):
         x_test = np.random.randint(vocab, size=(self.batch, self.timestep))
         y = np.random.randn(self.batch, self.timestep)
         model = keras.models.Model([inputs, query, state], score)
-        # Fall back to v1-style Keras training loop until issue with 
+        # Fall back to v1 style Keras training loop until issue with 
         # using outputs of a layer in another layer's constructor.
         model.compile("rmsprop", "mse", experimental_run_tf_function=False)
         model.fit([x, self.query, self.state], (y, y))
@@ -159,7 +159,7 @@ class AttentionMechanismTest(tf.test.TestCase, parameterized.TestCase):
             config, custom_objects={attention_cls.__name__: attention_cls})
         loaded_model.set_weights(weights)
 
-        # Fall back to v1-style Keras training loop until issue with 
+        # Fall back to v1 style Keras training loop until issue with 
         # using outputs of a layer in another layer's constructor.
         loaded_model.compile(
             "rmsprop", "mse", experimental_run_tf_function=False)
