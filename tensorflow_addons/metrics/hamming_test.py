a10335a391f2a32169f7103062c93db3b88d762f
saishruthi <saishruthi.tn@ibm.com>
2019-07-31 17:11:00 -0700

    resolving conflicts

--- tensorflow_addons/metrics/hamming_test.py
+++ tensorflow_addons/metrics/hamming_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests F1 metrics."""
+"""Tests Hamming metrics."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -42,14 +42,14 @@ class HammingMetricsTest(tf.test.TestCase):
         self.assertAllClose(value, self.evaluate(obj.result()), atol=1e-5)
 
     def test_mc_4_classes(self):
-        actuals = tf.constant([[1, 0, 0, 0], [0, 0, 1, 0],
-                               [0, 0, 0, 1], [0, 1, 0, 0],
-                               [0, 1, 0, 0], [1, 0, 0, 0],
-                               [0, 0, 1, 0]], dtype=tf.float32)
-        predictions = tf.constant([[1, 0, 0, 0], [0, 0, 1, 0],
-                                   [0, 0, 0, 1], [1, 0, 0, 0],
-                                   [1, 0, 0, 0], [1, 0, 0, 0],
-                                   [0, 0, 1, 0]], dtype=tf.float32)
+        actuals = tf.constant(
+            [[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0],
+             [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0]],
+            dtype=tf.float32)
+        predictions = tf.constant(
+            [[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [1, 0, 0, 0],
+             [1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0]],
+            dtype=tf.float32)
         # Initialize
         hl_obj = self.initialize_vars('multiclass')
         # Update
@@ -58,16 +58,16 @@ class HammingMetricsTest(tf.test.TestCase):
         self.check_results(hl_obj, 0.2857143)
 
     def test_mc_5_classes(self):
-        actuals = tf.constant([[1, 0, 0, 0, 0], [0, 0, 0, 1, 0],
-                               [0, 0, 0, 0, 1], [0, 1, 0, 0, 0],
-                               [0, 0, 1, 0, 0], [0, 0, 1, 0, 0],
-                               [1, 0, 0, 0, 0], [0, 1, 0, 0, 0]],
-                              dtype=tf.int32)
-        predictions = tf.constant([[1, 0, 0, 0, 0], [0, 0, 0, 1, 0],
-                                   [0, 1, 0, 0, 0],
-                                   [0, 1, 0, 0, 0], [0, 0, 1, 0, 0],
-                                   [0, 0, 0, 1, 0], [1, 0, 0, 0, 0],
-                                   [0, 1, 0, 0, 0]], dtype=tf.int32)
+        actuals = tf.constant(
+            [[1, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1],
+             [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0],
+             [1, 0, 0, 0, 0], [0, 1, 0, 0, 0]],
+            dtype=tf.int32)
+        predictions = tf.constant(
+            [[1, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 1, 0, 0, 0],
+             [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0],
+             [1, 0, 0, 0, 0], [0, 1, 0, 0, 0]],
+            dtype=tf.int32)
         # Initialize
         hl_obj = self.initialize_vars('multiclass')
         # Update
@@ -76,10 +76,10 @@ class HammingMetricsTest(tf.test.TestCase):
         self.check_results(hl_obj, 0.25)
 
     def test_ml_4_classes(self):
-        actuals = tf.constant([[1, 0, 1, 0], [0, 1, 0, 1],
-                               [0, 0, 0, 1]], dtype=tf.float32)
-        predictions = tf.constant([[1, 0, 1, 0], [0, 1, 0, 1],
-                                   [1, 0, 0, 0]], dtype=tf.float32)
+        actuals = tf.constant([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 0, 1]],
+                              dtype=tf.float32)
+        predictions = tf.constant([[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 0, 0]],
+                                  dtype=tf.float32)
         # Initialize
         hl_obj = self.initialize_vars('multilabel')
         # Update
@@ -88,16 +88,16 @@ class HammingMetricsTest(tf.test.TestCase):
         self.check_results(hl_obj, 0.16666667)
 
     def test_ml_5_classes(self):
-        actuals = tf.constant([[1, 0, 0, 0, 0], [0, 0, 1, 1, 0],
-                               [0, 1, 0, 1, 0], [0, 1, 1, 0, 0],
-                               [0, 0, 1, 1, 0], [0, 0, 1, 1, 0],
-                               [1, 0, 0, 0, 1], [0, 1, 1, 0, 0]],
-                              dtype=tf.int32)
-        predictions = tf.constant([[1, 1, 0, 0, 0], [0, 0, 1, 1, 0],
-                                   [0, 1, 0, 1, 0], [0, 1, 1, 0, 0],
-                                   [0, 0, 1, 0, 0], [0, 0, 1, 1, 0],
-                                   [1, 0, 0, 0, 0], [0, 1, 1, 0, 0]],
-                                  dtype=tf.int32)
+        actuals = tf.constant(
+            [[1, 0, 0, 0, 0], [0, 0, 1, 1, 0], [0, 1, 0, 1, 0],
+             [0, 1, 1, 0, 0], [0, 0, 1, 1, 0], [0, 0, 1, 1, 0],
+             [1, 0, 0, 0, 1], [0, 1, 1, 0, 0]],
+            dtype=tf.int32)
+        predictions = tf.constant(
+            [[1, 1, 0, 0, 0], [0, 0, 1, 1, 0], [0, 1, 0, 1, 0],
+             [0, 1, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 1, 0],
+             [1, 0, 0, 0, 0], [0, 1, 1, 0, 0]],
+            dtype=tf.int32)
         # Initialize
         hl_obj = self.initialize_vars('multilabel')
         # Update
@@ -106,8 +106,7 @@ class HammingMetricsTest(tf.test.TestCase):
         self.check_results(hl_obj, 0.075)
 
     def hamming_distance_test(self):
-        actuals = tf.constant([1, 1, 0, 0, 1, 0, 1, 0, 0, 1],
-                              dtype=tf.int32)
+        actuals = tf.constant([1, 1, 0, 0, 1, 0, 1, 0, 0, 1], dtype=tf.int32)
         predictions = tf.constant([1, 0, 0, 0, 1, 0, 0, 1, 0, 1],
                                   dtype=tf.int32)
         test_result = hamming_distance(actuals, predictions)
