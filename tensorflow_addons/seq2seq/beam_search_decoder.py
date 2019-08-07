fabbca9bde6bd5a66de36cc87f962f5ce3ef67a8
Yan Facai (颜发才) <facai.yan@gmail.com>
2019-08-07 12:54:46 +0800

    BLD: upgrade pylint for python 3 (#399)

--- tensorflow_addons/seq2seq/beam_search_decoder.py
+++ tensorflow_addons/seq2seq/beam_search_decoder.py
@@ -514,8 +514,13 @@ class BeamSearchDecoderMixin(object):
                 self._beam_width):
             return t
         t = t.stack()
+        # yapf:disable
         with tf.control_dependencies(
-            [_check_batch_beam(t, self._batch_size, self._beam_width)]):
+                [_check_batch_beam(  # pylint: disable=bad-continuation
+                    t,
+                    self._batch_size,
+                    self._beam_width)]):
+            # yapf:enable
             return gather_tree_from_array(t, parent_ids, sequence_length)
 
     def step(self, time, inputs, state, training=None, name=None):
