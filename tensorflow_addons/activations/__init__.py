3a653b7c79649b69341c4d89ffdaaf5f8a9aecc5
Aakash Kumar Nain <aakashnain@outlook.com>
2019-08-15 22:38:28 +0530

    add new import for GELU activation

--- tensorflow_addons/activations/__init__.py
+++ tensorflow_addons/activations/__init__.py
@@ -19,3 +19,4 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow_addons.activations.sparsemax import sparsemax
+from tensorflow_addons.activations.gelu import gelu
