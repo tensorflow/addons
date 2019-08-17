bed610a8353da41dd99fbfe3debf335b7e8f4a2b
Aakash Kumar Nain <aakashnain@outlook.com>
2019-08-17 15:28:38 +0530

    change order of imports

--- tensorflow_addons/activations/__init__.py
+++ tensorflow_addons/activations/__init__.py
@@ -18,5 +18,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from tensorflow_addons.activations.gelu import GeLU
 from tensorflow_addons.activations.sparsemax import sparsemax
-from tensorflow_addons.activations.gelu import gelu
