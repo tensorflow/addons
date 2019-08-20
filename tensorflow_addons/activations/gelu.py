37c7c646716e04a46833b346e03be5f7d9f2d8eb
Aakash Kumar Nain <aakashnain@outlook.com>
2019-08-20 18:45:04 +0530

    code format

--- tensorflow_addons/activations/gelu.py
+++ tensorflow_addons/activations/gelu.py
@@ -29,17 +29,18 @@ from tensorflow_addons.utils import keras_utils
 def gelu(x, dtype='float32'):
     """Gaussian Error Linear Unit.
 
-    A smoother version of ReLU generally used 
+    A smoother version of ReLU generally used
     in the BERT or BERT architecture based models.
     Original paper: https://arxiv.org/abs/1606.08415
-    
+
     Args:
         x: float Tensor to perform activation.
     Returns:
         `x` with the GELU activation applied.
     """
 
-    x  = K.cast(x, dtype=dtype)
+    x = K.cast(x, dtype=dtype)
     pi = K.cast(math.pi, dtype=dtype)
     return 0.5 * x * (1 + tf.tanh(tf.sqrt(2 / pi) * \
              (x + 0.044715 * tf.pow(x, 3))))
+    
