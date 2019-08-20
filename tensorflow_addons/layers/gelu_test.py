c6f981df6e27f912a93666e84590ba27c5e9b7e9
Aakash Kumar Nain <aakashnain@outlook.com>
2019-08-20 18:39:43 +0530

    code format and sanity check pass

--- tensorflow_addons/layers/gelu_test.py
+++ tensorflow_addons/layers/gelu_test.py
@@ -20,11 +20,10 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import backend as K
+from absl.testing import parameterized
 from tensorflow_addons.layers.gelu import GeLU
 from tensorflow_addons.utils import test_utils
 from tensorflow_addons.utils.test_utils import keras_parameterized
-from absl.testing import parameterized
 
 
 @parameterized.parameters([np.float16, np.float32, np.float64])
@@ -33,7 +32,6 @@ class TestGeLU(tf.test.TestCase):
     def test_random(self, dtype):
         x = np.array([[0.5, 1.2, -0.3]]).astype(dtype)
         val = np.array([[0.345714, 1.0617027, -0.11462909]]).astype(dtype)
-     
         test_utils.layer_test(
             GeLU,
             kwargs={'dtype': dtype},
@@ -43,16 +41,18 @@ class TestGeLU(tf.test.TestCase):
 
 @keras_parameterized.run_all_keras_modes
 @keras_parameterized.run_with_all_model_types
-class TestGeLU_v2(keras_parameterized.TestCase):
-	def test_layer_random(self):
-		layer = tf.keras.layers.Dense(1, activation=GeLU())
-		model = keras_parameterized.testing_utils.get_model_from_layers([layer], 
-                                                              input_shape=(10,))
-		model.compile(
-			'sgd',
-			'mse',
-			run_eagerly=keras_parameterized.testing_utils.should_run_eagerly())
-		model.fit(np.ones((10, 10)), np.ones((10, 1)), batch_size=2)
+class TestGeLUModel(keras_parameterized.TestCase):
+    """Test GeLU with random keras model"""
+    def test_layer_random(self):
+        layer = tf.keras.layers.Dense(1, activation=GeLU())
+        model = keras_parameterized.testing_utils.get_model_from_layers(
+            [layer],
+            input_shape=(10,))
+        model.compile(
+            'sgd',
+            'mse',
+            run_eagerly=keras_parameterized.testing_utils.should_run_eagerly())
+        model.fit(np.ones((10, 10)), np.ones((10, 1)), batch_size=2)
 
 if __name__ == '__main__':
     tf.test.main()
