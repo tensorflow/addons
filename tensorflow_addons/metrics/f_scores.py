92f5f37c641d86ae35b5af0e936b61884e5bc527
saishruthi <saishruthi.tn@ibm.com>
2019-08-22 17:27:49 -0700

    adding tofo

--- tensorflow_addons/metrics/f_scores.py
+++ tensorflow_addons/metrics/f_scores.py
@@ -175,7 +175,8 @@ class FBetaScore(Metric):
                 initializer='zeros',
                 dtype=self.dtype)
 
-    # TO DO SSaishruthi: Add sample weight option
+    # TODO: Add sample_weight support, currently it is
+    # ignored during calculations.
     def update_state(self, y_true, y_pred, sample_weight=None):
         y_true = tf.cast(y_true, tf.int32)
         y_pred = tf.cast(y_pred, tf.int32)
@@ -339,7 +340,8 @@ class F1Score(FBetaScore):
         super(F1Score, self).__init__(
             num_classes, average, 1.0, name=name, dtype=dtype)
 
-    # TO DO SSaishruthi: Add sample weight option
+    # TODO: Add sample_weight support, currently it is
+    # ignored during calculations.
     def get_config(self):
         base_config = super(F1Score, self).get_config()
         del base_config["beta"]
