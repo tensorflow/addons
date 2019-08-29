19708440caa0323a0fd0b036ba35b25ddd2d41a8
Tzu-Wei Sung <windqaq@gmail.com>
2019-08-29 15:49:20 +0800

    build gpu kernel

--- tensorflow_addons/custom_ops/layers/cc/kernels/correlation_cost_op.cc
+++ tensorflow_addons/custom_ops/layers/cc/kernels/correlation_cost_op.cc
@@ -331,7 +331,7 @@ TF_CALL_float(REGISTER_CORRELATIONCOST_OP_CPU);
 #undef REGISTER_CORRELATIONCOST_OP_CPU
 
 // Register the GPU kernels.
-#ifdef GOOGLE_CUDA
+#if GOOGLE_CUDA
 
 #define REGISTER_CORRELATIONCOST_OP_GPU(T)                                   \
   REGISTER_KERNEL_BUILDER(                                                   \
