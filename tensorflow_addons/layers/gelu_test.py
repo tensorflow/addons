db52c48a09652caa8fa3caa49ecc89a00ad9c53a
Aakash Kumar Nain <aakashnain@outlook.com>
2019-08-29 18:28:37 +0530

    remove redundant test cases

--- tensorflow_addons/layers/gelu_test.py
+++ tensorflow_addons/layers/gelu_test.py
@@ -23,7 +23,6 @@ import tensorflow as tf
 from absl.testing import parameterized
 from tensorflow_addons.layers.gelu import GeLU
 from tensorflow_addons.utils import test_utils
-from tensorflow_addons.utils.test_utils import keras_parameterized
 
 
 @parameterized.parameters([np.float16, np.float32, np.float64])
@@ -34,22 +33,6 @@ class TestGeLU(tf.test.TestCase):
         val = np.array([[0.345714, 1.0617027, -0.11462909]]).astype(dtype)
         test_utils.layer_test(
             GeLU, kwargs={'dtype': dtype}, input_data=x, expected_output=val)
-
-
-@keras_parameterized.run_all_keras_modes
-@keras_parameterized.run_with_all_model_types
-class TestGeLUModel(keras_parameterized.TestCase):
-    """Test GeLU with random keras model."""
-
-    def test_layer_random(self):
-        layer = tf.keras.layers.Dense(1, activation=GeLU())
-        model = keras_parameterized.testing_utils.get_model_from_layers(
-            [layer], input_shape=(10,))
-        model.compile(
-            'sgd',
-            'mse',
-            run_eagerly=keras_parameterized.testing_utils.should_run_eagerly())
-        model.fit(np.ones((10, 10)), np.ones((10, 1)), batch_size=2)
 
 
 if __name__ == '__main__':
