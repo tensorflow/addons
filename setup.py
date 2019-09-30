1c6862eb0d655ba20a360af90bd3b2a1c5f5ab86
Yan Facai (颜发才) <facai.yan@gmail.com>
2019-10-01 07:02:22 +0800

    RELEASE: 0.5.2 (#549)
    * RELEASE: 0.5.2
    
    * BLD: disable too-many-function-args pylint
    
    * BLD: built against 2.0.0

--- setup.py
+++ setup.py
@@ -69,9 +69,9 @@ REQUIRED_PACKAGES = [
 if project_name == TFA_RELEASE:
     # TODO: remove if-else condition when tf supports package consolidation.
     if platform.system() == 'Linux':
-        REQUIRED_PACKAGES.append('tensorflow-gpu == 2.0.0-rc1')
+        REQUIRED_PACKAGES.append('tensorflow-gpu == 2.0.0')
     else:
-        REQUIRED_PACKAGES.append('tensorflow == 2.0.0-rc1')
+        REQUIRED_PACKAGES.append('tensorflow == 2.0.0')
 elif project_name == TFA_NIGHTLY:
     # TODO: remove if-else condition when tf-nightly supports package consolidation.
     if platform.system() == 'Linux':
