d8d2157874d5c074275da30d1ed9f3bf433346b1
Xiaoquan Kong <u1mail2me@gmail.com>
2019-10-11 20:52:57 +0800

    Update

--- tensorflow_addons/losses/crf_loss.py
+++ tensorflow_addons/losses/crf_loss.py
@@ -20,7 +20,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-from tensorflow_addons.layers import CRF
+from tensorflow_addons.layers.crf import CRF
 from tensorflow_addons.utils import keras_utils
 
 
