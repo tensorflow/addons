c16dfa4ac4e9e869ca63dbe147097f74bb0d1ab8
Sean Morgan <seanmorgan91@gmail.com>
2019-10-16 18:15:56 -0400

    DOC: Standardize doc information

--- tensorflow_addons/rnn/__init__.py
+++ tensorflow_addons/rnn/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Customized RNN cells."""
+"""Additional RNN cells that corform to Keras API."""
 
 from __future__ import absolute_import
 from __future__ import division
