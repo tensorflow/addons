b06eda783c0456f3c03560f39c8e6926f7fe510a
Sean Morgan <seanmorgan@outlook.com>
2019-10-22 18:16:20 -0400

    DOC: Standardize doc information (#600)

--- tensorflow_addons/activations/__init__.py
+++ tensorflow_addons/activations/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""A module containing activation routines."""
+"""Addititonal activation functions."""
 
 from __future__ import absolute_import
 from __future__ import division
--- tensorflow_addons/image/__init__.py
+++ tensorflow_addons/image/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Image manipulation ops."""
+"""Additional image manipulation ops."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
