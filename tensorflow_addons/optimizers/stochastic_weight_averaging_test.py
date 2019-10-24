7caaeda440b6816594fdc0c0394a0c75dcb2f4ad
Shreyash Patodia <patodiashreyash32@gmail.com>
2019-10-24 03:34:42 -0400

    Update stochastic_weight_averaging_test.py

--- tensorflow_addons/optimizers/stochastic_weight_averaging_test.py
+++ tensorflow_addons/optimizers/stochastic_weight_averaging_test.py
@@ -30,47 +30,40 @@ SWA = stochastic_weight_averaging.SWA
 @test_utils.run_all_in_graph_and_eager_modes
 class SWATest(tf.test.TestCase):
 
-    def run_sample(self, iterations, optimizer, seed=0x2019):
-        np.random.seed(seed)
-
-        val_0 = np.random.random((2,))
-        val_1 = np.random.random((2,))
-
-        var_0 = tf.Variable(val_0, dtype=tf.dtypes.float32)
-        var_1 = tf.Variable(val_1, dtype=tf.dtypes.float32)
-
-        grad_0 = tf.constant([0.1, 0.1])
-        grad_1 = tf.constant([0.01, 0.01])
-
-        grads_and_vars = list(zip([grad_0, grad_1], [var_0, var_1]))
-
-        if tf.executing_eagerly():
-            for _ in range(iterations):
-                optimizer.apply_gradients(grads_and_vars)
-        else:
-            update = optimizer.apply_gradients(grads_and_vars)
-            self.evaluate(tf.compat.v1.global_variables_initializer())
-            for _ in range(iterations):
-                self.evaluate(update)
-
-        return [val_0, val_1], [grad_0, grad_1], [self.evaluate(var_0),
-                                                  self.evaluate(var_1)]
-
     def test_averaging(self):
-        for start_averaging in [0]:
-            for average_period in [5, 10, 100]:
+        
+        for start_averaging in [0, 1]:
+            for average_period in [1, 2]:
                 optimizer = SWA('adam', start_averaging, average_period)
-                initial_vals, grads, final_vals = self.run_sample(
-                    start_averaging + average_period + 1, 
-                    optimizer
-                )
-                first_vals = [
-                    initial_vals[0] - (start_averaging + 1) * grads[0],
-                    initial_vals[1] - (start_averaging + 1) * grads[1]
+                
+                val_0 = [1, 1]
+                val_1 = [2, 2]
+                
+                var_0 = tf.Variable(val_0)
+                var_1 = tf.Variable(val_1)
+                
+                grad_0 = tf.constant([0.1, 0.1])
+                grad_1 = tf.constant([0.1, 0.1])
+                
+                for _ in range(start_averaging + 1):
+                    optimizer.apply_gradients(zip([grad_0, grad_1], [var_0, var_1]))
+                    
+                first_val_0 = tf.identity(var_0)
+                first_val_1 = tf.identity(var_1)
+                
+                for _ in range(average_period + 1):
+                    optimizer.apply_gradients(zip([grad_0, grad_1], [var_0, var_1]))
+                
+                second_val_0 = tf.identity(var_0)
+                second_val_1 = tf.identity(var_1)
+                
+                optimizer.assign_average_vars([var_0, var_1])
+                expected_val = [
+                    (first_val_0 + second_val_0) / 2.0,
+                    (first_val_1 + second_val_1) / 2.0
                 ]
-                average_vals = [tf.Variable((first_vals[0] + final_vals[0]) / 2.0),
-                                tf.Variable((first_vals[1] + final_vals[1]) / 2.0)]
-                msg = '{}'.format(final_vals)
+                msg = '{} | {}'.format([var_0, var_1], expected_val)
+                
                 try:
                     optimizer.assign_average_vars(final_vals)
                 except:
