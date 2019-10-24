888b2639555e341767e4dcc837f63c38c886e5ca
Shreyash Patodia <patodiashreyash32@gmail.com>
2019-10-24 05:12:28 -0400

    Lint.

--- tensorflow_addons/optimizers/stochastic_weight_averaging.py
+++ tensorflow_addons/optimizers/stochastic_weight_averaging.py
@@ -14,13 +14,13 @@
 # ==============================================================================
 """An implementation of the Stochastic Weight Averaging optimizer.
 
-The Stochastic Weight Averaging mechanism was proposed by Pavel Izmailov et.
-al in the paper [Averaging Weights Leads to Wider Optima and
-Better Generalization](https://arxiv.org/abs/1803.05407). The optimizer
-implements averaging of multiple points along the trajectory of SGD. This
-averaging has shown to improve model performance on validation/test sets 
-whilst possibly causing a small increase in loss on the training set.
-
+The Stochastic Weight Averaging mechanism was proposed by Pavel Izmailov
+et. al in the paper [Averaging Weights Leads to Wider Optima and Better
+Generalization](https://arxiv.org/abs/1803.05407). The optimizer
+implements averaging of multiple points along the trajectory of SGD.
+This averaging has shown to improve model performance on validation/test
+sets whilst possibly causing a small increase in loss on the training
+set.
 """
 
 from __future__ import absolute_import
@@ -53,7 +53,7 @@ class SWA(tf.keras.optimizers.Optimizer):
 
     Note: If your model has batch-normalization layers you would need to run the
     final weights through the data to compute the running mean and variance
-    corresponding to the activations for each layer of the network. From 
+    corresponding to the activations for each layer of the network. From
     the paper: If the DNN uses batch normalization we run one additional
     pass over the data, to compute the running mean and standard deviation
     of the activations for each layer of the network with SWA weights
@@ -117,18 +117,18 @@ class SWA(tf.keras.optimizers.Optimizer):
         self._initialized = False
 
     def _create_slots(self, var_list):
-        self._optimizer._create_slots(var_list=var_list)    # pylint: disable=protected-access
+        self._optimizer._create_slots(var_list=var_list)  # pylint: disable=protected-access
         for var in var_list:
             self.add_slot(var, 'average')
 
     def _create_hypers(self):
-        self._optimizer._create_hypers()    # pylint: disable=protected-access
+        self._optimizer._create_hypers()  # pylint: disable=protected-access
 
     def _prepare(self, var_list):
-        return self._optimizer._prepare(var_list=var_list)    # pylint: disable=protected-access
+        return self._optimizer._prepare(var_list=var_list)  # pylint: disable=protected-access
 
     def apply_gradients(self, grads_and_vars, name=None):
-        self._optimizer._iterations = self.iterations    # pylint: disable=protected-access
+        self._optimizer._iterations = self.iterations  # pylint: disable=protected-access
         return super(SWA, self).apply_gradients(grads_and_vars, name)
 
     def _average_op(self, var):
@@ -141,15 +141,15 @@ class SWA(tf.keras.optimizers.Optimizer):
         # number of times snapshots of weights have been taken (using max to
         # avoid negative values of num_snapshots).
         num_snapshots = tf.math.maximum(
-            tf.cast(0, tf.int64), tf.math.floordiv(self.iterations - start_averaging,
-                                                   average_period)
-        )
+            tf.cast(0, tf.int64),
+            tf.math.floordiv(self.iterations - start_averaging,
+                             average_period))
         # checks if the iteration is one in which a snapshot should be taken.
         sync_cond = tf.equal(start_averaging + num_snapshots * average_period,
                              self.iterations)
         num_snapshots = tf.cast(num_snapshots, tf.float32)
-        average_value = ((average_var * num_snapshots + var) /
-                         (num_snapshots + 1.))
+        average_value = (
+            (average_var * num_snapshots + var) / (num_snapshots + 1.))
         average_cond = tf.reduce_all([thresold_cond, sync_cond])
         with tf.control_dependencies([average_value]):
             average_update = average_var.assign(
@@ -166,13 +166,13 @@ class SWA(tf.keras.optimizers.Optimizer):
         return self._weights + self._optimizer.weights
 
     def _resource_apply_dense(self, grad, var):
-        train_op = self._optimizer._resource_apply_dense(grad, var)    # pylint: disable=protected-access
+        train_op = self._optimizer._resource_apply_dense(grad, var)  # pylint: disable=protected-access
         with tf.control_dependencies([train_op]):
             average_op = self._average_op(var)
         return tf.group(train_op, average_op)
 
     def _resource_apply_sparse(self, grad, var, indices):
-        train_op = self._optimizer._resource_apply_sparse(    # pylint: disable=protected-access
+        train_op = self._optimizer._resource_apply_sparse(  # pylint: disable=protected-access
             grad, var, indices)
         with tf.control_dependencies([train_op]):
             average_op = self._average_op(var)
@@ -210,26 +210,27 @@ class SWA(tf.keras.optimizers.Optimizer):
         config = {
             'optimizer': tf.keras.optimizers.serialize(self._optimizer),
             'average_period': self._serialize_hyperparameter('average_period'),
-            'start_averaging': self._serialize_hyperparameter('start_averaging')
+            'start_averaging':
+            self._serialize_hyperparameter('start_averaging')
         }
         base_config = super(SWA, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
     @property
     def lr(self):
-        return self._optimizer._get_hyper('learning_rate')    # pylint: disable=protected-access
+        return self._optimizer._get_hyper('learning_rate')  # pylint: disable=protected-access
 
     @lr.setter
     def lr(self, lr):
-        self._optimizer._set_hyper('learning_rate', lr)    # pylint: disable=protected-access
+        self._optimizer._set_hyper('learning_rate', lr)  # pylint: disable=protected-access
 
     @property
     def learning_rate(self):
-        return self._optimizer._get_hyper('learning_rate')    # pylint: disable=protected-access
+        return self._optimizer._get_hyper('learning_rate')  # pylint: disable=protected-access
 
     @learning_rate.setter
     def learning_rate(self, learning_rate):
-        self._optimizer._set_hyper('learning_rate', learning_rate)    # pylint: disable=protected-access
+        self._optimizer._set_hyper('learning_rate', learning_rate)  # pylint: disable=protected-access
 
     @classmethod
     def from_config(cls, config, custom_objects=None):
