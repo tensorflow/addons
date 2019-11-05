9beec61b9016a28463b71a03ce810b08d42294c6
fsx950223 <fsx950223@gmail.com>
2019-11-05 02:14:25 +0000

    Change to official api

--- tensorflow_addons/losses/giou_loss.py
+++ tensorflow_addons/losses/giou_loss.py
@@ -19,10 +19,9 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-from tensorflow_addons.utils import keras_utils
 
 
-@keras_utils.register_keras_custom_object
+@tf.keras.utils.register_keras_serializable(package='Addons')
 class GIoULoss(tf.keras.losses.Loss):
     """Implements the GIoU loss function.
 
@@ -69,7 +68,7 @@ class GIoULoss(tf.keras.losses.Loss):
         return giou_loss(y_true, y_pred, mode=self.mode)
 
 
-@keras_utils.register_keras_custom_object
+@tf.keras.utils.register_keras_serializable(package='Addons')
 @tf.function
 def giou_loss(y_true, y_pred, mode='giou'):
     """
