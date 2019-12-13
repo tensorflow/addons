2d0b3d605df6009784e8aa7df62e358fd3174dfa
Shun Lin <shunlin@google.com>
2019-12-12 20:05:42 -0800

    fixed line too long issue

--- tensorflow_addons/callbacks/time_stopping.py
+++ tensorflow_addons/callbacks/time_stopping.py
@@ -50,9 +50,9 @@ class TimeStopping(Callback):
     def on_train_end(self, logs=None):
         if self.verbose > 0:
             formatted_time = datetime.timedelta(seconds=self.seconds)
-            message = 'Timed stopping at epoch {} after training for {}'.format(
+            msg = 'Timed stopping at epoch {} after training for {}'.format(
                 self.stopped_epoch + 1, formatted_time)
-            print(message)
+            print(msg)
 
     def get_config(self):
         config = {
