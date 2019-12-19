d97d2506da6d3a37489873ff9acc7e94b73351dd
Brian Patton <22173987+brianwa84@users.noreply.github.com>
2019-12-19 10:55:51 -0500

    Update transform_ops_test.py

--- tensorflow_addons/image/transform_ops_test.py
+++ tensorflow_addons/image/transform_ops_test.py
@@ -121,7 +121,7 @@ class ImageOpsTest(tf.test.TestCase):
         for dtype in _DTYPES:
             image = tf.constant([[1, 2], [3, 4]], dtype=dtype)
             self.assertAllEqual(
-                np.array([[4, 4], [4, 4]]).astype(dtype.as_numpy_dtype(0)),
+                np.array([[4, 4], [4, 4]]).astype(dtype.as_numpy_dtype),
                 transform_ops.transform(image, [1] * 8))
 
     def test_transform_eager(self):
