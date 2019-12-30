669ea44aed9c426e4e72235d66ea4ef57e451ec1
Shubhanshu Mishra <smishra8@illinois.edu>
2019-12-30 16:23:51 -0500

    Fixed import of crf_filtetered_inputs to be at text module level

--- tensorflow_addons/text/__init__.py
+++ tensorflow_addons/text/__init__.py
@@ -22,6 +22,7 @@ from tensorflow_addons.text.crf import crf_binary_score
 from tensorflow_addons.text.crf import crf_decode
 from tensorflow_addons.text.crf import crf_decode_backward
 from tensorflow_addons.text.crf import crf_decode_forward
+from tensorflow_addons.text.crf import crf_filtered_inputs
 from tensorflow_addons.text.crf import crf_forward
 from tensorflow_addons.text.crf import crf_log_likelihood
 from tensorflow_addons.text.crf import crf_log_norm
