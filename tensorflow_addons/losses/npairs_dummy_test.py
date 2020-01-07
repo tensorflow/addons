4997d706f7fab8643856152de4f9bd64c9a23009
Sean Morgan <seanmorgan@outlook.com>
2020-01-07 01:11:32 -0500

    Add Github CI for windows (#834)
    * Add Github CI for windows
    
    * Update job names
    
    * Update python version for mac to match
    
    * Remove npairs from windows build and tests
    
    * Cleanup
    
    * SkipTest benchmark op
    
    * No skipTest available
    
    * Lint
    
    * Remove op build

---
+++ tensorflow_addons/losses/npairs_dummy_test.py
@@ -0,0 +1,6 @@
+"""Temporary until SparseMatMul issue is solved for windows."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Remove after: https://github.com/tensorflow/addons/issues/838
