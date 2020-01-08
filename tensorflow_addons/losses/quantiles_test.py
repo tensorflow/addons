9edab58af8d3a23079227047bb3c0a8e451fda8e
Romain Brault <mail@romainbrault.com>
2020-01-08 11:31:55 +0100

    typo

--- tensorflow_addons/losses/quantiles_test.py
+++ tensorflow_addons/losses/quantiles_test.py
@@ -21,10 +21,10 @@ from __future__ import print_function
 import tensorflow as tf
 import tensorflow_addons as tfa
 
-from tensorflow_addons import utils
+from tensorflow_addons.utils import test_utils
 
 
-@utils.test_utils.run_all_in_graph_and_eager_modes
+@test_utils.run_all_in_graph_and_eager_modes
 class PinballLossTest(tf.test.TestCase):
     def test_config(self):
         pin_obj = tfa.losses.PinballLoss(reduction=tf.keras.losses.Reduction.SUM, name='pin_1')
