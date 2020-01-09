8da0cc00c30b10ca83107af5fd145f050e8ddf7b
UH <554c46@gmail.com>
2020-01-09 14:44:03 +0100

    import aligned with cell_test.py

--- tensorflow_addons/rnn/layernorm_simplernn_test.py
+++ tensorflow_addons/rnn/layernorm_simplernn_test.py
@@ -22,13 +22,10 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 
-from tensorflow_addons.utils import test_utils  # for eager mode
-#from tensorflow.python.eager import context  # to check eager mode
-from tensorflow.python.keras import testing_utils  # for 'layer_test'
-from tensorflow.python.training import gradient_descent  # for GD
-
-import tensorflow_addons.rnn.layernorm_simplernn as lnrnn
-# import layernorm_simplernn as lnrnn
+from tensorflow_addons.utils import test_utils
+# from tensorflow_addons.rnn import LayernormSimpleRNN
+from tensorflow_addons.rnn.layernorm_simplernn import LayernormSimpleRNN
+# from layernorm_simplernn import LayernormSimpleRNN
 
 
 @test_utils.run_all_in_graph_and_eager_modes
@@ -38,8 +35,8 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         timesteps = 3
         embedding_dim = 4
         units = 2
-        testing_utils.layer_test(
-            lnrnn.LayernormSimpleRNN,
+        test_utils.layer_test(
+            LayernormSimpleRNN,
             kwargs={
                 'units': units,
                 'use_layernorm': True,
@@ -52,8 +49,8 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         timesteps = 3
         embedding_dim = 4
         units = 2
-        testing_utils.layer_test(
-            lnrnn.LayernormSimpleRNN,
+        test_utils.layer_test(
+            LayernormSimpleRNN,
             kwargs={
                 'units': units,
                 'use_layernorm': True,
@@ -68,7 +65,7 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         timesteps = 3
         embedding_dim = 4
         units = 2
-        layer = lnrnn.LayernormSimpleRNN(
+        layer = LayernormSimpleRNN(
             units, use_layernorm=True, input_shape=(None, embedding_dim))
         model = keras.models.Sequential()
         model.add(layer)
@@ -77,15 +74,15 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         y = np.random.random((num_samples, units))
         model.train_on_batch(x, y)
 
-    # test_implementation_mode_layernorm_rnn deleted
+    # DELETED TEST: test_implementation_mode_layernorm_rnn
 
     def test_dropout_layernorm_rnn(self):
         num_samples = 2
         timesteps = 3
         embedding_dim = 4
         units = 2
-        testing_utils.layer_test(
-            lnrnn.LayernormSimpleRNN,
+        test_utils.layer_test(
+            LayernormSimpleRNN,
             kwargs={
                 'units': units,
                 'use_layernorm': True,
@@ -96,7 +93,7 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
 
     def test_constraints_layernorm_rnn(self):
         embedding_dim = 4
-        layer_class = lnrnn.LayernormSimpleRNN
+        layer_class = LayernormSimpleRNN
         k_constraint = keras.constraints.max_norm(0.01)
         r_constraint = keras.constraints.max_norm(0.01)
         b_constraint = keras.constraints.max_norm(0.01)
@@ -118,7 +115,7 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         self.assertEqual(layer.cell.layernorm.gamma.constraint, g_constraint)
 
     def test_with_masking_layer_layernorm_rnn(self):
-        layer_class = lnrnn.LayernormSimpleRNN
+        layer_class = LayernormSimpleRNN
         inputs = np.random.random((2, 3, 4))
         targets = np.abs(np.random.random((2, 3, 5)))
         targets /= targets.sum(axis=-1, keepdims=True)
@@ -134,7 +131,7 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         model.fit(inputs, targets, epochs=1, batch_size=2, verbose=1)
 
     def test_from_config_layernorm_rnn(self):
-        layer_class = lnrnn.LayernormSimpleRNN
+        layer_class = LayernormSimpleRNN
         for stateful in (False, True):
             l1 = layer_class(units=1, use_layernorm=True, stateful=stateful)
             l2 = layer_class.from_config(l1.get_config())
@@ -142,7 +139,7 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
 
     def test_regularizers_layernorm_rnn(self):
         embedding_dim = 4
-        layer_class = lnrnn.LayernormSimpleRNN
+        layer_class = LayernormSimpleRNN
         layer = layer_class(
             5,
             use_layernorm=True,
@@ -164,76 +161,8 @@ class LayernormSimpleRNNTest(tf.test.TestCase):
         #else:
         #    self.assertEqual(len(layer.get_losses_for(x)), 1)
 
+    # STILL MISSING: test_statefulness_layernorm_rnn()
 
-"""
-STILL FAILS
-    def test_statefulness_layernorm_rnn(self):
-        num_samples = 2
-        timesteps = 3
-        embedding_dim = 4
-        units = 2
-        layer_class = lnrnn.LayernormSimpleRNN
-        model = keras.models.Sequential()
-        model.add(
-            keras.layers.Embedding(
-                4,
-                embedding_dim,
-                mask_zero=True,
-                input_length=timesteps,
-                batch_input_shape=(num_samples, timesteps)))
-        layer = layer_class(
-            units,
-            use_layernorm=True,
-            return_sequences=False,
-            stateful=True,
-            weights=None)
-        model.add(layer)
-        model.compile(
-            optimizer=gradient_descent.GradientDescentOptimizer(0.01),
-            loss='mse')
-        out1 = model.predict(np.ones((num_samples, timesteps)))
-        self.assertEqual(out1.shape, (num_samples, units))
-
-        # train once so that the states change
-        model.train_on_batch(
-            np.ones((num_samples, timesteps)), np.ones((num_samples, units)))
-        out2 = model.predict(np.ones((num_samples, timesteps)))
-
-        # if the state is not reset, output should be different
-        self.assertNotEqual(out1.max(), out2.max())
-
-        # check that output changes after states are reset
-        # (even though the model itself didn't change)
-        layer.reset_states()
-        out3 = model.predict(np.ones((num_samples, timesteps)))
-        self.assertNotEqual(out2.max(), out3.max())
-
-        # check that container-level reset_states() works
-        model.reset_states()
-        out4 = model.predict(np.ones((num_samples, timesteps)))
-        np.testing.assert_allclose(out3, out4, atol=1e-5)
-
-        # check that the call to `predict` updated the states
-        out5 = model.predict(np.ones((num_samples, timesteps)))
-        self.assertNotEqual(out4.max(), out5.max())
-
-        # Check masking
-        layer.reset_states()
-
-        left_padded_input = np.ones((num_samples, timesteps))
-        left_padded_input[0, :1] = 0
-        left_padded_input[1, :2] = 0
-        out6 = model.predict(left_padded_input)
-
-        layer.reset_states()
-
-        right_padded_input = np.ones((num_samples, timesteps))
-        right_padded_input[0, -1:] = 0
-        right_padded_input[1, -2:] = 0
-        out7 = model.predict(right_padded_input)
-
-        np.testing.assert_allclose(out7, out6, atol=1e-5)
-"""
 
 if __name__ == '__main__':
     tf.test.main()
