73c2b519b9113b4215e00c71021816dd31c0a0f9
Romain Brault <mail@romainbrault.com>
2020-01-14 09:30:28 +0100

    Add Pinball Loss (#579)
    * correction, typeError (#545)
    
    * correction, typeError
    
    * revise
    
    * add test
    
    * add typeerror
    
    * sanity indentation
    
    * get rid of the first for loop
    
    * test filter shape
    
    * remove unnecessary
    
    * minor
    
    Docs update for tensorflow.org (#578)
    
    * Simplify yaml files.
    
    * Add tensorflow.org buttons
    
    * switch imports to %tensorflow_version
    
    * switch tensorflow_version before installing addons
    
    * Switch to one of our images
    
    * Test-run in Colab
    
    * Add try/except for colab
    
    * verbose = 2
    
    * Use --no-deps
    
    * Update template, clear output
    
    * add download links
    
    pinball
    
    tests: alpha -> tau
    
    pinball
    
    tests: alpha -> tau
    
    list files
    
    passing loss tests
    
    passing all tests
    
    refs
    
    public API
    
    typo
    
    imports
    
    doc
    
    metric to mean
    
    pinball
    
    tests: alpha -> tau
    
    pinball
    
    tests: alpha -> tau
    
    list files
    
    passing loss tests
    
    passing all tests
    
    refs
    
    public API
    
    typo
    
    imports
    
    doc
    
    metric to mean
    
    pinball loss
    
    reformat
    
    removed tf.function
    
    pylint
    
    readme
    
    rename module + __init__
    
    rename module + __init__
    
    deletions
    
    correction, typeError (#545)
    
    * correction, typeError
    
    * revise
    
    * add test
    
    * add typeerror
    
    * sanity indentation
    
    * get rid of the first for loop
    
    * test filter shape
    
    * remove unnecessary
    
    * minor
    
    Docs update for tensorflow.org (#578)
    
    * Simplify yaml files.
    
    * Add tensorflow.org buttons
    
    * switch imports to %tensorflow_version
    
    * switch tensorflow_version before installing addons
    
    * Switch to one of our images
    
    * Test-run in Colab
    
    * Add try/except for colab
    
    * verbose = 2
    
    * Use --no-deps
    
    * Update template, clear output
    
    * add download links
    
    pinball
    
    tests: alpha -> tau
    
    pinball
    
    tests: alpha -> tau
    
    list files
    
    passing loss tests
    
    passing all tests
    
    refs
    
    public API
    
    typo
    
    imports
    
    doc
    
    metric to mean
    
    pinball
    
    tests: alpha -> tau
    
    pinball
    
    tests: alpha -> tau
    
    list files
    
    passing loss tests
    
    passing all tests
    
    refs
    
    public API
    
    typo
    
    imports
    
    doc
    
    metric to mean
    
    pinball loss
    
    reformat
    
    removed tf.function
    
    pylint
    
    readme
    
    rename module + __init__
    
    rename module + __init__
    
    deletions
    
    mrg
    
    mrg
    
    quantiles
    
    deps
    
    deps
    
    deletions
    
    deletions
    
    quantiles
    
    * quantiles
    
    * typo
    
    * typo
    
    * modifs
    
    * spacing
    
    * typo
    
    * typo
    
    * typo
    
    * code format
    
    * requires modifs
    
    * requires modifs
    
    * doc
    
    * tau
    
    * tau
    
    * tau
    
    * doc + tests

--- tensorflow_addons/losses/__init__.py
+++ tensorflow_addons/losses/__init__.py
@@ -24,6 +24,7 @@ from tensorflow_addons.losses.giou_loss import giou_loss, GIoULoss
 from tensorflow_addons.losses.lifted import lifted_struct_loss, LiftedStructLoss
 from tensorflow_addons.losses.sparsemax_loss import sparsemax_loss, SparsemaxLoss
 from tensorflow_addons.losses.triplet import triplet_semihard_loss, TripletSemiHardLoss
+from tensorflow_addons.losses.quantiles import pinball_loss, PinballLoss
 
 # Temporarily disable for windows
 # Remove after: https://github.com/tensorflow/addons/issues/838
