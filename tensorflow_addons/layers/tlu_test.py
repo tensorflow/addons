87d06633f922837b00ed8aadd99cf3ed7abfd70e
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-01-15 02:45:28 +0100

    Used pyupgrade on the layers. (#884)

--- tensorflow_addons/layers/tlu_test.py
+++ tensorflow_addons/layers/tlu_test.py
@@ -14,10 +14,6 @@
 # ==============================================================================
 """Tests for TLU activation."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 import tensorflow as tf
 from absl.testing import parameterized
