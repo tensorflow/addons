06d2a96315b29f442ae02a860ed89cb6e758497f
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-15 22:34:47 +0000

    Export env variables to profile.

--- tools/ci_testing/install_bazel_windows.sh
+++ tools/ci_testing/install_bazel_windows.sh
@@ -1,6 +1,5 @@
 set -e -x
 curl -sSOL https://github.com/bazelbuild/bazel/releases/download/${1}/bazel-${1}-windows-x86_64.exe
-echo export BAZEL_VC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/  >> ~/.bash_profile
-echo export BAZEL_PATH=/d/a/addons/addons/bazel-${1}-windows-x86_64.exe  >> ~/.bash_profile
-echo export PATH=/c/hostedtoolcache/windows/Python/${2}/x64/:$PATH  >> ~/.bash_profile
-echo environement $BASH_ENV
+echo export BAZEL_VC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/  >> ~/.profile
+echo export BAZEL_PATH=/d/a/addons/addons/bazel-${1}-windows-x86_64.exe  >> ~/.profile
+echo export PATH=/c/hostedtoolcache/windows/Python/${2}/x64/:$PATH  >> ~/.profile
