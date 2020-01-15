484db678a112c028094fa90262c5b0351c0bd00c
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-15 22:32:30 +0000

    Trying to get the bash env.

--- tools/ci_testing/install_bazel_windows.sh
+++ tools/ci_testing/install_bazel_windows.sh
@@ -3,3 +3,4 @@ curl -sSOL https://github.com/bazelbuild/bazel/releases/download/${1}/bazel-${1}
 echo export BAZEL_VC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/  >> ~/.bash_profile
 echo export BAZEL_PATH=/d/a/addons/addons/bazel-${1}-windows-x86_64.exe  >> ~/.bash_profile
 echo export PATH=/c/hostedtoolcache/windows/Python/${2}/x64/:$PATH  >> ~/.bash_profile
+echo environement $BASH_ENV
