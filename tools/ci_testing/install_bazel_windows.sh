e4514bbe0d2e1cd6897199c6ed3716ae8352b82b
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-15 22:23:45 +0000

    Added the variables in the bash_profile.

--- tools/ci_testing/install_bazel_windows.sh
+++ tools/ci_testing/install_bazel_windows.sh
@@ -1,5 +1,5 @@
 set -e -x
 curl -sSOL https://github.com/bazelbuild/bazel/releases/download/${1}/bazel-${1}-windows-x86_64.exe
-echo export BAZEL_VC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/  >>D:\a\_temp\msys\msys64\home\runneradmin\.bash_profile
-echo export BAZEL_PATH=/d/a/addons/addons/bazel-${1}-windows-x86_64.exe  >>D:\a\_temp\msys\msys64\home\runneradmin\.bash_profile
-echo export PATH=/c/hostedtoolcache/windows/Python/${2}/x64/:$PATH  >>D:\a\_temp\msys\msys64\home\runneradmin\.bash_profile
+echo export BAZEL_VC=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Enterprise/VC/  >> ~/.bash_profile
+echo export BAZEL_PATH=/d/a/addons/addons/bazel-${1}-windows-x86_64.exe  >> ~/.bash_profile
+echo export PATH=/c/hostedtoolcache/windows/Python/${2}/x64/:$PATH  >> ~/.bash_profile
