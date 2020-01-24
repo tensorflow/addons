d1009fa99162f624855ade929f2a944df4fbd1fa
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-24 13:30:18 +0000

    More extensive about types.

--- tensorflow_addons/utils/types.py
+++ tensorflow_addons/utils/types.py
@@ -19,8 +19,23 @@ from typing import Union, Callable, List
 import numpy as np
 import tensorflow as tf
 
+
+Number = Union[float,
+               int,
+               np.float16,
+               np.float32,
+               np.float64,
+               np.int8,
+               np.int16,
+               np.int32,
+               np.int64,
+               np.uint8,
+               np.uint16,
+               np.uint32,
+               np.uint64]
+
 Initializer = Union[None, dict, str, Callable]
 Regularizer = Union[None, dict, str, Callable]
 Constraint = Union[None, dict, str, Callable]
 
-TensorLike = Union[List[Union[float, int]], np.ndarray, tf.Tensor]
+TensorLike = Union[List[Union[Number, list]], Number, np.ndarray, tf.Tensor]
