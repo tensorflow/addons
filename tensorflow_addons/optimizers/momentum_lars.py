e87caed6faf8a695ddcb9f428b466ebb4283f9ae
Pankaj Kanwar <pkanwar@google.com>
2020-01-28 12:12:37 -0800

    formating changes and merge

--- tensorflow_addons/optimizers/momentum_lars.py
+++ tensorflow_addons/optimizers/momentum_lars.py
@@ -25,7 +25,8 @@ from tensorflow.python.training import training_ops
 import tensorflow.compat.v2 as tf
 from tensorflow_addons.utils import keras_utils
 
-@tf.keras.utils.register_keras_serializable(package='Addons')
+
+@tf.keras.utils.register_keras_serializable(package="Addons")
 class MomentumLARS(tf.keras.optimizers.Optimizer):
     """Layer-wise Adaptive Rate Scaling for large batch training.
 
@@ -44,20 +45,22 @@ class MomentumLARS(tf.keras.optimizers.Optimizer):
     tensors use the default momentum optimizer.
     """
 
-    def __init__(self,
-                 learning_rate,
-                  momentum=0.9,
-                  weight_decay=0.0001,
-                  # The LARS coefficient is a hyperparameter
-                  eeta=0.001,
-                  epsilon=0.0,
-                  name="LARSOptimizer",
-                  # Enable skipping variables from LARS scaling.
-                  skip_list=(),
-                  use_nesterov=False,
-                  clip=False,
-                  **kwargs):
-      """Construct a new LARS Optimizer.
+    def __init__(
+        self,
+        learning_rate,
+        momentum=0.9,
+        weight_decay=0.0001,
+        # The LARS coefficient is a hyperparameter
+        eeta=0.001,
+        epsilon=0.0,
+        name="LARSOptimizer",
+        # Enable skipping variables from LARS scaling.
+        skip_list=(),
+        use_nesterov=False,
+        clip=False,
+        **kwargs
+    ):
+        """Construct a new LARS Optimizer.
 
       Args:
           learning_rate: A `Tensor` or floating point value. The base learning rate.
@@ -85,114 +88,120 @@ class MomentumLARS(tf.keras.optimizers.Optimizer):
       Raises:
           ValueError: If a hyperparameter is set to a non-sensical value.
       """
-      if momentum < 0.0:
-        raise ValueError("momentum should be positive: %s" % momentum)
-      if weight_decay < 0.0:
-        raise ValueError("weight_decay should be positive: %s" % weight_decay)
-      super(LARSOptimizer, self).__init__(name=name, **kwargs)
+        if momentum < 0.0:
+            raise ValueError("momentum should be positive: %s" % momentum)
+        if weight_decay < 0.0:
+            raise ValueError("weight_decay should be positive: %s" % weight_decay)
+        super(LARSOptimizer, self).__init__(name=name, **kwargs)
 
-      self._skip_list = skip_list
-      self._set_hyper('learning_rate', learning_rate)
-      self._set_hyper('momentum', momentum)
-      self._set_hyper('weight_decay', weight_decay)
-      self._set_hyper('eeta', eeta)
-      self._set_hyper('epsilon', epsilon)
-      self._set_hyper('use_nesterov', use_nesterov)
-      self._set_hyper('use_clipping', clip)
+        self._skip_list = skip_list
+        self._set_hyper("learning_rate", learning_rate)
+        self._set_hyper("momentum", momentum)
+        self._set_hyper("weight_decay", weight_decay)
+        self._set_hyper("eeta", eeta)
+        self._set_hyper("epsilon", epsilon)
+        self._set_hyper("use_nesterov", use_nesterov)
+        self._set_hyper("use_clipping", clip)
 
     def get_config(self):
-      config = {
-          'learning_rate': self._serialize_hyperparameter('learning_rate'),
-          'momentum': self._serialize_hyperparameter('momentum'),
-          'weight_decay': self._serialize_hyperparameter('weight_decay'),
-          'eeta': self._serialize_hyperparameter('eeta'),
-          'epsilon': self._serialize_hyperparameter('epsilon'),
-          'use_nesterov': self._serialize_hyperparameter('use_nesterov'),
-          'clip': self._serialize_hyperparameter('use_clipping')
-      }
-      base_config = super(LARSOptimizer, self).get_config()
-      return dict(list(base_config.items()) + list(config.items()))
+        config = {
+            "learning_rate": self._serialize_hyperparameter("learning_rate"),
+            "momentum": self._serialize_hyperparameter("momentum"),
+            "weight_decay": self._serialize_hyperparameter("weight_decay"),
+            "eeta": self._serialize_hyperparameter("eeta"),
+            "epsilon": self._serialize_hyperparameter("epsilon"),
+            "use_nesterov": self._serialize_hyperparameter("use_nesterov"),
+            "clip": self._serialize_hyperparameter("use_clipping"),
+        }
+        base_config = super(LARSOptimizer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
     def _create_slots(self, var_list):
-      for var in var_list:
-        self.add_slot(var, 'momentum')
+        for var in var_list:
+            self.add_slot(var, "momentum")
 
     def compute_lr(self, grad, var):
-      if self._skip_list is None or not any(v in var.name
-                                            for v in self._skip_list):
-        w_norm = tf.linalg.norm(var, ord=2)
-        g_norm = tf.linalg.norm(grad, ord=2)
-        eeta = self._serialize_hyperparameter('eeta')
-        weight_decay = self._serialize_hyperparameter('weight_decay')
-        epsilon = self._serialize_hyperparameter('epsilon')
-        trust_ratio = array_ops.where_v2(
-            math_ops.greater(w_norm, 0),
-            array_ops.where_v2(
-                math_ops.greater(g_norm, 0),
-                (eeta * w_norm /
-                 (g_norm + weight_decay * w_norm + epsilon)), 1.0),
-            1.0)
-        scaled_lr = self._serialize_hyperparameter('learning_rate') * trust_ratio
-        # clip learning rate for LARC.
-        if self._serialize_hyperparameter('use_clipping'):
-          scaled_lr = min(scaled_lr, self._serialize_hyperparameter('learning_rate'))
+        if self._skip_list is None or not any(v in var.name for v in self._skip_list):
+            w_norm = tf.linalg.norm(var, ord=2)
+            g_norm = tf.linalg.norm(grad, ord=2)
+            eeta = self._serialize_hyperparameter("eeta")
+            weight_decay = self._serialize_hyperparameter("weight_decay")
+            epsilon = self._serialize_hyperparameter("epsilon")
+            trust_ratio = array_ops.where_v2(
+                math_ops.greater(w_norm, 0),
+                array_ops.where_v2(
+                    math_ops.greater(g_norm, 0),
+                    (eeta * w_norm / (g_norm + weight_decay * w_norm + epsilon)),
+                    1.0,
+                ),
+                1.0,
+            )
+            scaled_lr = self._serialize_hyperparameter("learning_rate") * trust_ratio
+            # clip learning rate for LARC.
+            if self._serialize_hyperparameter("use_clipping"):
+                scaled_lr = min(
+                    scaled_lr, self._serialize_hyperparameter("learning_rate")
+                )
 
-        # Add the weight regularization gradient
-        grad = grad + weight_decay * var
-      return scaled_lr, grad
+            # Add the weight regularization gradient
+            grad = grad + weight_decay * var
+        return scaled_lr, grad
 
     def _apply_dense(self, grad, var):
-      scaled_lr, grad = self.compute_lr(grad, var)
-      mom = self.get_slot(var, "momentum")
-      momentum = self._serialize_hyperparameter('momentum')
-      use_nesterov = bool(self._serialize_hyperparameter('use_nesterov'))
-      return training_ops.apply_momentum(
-          var,
-          mom,
-          math_ops.cast(1.0, var.dtype.base_dtype),
-          grad * scaled_lr,
-          momentum,
-          use_locking=False,
-          use_nesterov=use_nesterov)
+        scaled_lr, grad = self.compute_lr(grad, var)
+        mom = self.get_slot(var, "momentum")
+        momentum = self._serialize_hyperparameter("momentum")
+        use_nesterov = bool(self._serialize_hyperparameter("use_nesterov"))
+        return training_ops.apply_momentum(
+            var,
+            mom,
+            math_ops.cast(1.0, var.dtype.base_dtype),
+            grad * scaled_lr,
+            momentum,
+            use_locking=False,
+            use_nesterov=use_nesterov,
+        )
 
     def _resource_apply_dense(self, grad, var):
-      scaled_lr, grad = self.compute_lr(grad, var)
-      mom = self.get_slot(var, "momentum")
-      momentum = self._serialize_hyperparameter('momentum')
-      use_nesterov = bool(self._serialize_hyperparameter('use_nesterov'))
-      return training_ops.resource_apply_momentum(
-          var.handle,
-          mom.handle,
-          math_ops.cast(1.0, var.dtype.base_dtype),
-          grad * scaled_lr,
-          momentum,
-          use_locking=False,
-          use_nesterov=use_nesterov)
+        scaled_lr, grad = self.compute_lr(grad, var)
+        mom = self.get_slot(var, "momentum")
+        momentum = self._serialize_hyperparameter("momentum")
+        use_nesterov = bool(self._serialize_hyperparameter("use_nesterov"))
+        return training_ops.resource_apply_momentum(
+            var.handle,
+            mom.handle,
+            math_ops.cast(1.0, var.dtype.base_dtype),
+            grad * scaled_lr,
+            momentum,
+            use_locking=False,
+            use_nesterov=use_nesterov,
+        )
 
     # Fallback to momentum optimizer for sparse tensors
     def _apply_sparse(self, grad, var):
-      mom = self.get_slot(var, "momentum")
-      use_nesterov = bool(self._serialize_hyperparameter('use_nesterov'))
-      return training_ops.sparse_apply_momentum(
-          var,
-          mom,
-          math_ops.cast(self._learning_rate_tensor, var.dtype.base_dtype),
-          grad.values,
-          grad.indices,
-          math_ops.cast(self._momentum_tensor, var.dtype.base_dtype),
-          use_locking=False,
-          use_nesterov=use_nesterov).op
-
+        mom = self.get_slot(var, "momentum")
+        use_nesterov = bool(self._serialize_hyperparameter("use_nesterov"))
+        return training_ops.sparse_apply_momentum(
+            var,
+            mom,
+            math_ops.cast(self._learning_rate_tensor, var.dtype.base_dtype),
+            grad.values,
+            grad.indices,
+            math_ops.cast(self._momentum_tensor, var.dtype.base_dtype),
+            use_locking=False,
+            use_nesterov=use_nesterov,
+        ).op
 
     def _resource_apply_sparse(self, grad, var, indices):
-      mom = self.get_slot(var, "momentum")
-      use_nesterov = bool(self._serialize_hyperparameter('use_nesterov'))
-      return training_ops.resource_sparse_apply_momentum(
-          var.handle,
-          mom.handle,
-          math_ops.cast(self._learning_rate_tensor, grad.dtype),
-          grad,
-          indices,
-          math_ops.cast(self._momentum_tensor, grad.dtype),
-          use_locking=False,
-          use_nesterov=use_nesterov)
+        mom = self.get_slot(var, "momentum")
+        use_nesterov = bool(self._serialize_hyperparameter("use_nesterov"))
+        return training_ops.resource_sparse_apply_momentum(
+            var.handle,
+            mom.handle,
+            math_ops.cast(self._learning_rate_tensor, grad.dtype),
+            grad,
+            indices,
+            math_ops.cast(self._momentum_tensor, grad.dtype),
+            use_locking=False,
+            use_nesterov=use_nesterov,
+        )
--- tensorflow_addons/optimizers/momentum_lars_test.py
+++ tensorflow_addons/optimizers/momentum_lars_test.py
@@ -24,141 +24,144 @@ import numpy as np
 from tensorflow_addons.optimizers import momentum_lars as lo
 import tensorflow_addons.optimizers.conditional_gradient as cg_lib
 
+
 @test_utils.run_all_in_graph_and_eager_modes
 class MomentumLARSTest(tf.test.TestCase):
+    def testLARSGradientOneStep(self, use_resource=False, use_callable_params=False):
+        for dtype in [tf.float16, tf.float32, tf.float64]:
+            shape = [3, 3]
+            var_np = np.ones(shape)
+            grad_np = np.full((3, 3), 3)
+            lr_np = 0.1
+            m_np = 0.9
+            wd_np = 0.1
+            ep_np = 1e-5
+            eeta = 0.1
+            vel_np = np.zeros(shape)
 
-  def testLARSGradientOneStep(self, use_resource=False, use_callable_params=False):
-      for dtype in [tf.float16, tf.float32, tf.float64]:
-          shape = [3, 3]
-          var_np = np.ones(shape)
-          grad_np = np.full((3, 3), 3)
-          lr_np = 0.1
-          m_np = 0.9
-          wd_np = 0.1
-          ep_np = 1e-5
-          eeta = 0.1
-          vel_np = np.zeros(shape)
+            var = tf.Variable(var_np, dtype=dtype)
+            grad = tf.constant(grad_np, dtype=dtype)
+            opt = lo.LARSOptimizer(
+                learning_rate=lr_np,
+                momentum=m_np,
+                weight_decay=wd_np,
+                eeta=eeta,
+                epsilon=ep_np,
+            )
 
-          var = tf.Variable(var_np, dtype=dtype)
-          grad = tf.constant(grad_np, dtype=dtype)
-          opt = lo.LARSOptimizer(
-               learning_rate=lr_np,
-               momentum=m_np,
-               weight_decay=wd_np,
-               eeta=eeta,
-               epsilon=ep_np)
+            update = opt.apply_gradients([(grad, var)])
 
-          update = opt.apply_gradients([(grad, var)])
+            if not tf.executing_eagerly():
+                self.evaluate(tf.compat.v1.global_variables_initializer())
+                # Fetch params to validate initial values
+                var_tmp = np.ones(shape)
+                self.assertAllClose(var_tmp, self.evaluate(var))
 
-          if not tf.executing_eagerly():
-            self.evaluate(tf.compat.v1.global_variables_initializer())
-            # Fetch params to validate initial values
-            var_tmp = np.ones(shape)
-            self.assertAllClose(var_tmp, self.evaluate(var))
+            # Run 1 step of the optimizer.
+            self.evaluate(update)
+            post_var = self.evaluate(var)
 
-          # Run 1 step of the optimizer.
-          self.evaluate(update)
-          post_var = self.evaluate(var)
+            # Check we have slots
+            self.assertEqual(["momentum"], opt.get_slot_names())
+            slot0 = opt.get_slot(var, "momentum")
+            self.assertEqual(slot0.get_shape(), var.get_shape())
+            post_vel = self.evaluate(opt.get_slot(var, "momentum"))
 
-          # Check we have slots
-          self.assertEqual(["momentum"], opt.get_slot_names())
-          slot0 = opt.get_slot(var, "momentum")
-          self.assertEqual(slot0.get_shape(), var.get_shape())
-          post_vel = self.evaluate(opt.get_slot(var, 'momentum'))
+            w_norm = np.linalg.norm(var_np.flatten(), ord=2)
+            g_norm = np.linalg.norm(grad_np.flatten(), ord=2)
+            trust_ratio = eeta * w_norm / (g_norm + wd_np * w_norm + ep_np)
+            scaled_lr = lr_np * trust_ratio
+            grad_np = grad_np + wd_np * var_np
 
-          w_norm = np.linalg.norm(var_np.flatten(), ord=2)
-          g_norm = np.linalg.norm(grad_np.flatten(), ord=2)
-          trust_ratio = eeta * w_norm / (g_norm + wd_np * w_norm + ep_np)
-          scaled_lr = lr_np * trust_ratio
-          grad_np = grad_np + wd_np * var_np
+            vel_np = m_np * vel_np + scaled_lr * grad_np
+            var_np -= vel_np
 
-          vel_np = m_np * vel_np + scaled_lr * grad_np
-          var_np -= vel_np
+            self.assertAllClose(var_np, post_var, 1e-3, 1e-3)
+            self.assertAllClose(vel_np, post_vel, 1e-3, 1e-3)
 
-          self.assertAllClose(var_np, post_var, 1e-3, 1e-3)
-          self.assertAllClose(vel_np, post_vel, 1e-3, 1e-3)
+    def testLARSGradientMultiStep(self, use_resource=False, use_callable_params=False):
+        for dtype in [tf.float16, tf.float32, tf.float64]:
+            shape = [3, 3]
+            var_np = np.ones(shape)
+            grad_np = np.ones(shape)
+            lr_np = 0.1
+            m_np = 0.9
+            wd_np = 0.1
+            ep_np = 1e-5
+            eeta = 0.1
+            vel_np = np.zeros(shape)
+            iterations = 10
 
-  def testLARSGradientMultiStep(self, use_resource=False, use_callable_params=False):
-      for dtype in [tf.float16, tf.float32, tf.float64]:
-           shape = [3, 3]
-           var_np = np.ones(shape)
-           grad_np = np.ones(shape)
-           lr_np = 0.1
-           m_np = 0.9
-           wd_np = 0.1
-           ep_np = 1e-5
-           eeta = 0.1
-           vel_np = np.zeros(shape)
-           iterations = 10
+            var = tf.Variable(var_np, dtype=dtype)
+            grad = tf.Variable(grad_np, dtype=dtype)
+            opt = lo.LARSOptimizer(
+                learning_rate=lr_np,
+                momentum=m_np,
+                eeta=eeta,
+                weight_decay=wd_np,
+                epsilon=ep_np,
+            )
 
-           var = tf.Variable(var_np, dtype=dtype)
-           grad = tf.Variable(grad_np, dtype=dtype)
-           opt = lo.LARSOptimizer(
-               learning_rate=lr_np,
-               momentum=m_np,
-               eeta=eeta,
-               weight_decay=wd_np,
-               epsilon=ep_np)
+            if not tf.executing_eagerly():
+                self.evaluate(tf.compat.v1.global_variables_initializer())
+                # Fetch params to validate initial values
+                var_tmp = np.ones(shape)
+                self.assertAllClose(var_tmp, self.evaluate(var))
 
-           if not tf.executing_eagerly():
-             self.evaluate(tf.compat.v1.global_variables_initializer())
-             # Fetch params to validate initial values
-             var_tmp = np.ones(shape)
-             self.assertAllClose(var_tmp, self.evaluate(var))
+            # initialize the variables for eager mode.
+            if not tf.executing_eagerly():
+                update = opt.apply_gradients([(grad, var)])
+                self.evaluate(tf.compat.v1.global_variables_initializer())
 
-           # initialize the variables for eager mode.
-           if not tf.executing_eagerly():
-             update = opt.apply_gradients([(grad, var)])
-             self.evaluate(tf.compat.v1.global_variables_initializer())
+            for _ in range(iterations):
+                if tf.executing_eagerly():
+                    opt.apply_gradients([(grad, var)])
+                else:
+                    self.evaluate(update)
 
-           for _ in range(iterations):
-             if tf.executing_eagerly():
-               opt.apply_gradients([(grad, var)])
-             else:
-               self.evaluate(update)
+                post_var = self.evaluate(var)
 
-             post_var = self.evaluate(var)
+                # Check we have slots
+                self.assertEqual(["momentum"], opt.get_slot_names())
+                slot0 = opt.get_slot(var, "momentum")
+                self.assertEqual(slot0.get_shape(), var.get_shape())
+                post_vel = self.evaluate(opt.get_slot(var, "momentum"))
 
-             # Check we have slots
-             self.assertEqual(["momentum"], opt.get_slot_names())
-             slot0 = opt.get_slot(var, "momentum")
-             self.assertEqual(slot0.get_shape(), var.get_shape())
-             post_vel = self.evaluate(opt.get_slot(var, 'momentum'))
+                w_norm = np.linalg.norm(var_np.flatten(), ord=2)
+                g_norm = np.linalg.norm(grad_np.flatten(), ord=2)
+                trust_ratio = eeta * w_norm / (g_norm + wd_np * w_norm + ep_np)
+                scaled_lr = lr_np * trust_ratio
+                grad_np = grad_np + wd_np * var_np
 
-             w_norm = np.linalg.norm(var_np.flatten(), ord=2)
-             g_norm = np.linalg.norm(grad_np.flatten(), ord=2)
-             trust_ratio = eeta * w_norm / (g_norm + wd_np * w_norm + ep_np)
-             scaled_lr = lr_np * trust_ratio
-             grad_np = grad_np + wd_np * var_np
+                vel_np = m_np * vel_np + scaled_lr * grad_np
+                var_np -= vel_np
 
-             vel_np = m_np * vel_np + scaled_lr * grad_np
-             var_np -= vel_np
+                self.assertAllClose(var_np, post_var, 1e-3, 1e-3)
+                self.assertAllClose(vel_np, post_vel, 1e-3, 1e-3)
 
-             self.assertAllClose(var_np, post_var, 1e-3, 1e-3)
-             self.assertAllClose(vel_np, post_vel, 1e-3, 1e-3)
+    def testGetConfig(self, use_resource=False, use_callable_params=False):
+        lr_np = 0.1
+        m_np = 0.9
+        eeta = 0.1
+        wd_np = 0.1
+        ep_np = 1e-5
+        opt = lo.LARSOptimizer(
+            learning_rate=lr_np,
+            momentum=m_np,
+            eeta=eeta,
+            weight_decay=wd_np,
+            epsilon=ep_np,
+        )
 
-  def testGetConfig(self, use_resource=False, use_callable_params=False):
-    lr_np = 0.1
-    m_np = 0.9
-    eeta = 0.1
-    wd_np = 0.1
-    ep_np = 1e-5
-    opt = lo.LARSOptimizer(
-        learning_rate=lr_np,
-        momentum=m_np,
-        eeta=eeta,
-        weight_decay=wd_np,
-        epsilon=ep_np)
-
-    opt = tf.keras.optimizers.deserialize(tf.keras.optimizers.serialize(opt))
-    config = opt.get_config()
-    self.assertEqual(config['learning_rate'], 0.1)
-    self.assertEqual(config['momentum'], 0.9)
-    self.assertEqual(config['weight_decay'], 0.1)
-    self.assertEqual(config['eeta'], 0.1)
-    self.assertEqual(config['epsilon'], 1e-5)
-    self.assertEqual(config['use_nesterov'], False)
+        opt = tf.keras.optimizers.deserialize(tf.keras.optimizers.serialize(opt))
+        config = opt.get_config()
+        self.assertEqual(config["learning_rate"], 0.1)
+        self.assertEqual(config["momentum"], 0.9)
+        self.assertEqual(config["weight_decay"], 0.1)
+        self.assertEqual(config["eeta"], 0.1)
+        self.assertEqual(config["epsilon"], 1e-5)
+        self.assertEqual(config["use_nesterov"], False)
 
 
-if __name__ == '__main__':
-  tf.test.main()
+if __name__ == "__main__":
+    tf.test.main()
