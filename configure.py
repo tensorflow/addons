9202c85cdebde4b5ec8b0e81f6f5003683bd0db0
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-01-31 12:19:02 +0000

    Refactoring of configure.py

--- configure.py
+++ configure.py
@@ -33,10 +33,6 @@ _DEFAULT_CUDNN_PATH = "/usr/lib/x86_64-linux-gnu"
 
 _TFA_BAZELRC = ".bazelrc"
 _TF_NEED_CUDA = os.getenv("TF_NEED_CUDA")
-_TF_CUDA_VERSION = os.getenv("TF_CUDA_VERSION")
-_CUDA_TOOLKIT_PATH = os.getenv("CUDA_TOOLKIT_PATH")
-_TF_CUDNN_VERSION = os.getenv("TF_CUDNN_VERSION")
-_CUDNN_INSTALL_PATH = os.getenv("CUDNN_INSTALL_PATH")
 
 
 # Writes variables to bazelrc file
@@ -85,84 +81,92 @@ def generate_shared_lib_name(namespec):
         return namespec[1][3:]
 
 
-print()
-print("Configuring TensorFlow Addons to be built from source...")
-
-_PIP_INSTALL_OPTS = ["--upgrade"]
-parser = argparse.ArgumentParser()
-parser.add_argument("--quiet", action="store_true", help="Give less output.")
-parser.add_argument(
-    "--no-deps",
-    action="store_true",
-    help="Do not check and install Python dependencies.",
-)
-args = parser.parse_args()
-if args.quiet:
-    _PIP_INSTALL_OPTS.append("--quiet")
-
-_PYTHON_PATH = sys.executable
-with open("requirements.txt") as f:
-    _REQUIRED_PKG = f.read().splitlines()
-
-with open("build_deps/build-requirements.txt") as f:
-    _REQUIRED_PKG.extend(f.read().splitlines())
-
-print()
-if args.no_deps:
-    print("> Using pre-installed Tensorflow.")
-else:
-    print("> Installing", _REQUIRED_PKG)
-    install_cmd = [_PYTHON_PATH, "-m", "pip", "install"]
-    install_cmd.extend(_PIP_INSTALL_OPTS)
-    install_cmd.extend(_REQUIRED_PKG)
-    subprocess.check_call(install_cmd)
-
-if os.path.isfile(_TFA_BAZELRC):
-    os.remove(_TFA_BAZELRC)
-
-logging.disable(logging.WARNING)
-
-import tensorflow as tf  # noqa: E402 module level import not at top of file
-
-_TF_CFLAGS = tf.sysconfig.get_compile_flags()
-_TF_LFLAGS = tf.sysconfig.get_link_flags()
-_TF_CXX11_ABI_FLAG = tf.sysconfig.CXX11_ABI_FLAG
-
-_TF_SHARED_LIBRARY_NAME = generate_shared_lib_name(_TF_LFLAGS)
-_TF_HEADER_DIR = _TF_CFLAGS[0][2:]
-
-# OS Specific parsing
-if is_windows():
-    _TF_SHARED_LIBRARY_DIR = _TF_CFLAGS[0][2:-7] + "python"
-    _TF_SHARED_LIBRARY_DIR = _TF_SHARED_LIBRARY_DIR.replace("\\", "/")
-
-    _TF_HEADER_DIR = _TF_HEADER_DIR.replace("\\", "/")
-else:
-    _TF_SHARED_LIBRARY_DIR = _TF_LFLAGS[0][2:]
-
-write_action_env_to_bazelrc("TF_HEADER_DIR", _TF_HEADER_DIR)
-write_action_env_to_bazelrc("TF_SHARED_LIBRARY_DIR", _TF_SHARED_LIBRARY_DIR)
-write_action_env_to_bazelrc("TF_SHARED_LIBRARY_NAME", _TF_SHARED_LIBRARY_NAME)
-write_action_env_to_bazelrc("TF_CXX11_ABI_FLAG", _TF_CXX11_ABI_FLAG)
-
-write_to_bazelrc("build --spawn_strategy=standalone")
-write_to_bazelrc("build --strategy=Genrule=standalone")
-write_to_bazelrc("build -c opt")
-
-
-while _TF_NEED_CUDA is None:
+def main():
     print()
-    INPUT = get_input("Do you want to build GPU ops? [y/N] ")
-    if INPUT in ("Y", "y"):
-        print("> Building GPU & CPU ops")
-        _TF_NEED_CUDA = "1"
-    elif INPUT in ("N", "n", ""):
-        print("> Building only CPU ops")
-        _TF_NEED_CUDA = "0"
-    else:
-        print("Invalid selection: {}".format(INPUT))
+    print("Configuring TensorFlow Addons to be built from source...")
 
-if _TF_NEED_CUDA == "1":
+    _PIP_INSTALL_OPTS = ["--upgrade"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--quiet", action="store_true", help="Give less output.")
+    parser.add_argument(
+        "--no-deps",
+        action="store_true",
+        help="Do not check and install Python dependencies.",
+    )
+    args = parser.parse_args()
+    if args.quiet:
+        _PIP_INSTALL_OPTS.append("--quiet")
+
+    _PYTHON_PATH = sys.executable
+    with open("requirements.txt") as f:
+        _REQUIRED_PKG = f.read().splitlines()
+
+    with open("build_deps/build-requirements.txt") as f:
+        _REQUIRED_PKG.extend(f.read().splitlines())
+
+    print()
+    if args.no_deps:
+        print("> Using pre-installed Tensorflow.")
+    else:
+        print("> Installing", _REQUIRED_PKG)
+        install_cmd = [_PYTHON_PATH, "-m", "pip", "install"]
+        install_cmd.extend(_PIP_INSTALL_OPTS)
+        install_cmd.extend(_REQUIRED_PKG)
+        subprocess.check_call(install_cmd)
+
+    if os.path.isfile(_TFA_BAZELRC):
+        os.remove(_TFA_BAZELRC)
+
+    logging.disable(logging.WARNING)
+
+    import tensorflow as tf  # noqa: E402 module level import not at top of file
+
+    _TF_CFLAGS = tf.sysconfig.get_compile_flags()
+    _TF_LFLAGS = tf.sysconfig.get_link_flags()
+    _TF_CXX11_ABI_FLAG = tf.sysconfig.CXX11_ABI_FLAG
+
+    _TF_SHARED_LIBRARY_NAME = generate_shared_lib_name(_TF_LFLAGS)
+    _TF_HEADER_DIR = _TF_CFLAGS[0][2:]
+
+    # OS Specific parsing
+    if is_windows():
+        _TF_SHARED_LIBRARY_DIR = _TF_CFLAGS[0][2:-7] + "python"
+        _TF_SHARED_LIBRARY_DIR = _TF_SHARED_LIBRARY_DIR.replace("\\", "/")
+
+        _TF_HEADER_DIR = _TF_HEADER_DIR.replace("\\", "/")
+    else:
+        _TF_SHARED_LIBRARY_DIR = _TF_LFLAGS[0][2:]
+
+    write_action_env_to_bazelrc("TF_HEADER_DIR", _TF_HEADER_DIR)
+    write_action_env_to_bazelrc("TF_SHARED_LIBRARY_DIR", _TF_SHARED_LIBRARY_DIR)
+    write_action_env_to_bazelrc("TF_SHARED_LIBRARY_NAME", _TF_SHARED_LIBRARY_NAME)
+    write_action_env_to_bazelrc("TF_CXX11_ABI_FLAG", _TF_CXX11_ABI_FLAG)
+
+    write_to_bazelrc("build --spawn_strategy=standalone")
+    write_to_bazelrc("build --strategy=Genrule=standalone")
+    write_to_bazelrc("build -c opt")
+
+    if _TF_NEED_CUDA:
+        print()
+        answer = get_input("Do you want to build GPU ops? [y/N] ")
+        if answer in ("Y", "y"):
+            print("> Building GPU & CPU ops")
+            configure_gpu_setup()
+        elif answer in ("N", "n", ""):
+            print("> Building only CPU ops")
+        else:
+            raise SyntaxError("Invalid selection: {}".format(answer))
+
+    print()
+    print("Build configurations successfully written to {}".format(_TFA_BAZELRC))
+    print()
+
+
+def configure_gpu_setup():
+    _TF_CUDA_VERSION = os.getenv("TF_CUDA_VERSION")
+    _CUDA_TOOLKIT_PATH = os.getenv("CUDA_TOOLKIT_PATH")
+    _TF_CUDNN_VERSION = os.getenv("TF_CUDNN_VERSION")
+    _CUDNN_INSTALL_PATH = os.getenv("CUDNN_INSTALL_PATH")
     print()
     print("Configuring GPU setup...")
 
@@ -206,7 +210,7 @@ if _TF_NEED_CUDA == "1":
     print("> cuDNN installation path: {}".format(_CUDNN_INSTALL_PATH))
     print()
 
-    write_action_env_to_bazelrc("TF_NEED_CUDA", _TF_NEED_CUDA)
+    write_action_env_to_bazelrc("TF_NEED_CUDA", "1")
     write_action_env_to_bazelrc("CUDA_TOOLKIT_PATH", _CUDA_TOOLKIT_PATH)
     write_action_env_to_bazelrc("CUDNN_INSTALL_PATH", _CUDNN_INSTALL_PATH)
     write_action_env_to_bazelrc("TF_CUDA_VERSION", _TF_CUDA_VERSION)
@@ -220,14 +224,6 @@ if _TF_NEED_CUDA == "1":
     write_to_bazelrc(
         "build:cuda --crosstool_top=@local_config_cuda//crosstool:toolchain"
     )
-
-print()
-print("Build configurations successfully written to {}".format(_TFA_BAZELRC))
-print()
-
-
-def main():
-    pass
 
 
 if __name__ == "__main__":
