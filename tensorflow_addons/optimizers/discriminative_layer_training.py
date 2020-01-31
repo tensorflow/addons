0fefcdf325e96681c92730d081335b4f1d04ceb3
Hongya <hong.meng.yang@outlook.com>
2020-01-31 18:59:11 -0500

    updated documentation explicitly defined serialization as not supported

--- tensorflow_addons/optimizers/discriminative_layer_training.py
+++ tensorflow_addons/optimizers/discriminative_layer_training.py
@@ -19,16 +19,11 @@ import numpy as np
 from typeguard import typechecked
 
 
-# @tf.keras.utils.register_keras_serializable(package="Addons") :TODO figure out why other classes have this wrapper
-
-
 class ModelManager:
     """Class for grouping functions related to model lr_mult management"""
 
     def _get_layers(self, layer):
         """Helper method to access a layer's sublayers as a list or return an empty list
-        :TODO finish docstring
-
         """
 
         try:
@@ -39,7 +34,6 @@ class ModelManager:
     def _get_lr_mult(self, layer):
 
         """Helper method to access a layer's learning rate multiplier, which defaults to 1 if lr mult is not set
-        :TODO finish docstring
         """
 
         try:
@@ -50,7 +44,6 @@ class ModelManager:
     def _assign_lr_mult(self, layer, lr_mult, override=False):
 
         """Helper method to assign a layer's learning rate multiplier, which does nothing if lr mult is already set
-        :TODO finish docstring
         """
 
         try:
@@ -66,8 +59,6 @@ class ModelManager:
         tbh I can't properly explain how this works so see this post
 
         https://stackoverflow.com/questions/6340351/iterating-through-list-of-list-in-python
-        :TODO finish docstring
-
         """
 
         mult = self._get_lr_mult(layer)
@@ -89,7 +80,6 @@ class ModelManager:
 
     def _apply_lr_mult_to_var(self, layer):
         """Helper method to apply the lr mult to the trainable variables of a layer
-        :TODO finish docstring
         """
 
         lr_mult = self._get_lr_mult(layer)
@@ -100,11 +90,8 @@ class ModelManager:
                 lr_mult  # easier to check vars lr mult in graph and eager
             )
 
-    # :TODO float16 testing? not sure what would happen atm
-
     def _check_for_lr_mult(self, layer, verbose=True, propagate=True):
         """Identify which layers have an LR mult not equal to 1
-        :TODO finish docstring
         """
 
         layers_with_lr_mult = []
@@ -116,32 +103,27 @@ class ModelManager:
             if lr_mult != 1.0:
                 layers_with_lr_mult.append(sub_layer)
                 if verbose:
-                    # :TODO this should be info
-                    print("layer %s lr_mult : %f" % (sub_layer.name, lr_mult))
+                    tf.print("layer %s lr_mult : %f" % (sub_layer.name, lr_mult))
 
         return layers_with_lr_mult
 
     def _compute_params(self, var_list):
         """helps compute params to provide a summary that aligns with model.summary()
-        :TODO finish docstring
         """
         return np.sum([np.prod(list(var.shape)) for var in var_list])
 
     def _prepare_model(self, model, verbose=True):
         """Prepares a built model for disc training
-        :TODO finish docstring
         """
-        # :TODO add checks to ensure model is built
 
         layers_with_lr_mult = self._check_for_lr_mult(model, verbose=verbose)
         if len(layers_with_lr_mult) == 0:
-            # :TODO this should be a warning
 
-            print(
+            tf.print(
                 "Discriminative Layer Training requires an lr_mult attribute on at least one layer"
             )
 
-            print(
+            tf.print(
                 "The assigned lr_mult must not be equal to 1. eg: model.layers[0].lr_mult = 0.01"
             )
 
@@ -152,9 +134,8 @@ class ModelManager:
             var for var in model.trainable_variables if var.lr_mult_value != 1.0
         ]
 
-        # :TODO this should be info
         if verbose:
-            print(
+            tf.print(
                 "%i params of %i will learn at a different rate"
                 % (
                     self._compute_params(vars_with_lr_mult),
@@ -163,64 +144,11 @@ class ModelManager:
             )
 
 
-# :TODO disable all other methods bc this is wrapper
-# notimplementedreason = '''Optimizer Wrappers only implement minimize, _compute_gradients, apply_gradients, and get_config'''
-
-
 class DiscriminativeWrapper(tf.keras.optimizers.Optimizer):
-    """Discriminative Layer Training Wrapper
-
-    Discriminative layer training is a technique that applies different learning rates to
-    different layers in a model. Generally, a lower learning rate is applied to the
-    layers closest to the input and a higher learning rate is applied to layers closer
-    to the output. This method helps in transfer learning by quickly calibrating the head
-    of a model while preserving the useful weights in the main part of the model.
-
-    You should assign the lr_mult attribute to a layer. This will multiply the learning rate
-    used by the base optimizer for that layer.
-
-    This method creates a copy of the base optimizer for each unique learning rate multipler.
-
-    Performance is similar to using a single copy of the base optimizer as gradients are computed
-    only once and then passed on.
-
-    Example usage
-        model = tf.keras.Sequential()
-        model.add(tf.keras.applications.resnet.ResNet50(include_top = False, pooling = 'avg'))
-        model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
-        model.layers[0].lr_mult = 0.01
-        opt = DiscriminativeWrapper(tf.keras.optimizers.Adam, model, learning_rate = 0.01)
-        model.compile(loss = tf.keras.losses.BinaryCrossentropy, optimizer = opt)
-        model.fit(x, y)
-
-    Arguments
-        base_optimizer: a class that inherits from tf.keras.optimizers.Optimizer. Do not
-            pass an instance of the class.
-
-        model: tf.keras.Model, The model to be used for discriminative learning.
-            It should have at least 1 layer with the attribute lr_mult. The lr_mult should
-            be set to a value not equal to 1. Otherwise, you will have the exact same
-            result as not using discriminative learning.
-
-        learning_rate: float, the learning rate for the model
-
-        verbose: Bool, to generate a report on how many parameters are affected
-
-        *args: Args to pass to the base optimizer
-
-        **kwargs: Kwargs to pass to the base optimizer
-
-    Returns
-        Optimizer - A keras optimizer
-
-    References
-        - [Universal Language Model Fine-tuning for Text Classification](https://arxiv.org/pdf/1801.06146.pdf)
-    """
-
     @typechecked
     def __init__(
         self,
-        base_optimizer,
+        base_optimizer: object,
         model: tf.keras.Model,
         learning_rate: float,
         verbose: bool = True,
@@ -228,6 +156,60 @@ class DiscriminativeWrapper(tf.keras.optimizers.Optimizer):
         *args,
         **kwargs
     ):
+        """Discriminative Layer Training Wrapper
+
+            Discriminative layer training is a technique that applies different learning rates to
+            different layers in a model. Generally, a lower learning rate is applied to the
+            layers closest to the input and a higher learning rate is applied to layers closer
+            to the output. This method helps in transfer learning by quickly calibrating the head
+            of a model while preserving the useful weights in the main part of the model.
+
+            You should assign the lr_mult attribute to a layer. This will multiply the learning rate
+            used by the base optimizer for that layer.
+
+            This method creates a copy of the base optimizer for each unique learning rate multipler.
+
+            Performance is similar to using a single copy of the base optimizer as gradients are computed
+            only once and then passed on.
+
+            This optimizer does not support from_config or get_config. To try to preserve the state, you may
+            serialize the optimizers in the optimizer_group attribute in an instance of this class.
+
+            Example usage
+                model = tf.keras.Sequential()
+                model.add(tf.keras.applications.resnet.ResNet50(include_top = False, pooling = 'avg'))
+                model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
+                model.layers[0].lr_mult = 0.01
+                opt = DiscriminativeWrapper(tf.keras.optimizers.Adam, model, learning_rate = 0.01)
+                model.compile(loss = tf.keras.losses.BinaryCrossentropy, optimizer = opt)
+                model.fit(x, y)
+
+            Arguments
+                base_optimizer: a class that inherits from tf.keras.optimizers.Optimizer. Do not
+                    pass an instance of the class.
+
+                model: tf.keras.Model, The model to be used for discriminative learning.
+                    It should have at least 1 layer with the attribute lr_mult. The lr_mult should
+                    be set to a value not equal to 1. Otherwise, you will have the exact same
+                    result as not using discriminative learning.
+
+                learning_rate: float, the learning rate for the model
+
+                verbose: Bool, to generate a report on how many parameters are affected
+
+                *args: Args to pass to the base optimizer
+
+                **kwargs: Kwargs to pass to the base optimizer
+
+            Returns
+                Optimizer - A keras optimizer to use with model.compile
+
+            References
+                - [Universal Language Model Fine-tuning for Text Classification](https://arxiv.org/pdf/1801.06146.pdf)
+        """
+        assert issubclass(
+            base_optimizer, tf.keras.optimizers.Optimizer
+        ), "Base optimizer must be a class that inherits from tf.keras.optimizers.Optimizer"
 
         super().__init__(lr=learning_rate, name=name, *args, **kwargs)
 
@@ -246,7 +228,9 @@ class DiscriminativeWrapper(tf.keras.optimizers.Optimizer):
             self.optimizer_group.append(opt)
 
     def apply_gradients(self, grads_and_vars, name=None):
-        # :TODO docstring
+        """allocates gradients to each optimizer based on the variable's learning rate multiplier
+        then applies the gradients. In graph mode, it returns 1 operation per optimizer
+        """
 
         # create gradvar buckets for each opt
         gvdict = {}
@@ -264,5 +248,7 @@ class DiscriminativeWrapper(tf.keras.optimizers.Optimizer):
         ]
 
     def get_config(self):
-        # :TODO determine appropriate config
-        pass
+        raise NotImplementedError("Optimizer wrapper does not support get config")
+
+    def from_config(self):
+        raise NotImplementedError("Optimizer wrapper does not support from config")
