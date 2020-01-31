3371a2a05989b4532ae374ac6d2f51e7c3fb7c76
autoih <ihjhuo@ibm.com>
2020-01-31 05:51:55 -0800

    typing functions

--- tensorflow_addons/image/distance_transform.py
+++ tensorflow_addons/image/distance_transform.py
@@ -17,13 +17,17 @@
 import tensorflow as tf
 from tensorflow_addons.image import utils as img_utils
 from tensorflow_addons.utils.resource_loader import LazySO
+from tensorflow_addons.utils.types import FloatTensorLike, TensorLike
+
+from typing import Optional
 
 _image_so = LazySO("custom_ops/image/_image_ops.so")
 
 tf.no_gradient("Addons>EuclideanDistanceTransform")
 
 
-def euclidean_dist_transform(images, dtype=tf.float32, name=None):
+def euclidean_dist_transform(images: TensorLike, dtype: FloatTensorLike = tf.float32,
+                             name: Optional[str] = None) -> tf.Tensor:
     """Applies euclidean distance transform(s) to the image(s).
 
     Args:
