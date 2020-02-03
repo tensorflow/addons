806cadf72c551e9044a800924860630ac2806ad3
tanzhenyu <tanzheny@google.com>
2020-02-03 07:02:58 -0800

    more indentation fix
    More indentitation fix.

--- tensorflow_addons/layers/polynomial_test.py
+++ tensorflow_addons/layers/polynomial_test.py
@@ -32,12 +32,12 @@ class PolynomialCrossingTest(tf.test.TestCase):
         layer([x0, x])
 
     def test_invalid_proj_dim(self):
-        with self.assertRaisesRegexp(ValueError, r'is not supported yet'):
+        with self.assertRaisesRegexp(ValueError, r"is not supported yet"):
             x0 = np.random.random((12, 5))
             x = np.random.random((12, 5))
             layer = PolynomialCrossing(projection_dim=6)
             layer([x0, x])
 
 
-if __name__ == '__main__':
-  tf.test.main()
+if __name__ == "__main__":
+    tf.test.main()
