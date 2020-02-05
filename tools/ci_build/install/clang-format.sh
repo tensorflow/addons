b8477fef43cc30587493c32f698212299da8d4aa
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-02-05 15:38:02 +0100

    New pre-commit running black/buildifier/clang-format. (#1022)
    * Made a pre-commit hook in python.
    * Use buildkit when possible.
    * Added the buildifier.sh

--- tools/ci_build/install/clang-format.sh
+++ tools/ci_build/install/clang-format.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,18 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-.PHONY: all
 
-all: code-format sanity-check unit-test
 
-install-ci-dependency:
-	bash tools/ci_build/install/install_ci_dependency.sh
-
-code-format:
-	bash tools/pre-commit.sh
-
-unit-test:
-	bash tools/ci_testing/addons_cpu.sh
-
-gpu-unit-test:
-	bash tools/ci_testing/addons_gpu.sh
+wget -O /usr/local/bin/clang-format-9 https://github.com/DoozyX/clang-format-lint-action/raw/master/clang-format/clang-format9
+chmod +x /usr/local/bin/clang-format-9
