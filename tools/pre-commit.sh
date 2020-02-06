3ce1bb040a45a8cc83e6e16408692eb7786d6b72
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-06 17:39:39 +0100

    Clean-up of the docker tools for local dev.

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -3,5 +3,5 @@
 set -e
 
 export DOCKER_BUILDKIT=1
-docker build -t tf_addons_formatting -f tools/docker/Dockerfile_formatting .
+docker build -t tf_addons_formatting -f tools/docker/formatting.Dockerfile .
 docker run --rm -t -v "$(pwd -P):/addons" tf_addons_formatting
