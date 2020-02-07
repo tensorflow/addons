af288916bd42bc2105c57db3f01431cfd6004a57
autoih <ihjhuo@ibm.com>
2020-02-07 15:10:39 -0800

    minor

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-DOCKER_BUILDKIT=1 docker build -t tf_addons_formatting -f tools/docker/Dockerfile_formatting .
+export DOCKER_BUILDKIT=1
+docker build -t tf_addons_formatting -f tools/docker/Dockerfile_formatting .
 docker run --rm -t -v "$(pwd -P):/addons" tf_addons_formatting
