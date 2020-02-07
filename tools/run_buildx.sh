cc0de6b9e91f90eb5a5293c08a8fdbf0cf699109
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-07 21:46:59 +0000

    The env variable doesn't work with their version.

--- tools/run_buildx.sh
+++ tools/run_buildx.sh
@@ -1,6 +1,5 @@
 set -e
 
-export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx build \
     -f tools/docker/sanity_check.Dockerfile \
     --cache-from=type=registry,ref=gabrieldemarmiesse/caching-stuff \
