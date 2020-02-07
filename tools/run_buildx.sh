20e099c564cf2dcb4900ef8bc2abdb9cea01f033
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-07 21:38:12 +0000

    Use the env variable to go faster.

--- tools/run_buildx.sh
+++ tools/run_buildx.sh
@@ -1,5 +1,6 @@
 set -e
 
+export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx build \
     -f tools/docker/sanity_check.Dockerfile \
     --cache-from=type=registry,ref=gabrieldemarmiesse/caching-stuff \
