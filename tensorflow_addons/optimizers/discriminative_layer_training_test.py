9f19a6319450465a7874836a7d1f9c88a92b86db
Hongya <hong.meng.yang@outlook.com>
2020-02-11 12:50:12 -0500

    added missing functions in prep for tests

--- tensorflow_addons/optimizers/discriminative_layer_training_test.py
+++ tensorflow_addons/optimizers/discriminative_layer_training_test.py
@@ -298,8 +298,10 @@ def generate_tests(devices):
 
 
 if __name__ == "__main__":
+    # save models so weights are always the same
+    toy_cnn(first_run=True)
+    toy_rnn(first_run=True)
+
     devices = test_utils.create_virtual_devices(2)
     generate_tests(devices)
-    #     DiscriminativeLearningTest()._run_tests_in_notebook()
-    #     print("done")
     tf.test.main()
