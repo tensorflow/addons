6629cde371bfea866cf0962a8877d70ce6ee7d18
Sean Morgan <seanmorgan@outlook.com>
2020-02-12 01:30:39 -0500

    Update to optionally run gpu tests without buildkit (#1081)
    * Update to optionally run without buildkit
    
    * Remove run docker script

--- tools/run_gpu_tests.sh
+++ tools/run_gpu_tests.sh
@@ -1,5 +1,17 @@
-set -e
+# usage: bash tools/run_gpu_tests.sh [--no-deps]
 
-export DOCKER_BUILDKIT=1
+set -x -e
+
+if [ "$1" != "--no-buildkit" ] && [ "$1" != "" ]; then
+  echo Wrong argument $1
+  exit 1
+fi
+
+if [ "$1" == "--no-buildkit" ]; then
+  export DOCKER_BUILDKIT=0
+else
+  export DOCKER_BUILDKIT=1
+fi
+
 docker build -f tools/docker/gpu_tests.Dockerfile -t tfa_gpu_tests ./
 docker run --rm -t --runtime=nvidia tfa_gpu_tests
