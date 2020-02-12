19196f2eae97aaf18bec865625c33098d1f6c7eb
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-02-12 12:41:16 +0100

    Respect the DOCKER_BUILDKIT env variable in all scripts. (#1082)
    * Respect the DOCKER_BUILDKIT env variable.
    * Instructions in the pre-commit.

--- tools/run_sanity_check.sh
+++ tools/run_sanity_check.sh
@@ -1,2 +1,11 @@
+# usage: bash tools/run_sanity_check.sh
+# by default uses docker buildkit.
+# to disable it:
+# DOCKER_BUILDKIT=0 bash tools/run_sanity_check.sh
 set -e
-DOCKER_BUILDKIT=1 docker build -f tools/docker/sanity_check.Dockerfile ./
+
+if [ "$DOCKER_BUILDKIT" == "" ]; then
+  export DOCKER_BUILDKIT=1
+fi
+
+docker build -f tools/docker/sanity_check.Dockerfile ./
