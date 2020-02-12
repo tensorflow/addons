f3b36290b9d7c5fbc955970444769de3343b49bc
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-12 07:49:31 +0100

    Respect the DOCKER_BUILDKIT env variable.

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-export DOCKER_BUILDKIT=1
+if [ "$DOCKER_BUILDKIT" == "" ]; then
+  export DOCKER_BUILDKIT=1
+fi
+
 docker build -t tf_addons_formatting -f tools/docker/pre-commit.Dockerfile .
 docker run --rm -t -v "$(pwd -P):/addons" tf_addons_formatting
