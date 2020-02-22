7641e809d1a214b6bef807c39b20b119c9d66d08
public <975759105@qq.com>
2020-02-22 11:41:30 +0800

    fix timeout problem

--- tensorflow_addons/layers/equi_conv_test.py
+++ tensorflow_addons/layers/equi_conv_test.py
@@ -7,14 +7,14 @@ from tensorflow_addons.utils import test_utils
 @test_utils.run_all_in_graph_and_eager_modes
 class EquiConvTest(tf.test.TestCase):
     def testKerasNHWC(self):
-        input = tf.ones(shape=[16, 400, 400, 3])
-        layer = EquiConv(64, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_last")
+        input = tf.ones(shape=[1, 10, 10, 3])
+        layer = EquiConv(16, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_last")
         res = layer(input)
         self.assertAllEqual(tf.shape(input), tf.shape(res))
 
     def testKerasNCHW(self):
-        input = tf.ones(shape=[16, 3, 400, 400])
-        layer = EquiConv(64, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_first")
+        input = tf.ones(shape=[1, 3, 10, 10])
+        layer = EquiConv(16, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_first")
         res = layer(input)
         self.assertAllEqual(tf.shape(input), tf.shape(res))
 
