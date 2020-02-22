663622a6c3f394d848d57641c107f521ff0f41a1
public <975759105@qq.com>
2020-02-22 11:55:06 +0800

    fix test problem

--- tensorflow_addons/layers/equi_conv_test.py
+++ tensorflow_addons/layers/equi_conv_test.py
@@ -7,14 +7,20 @@ from tensorflow_addons.utils import test_utils
 @test_utils.run_all_in_graph_and_eager_modes
 class EquiConvTest(tf.test.TestCase):
     def testKerasNHWC(self):
-        input = tf.ones(shape=[1, 10, 10, 3])
-        layer = EquiConv(16, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_last")
+        channel = 32
+        input = tf.ones(shape=[1, 10, 10, channel])
+        layer = EquiConv(
+            channel, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_last"
+        )
         res = layer(input)
         self.assertAllEqual(tf.shape(input), tf.shape(res))
 
     def testKerasNCHW(self):
-        input = tf.ones(shape=[1, 3, 10, 10])
-        layer = EquiConv(16, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_first")
+        channel = 32
+        input = tf.ones(shape=[1, channel, 10, 10])
+        layer = EquiConv(
+            channel, (3, 3), 1, 1, (1, 1), 1, False, "same", "channels_first"
+        )
         res = layer(input)
         self.assertAllEqual(tf.shape(input), tf.shape(res))
 
