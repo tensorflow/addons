9d3e9a4c9ce453311ed33254295fe1213dcc9b65
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-27 16:26:49 +0100

    Seems to work.

--- .github/workflows/notify_codeowners/notify_codeowners.py
+++ .github/workflows/notify_codeowners/notify_codeowners.py
@@ -11,12 +11,12 @@ import os
 
 def xor_strings(a, b):
     result = int(a, 16) ^ int(b, 16)
-    return '{:x}'.format(result)
+    return "{:x}".format(result)
 
 
 def get_github_client():
-    bot_token = '1353d990cdb8b8ceb1b73d301dce83cc0da3db29'
-    bot_token_key = 'a1b2c3d47311f8e29e204f85a81b4df4a44e252c'
+    bot_token = "1353d990cdb8b8ceb1b73d301dce83cc0da3db29"
+    bot_token_key = "a1b2c3d47311f8e29e204f85a81b4df4a44e252c"
 
     return github.Github(xor_strings(bot_token, bot_token_key))
 
@@ -25,34 +25,36 @@ CLIENT = get_github_client()
 
 
 def check_user(user: str, line_idx: int):
-    if user[0] != '@':
+    if user[0] != "@":
         raise ValueError(
             f"User '{user}' at line {line_idx} of CODEOWNERS "
-            f"doesn't start with '@' ")
+            f"doesn't start with '@' "
+        )
     user = user[1:]
-    if '/' not in user:
+    if "/" not in user:
         # it's a real user. Check for typos.
         try:
             CLIENT.get_user(user)
         except github.UnknownObjectException:
-            raise KeyError(f"User '{user}' line {line_idx} does not exist. "
-                           f"Did you make a typo?")
+            raise KeyError(
+                f"User '{user}' line {line_idx} does not exist. "
+                f"Did you make a typo?"
+            )
     return user
 
 
 def check_pattern(pattern: str, line_idx: int):
-    if pattern[0] == '/':
+    if pattern[0] == "/":
         pattern = pattern[1:]
 
     pattern = Pattern(pattern)
 
-    addons_dir = Path('/addons')
-    if addons_dir.exists():
-        if not pattern.match_in_dir(addons_dir):
-            raise FileNotFoundError(
-                f"'{pattern.string}' present in CODEOWNERS line"
-                f" {line_idx} does not match any file in the repository. "
-                f"Did you make a typo?")
+    if not pattern.match_in_dir("."):
+        raise FileNotFoundError(
+            f"'{pattern.string}' present in CODEOWNERS line"
+            f" {line_idx} does not match any file in the repository. "
+            f"Did you make a typo?"
+        )
 
     return pattern
 
@@ -60,7 +62,7 @@ def check_pattern(pattern: str, line_idx: int):
 class Pattern:
     def __init__(self, pattern: str):
         self.string = pattern
-        if '*' in self.string:
+        if "*" in self.string:
             self.regex = re.compile(fnmatch.translate(pattern))
         else:
             self.regex = None
@@ -72,7 +74,7 @@ class Pattern:
             return file.startswith(self.string)
 
     def match_in_dir(self, directory):
-        list_files = glob.glob(directory + '/**/*', recursive=True)
+        list_files = glob.glob(f"{directory}/**/*", recursive=True)
         list_files = [os.path.relpath(x, directory) for x in list_files]
         matching_files = list(filter(self.match, list_files))
         return bool(matching_files)
@@ -86,11 +88,11 @@ def parse_codeowners(text: str) -> CodeOwners:
 
     for i, line in enumerate(text.splitlines()):
         line = line.strip()
-        if line == '':
+        if line == "":
             continue
-        if line[0] == '#':  # comment
+        if line[0] == "#":  # comment
             continue
-        elements = list(filter(lambda x: x != '', line.split(' ')))
+        elements = list(filter(lambda x: x != "", line.split(" ")))
 
         pattern = check_pattern(elements[0], i)
         users = [check_user(user, i) for user in elements[1:]]
@@ -102,40 +104,43 @@ def parse_codeowners(text: str) -> CodeOwners:
 nice_message = """
 
 You are owners of some files modified in this pull request.
-Would you kindly review them? Thank you very much. 
+Would you kindly review the changes whenever you have the time to?
+Thank you very much. 
 """
 
 
-def notify_codeowners(codeowners: CodeOwners, pull_request_id: int, dry_run: bool):
-    pull_request = CLIENT.get_repo('tensorflow/addons').get_pull(pull_request_id)
+def craft_message(codeowners: CodeOwners, pull_request):
 
     owners = set()
-    for file in pull_request.files():
+    for file in pull_request.get_files():
         for pattern, users in codeowners:
-            if not pattern.match(file):
+            if not pattern.match(file.filename):
                 continue
             owners.update(users)
 
-    owners = [f'@{owner}' for owner in owners]
-    full_message = ' '.join(owners + [nice_message])
-    print(full_message)
+    owners = [f"@{owner}" for owner in owners]
+    return " ".join(owners + [nice_message])
 
 
 @click.command()
-@click.option('--pull-request-id', default=None)
-@click.option('--dry-run', is_flag=True)
-@click.argument('file')
+@click.option("--pull-request-id")
+@click.option("--dry-run", is_flag=True)
+@click.argument("file")
 def notify_codeowners(pull_request_id, dry_run, file):
-    if file.startswith('http'):
-        text = urllib.request.urlopen(file).read().decode('utf-8')
+    pull_request_id = int(pull_request_id)
+    if file.startswith("http"):
+        text = urllib.request.urlopen(file).read().decode("utf-8")
     else:
         text = Path(file).read_text()
-
     codeowners = parse_codeowners(text)
 
     if pull_request_id:
-        notify_codeowners(codeowners, pull_request_id, dry_run)
+        pull_request = CLIENT.get_repo("tensorflow/addons").get_pull(pull_request_id)
+        msg = craft_message(codeowners, pull_request)
+        print(msg)
+        if not dry_run:
+            pull_request.create_issue_comment(msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     notify_codeowners()
