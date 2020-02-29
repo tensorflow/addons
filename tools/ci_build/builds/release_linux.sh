b4e249bbbfc0d21bf29448ef2787544d074b1dc9
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-02-29 15:11:00 +0000

    Symlinked python3 too.

--- tools/ci_build/builds/release_linux.sh
+++ tools/ci_build/builds/release_linux.sh
@@ -30,6 +30,7 @@ export TF_CUDNN_VERSION="7"
 export CUDNN_INSTALL_PATH="/usr/lib/x86_64-linux-gnu"
 
 ln -sf $(which python$1) /usr/bin/python
+ln -sf $(which python$1) /usr/bin/python3
 python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
 
