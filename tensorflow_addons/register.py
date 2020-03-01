c16f9ef794d44ef95e26165fc4b4e9b50104ab12
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-01 12:36:19 +0000

    Added some tests.

--- tensorflow_addons/register.py
+++ tensorflow_addons/register.py
@@ -1,8 +1,5 @@
-import inspect
 import glob
 import tensorflow as tf
-from tensorflow.keras.utils import register_keras_serializable
-import warnings
 from tensorflow_addons import (
     activations,
     callbacks,
@@ -40,31 +37,38 @@ def register_all(keras_objects: bool = True, custom_kernels: bool = True) -> Non
 
 
 def register_keras_objects() -> None:
-    global already_registered
-    if already_registered:
-        warnings.warn(
-            "Tensorflow Addons' functions and classes are already "
-            "registered in the Keras custom objects dictionary.",
-            UserWarning,
-        )
-    for module in SUBMODULES:
-        for attribute in _get_attributes(module):
-            if inspect.isclass(attribute) or inspect.isfunction(attribute):
-                register_keras_serializable(package="Addons")(attribute)
-
-    already_registered = True
+    # TODO: once layer_test is replaced by a public API
+    # and we can used unregistered objects with it
+    # we can remove all decorators.
+    # And register Keras objects here.
+    pass
 
 
 def register_custom_kernels() -> None:
+    all_shared_objects = get_all_shared_objects()
+    if not all_shared_objects:
+        raise FileNotFoundError(
+            "No shared objects files were found in the custom ops "
+            "directory in Tensorflow Addons, check your installation again,"
+            "or, if you don't need custom ops, call `tfa.register_all(custom_kernels=False)`"
+            " instead."
+        )
+    try:
+        for shared_object in all_shared_objects:
+            tf.load_op_library(shared_object)
+    except tf.errors.NotFoundError as e:
+        raise RuntimeError(
+            "One of the shared objects ({}) could not be loaded. This may be "
+            "due to a number of reasons (incompatible TensorFlow version, buiding from "
+            "source with different flags, broken install of TensorFlow Addons...). If you"
+            "wanted to register the shared objects because you needed them when loading your "
+            "model, you should fix your install of TensorFlow Addons. If you don't "
+            "use custom ops in your model, you can skip registering custom ops with "
+            "`tfa.register_all(custom_kernels=False)`".format(shared_object)
+        ) from e
+
+
+def get_all_shared_objects():
     custom_ops_dir = os.path.join(get_project_root(), "custom_ops")
     all_shared_objects = glob.glob(custom_ops_dir + "/**/*.so", recursive=True)
-    for shared_object in all_shared_objects:
-        tf.load_op_library(shared_object)
-
-
-def _get_attributes(module):
-    for attr_name in dir(module):
-        if attr_name.startswith("_"):
-            continue
-        attr = getattr(module, attr_name)
-        yield attr
+    return list(all_shared_objects)
--- tensorflow_addons/register_test.py
+++ tensorflow_addons/register_test.py
@@ -1,5 +1,6 @@
 import unittest
-from tensorflow_addons.register import register_all
+import tensorflow as tf
+from tensorflow_addons.register import register_all, get_all_shared_objects
 
 
 class AssertRNNCellTest(unittest.TestCase):
@@ -9,6 +10,13 @@ class AssertRNNCellTest(unittest.TestCase):
     def test_multiple_register(self):
         register_all()
         register_all()
+
+    def test_get_all_shared_objects(self):
+        all_shared_objects = get_all_shared_objects()
+        self.assertTrue(len(all_shared_objects) >= 15)
+
+        for file in all_shared_objects:
+            tf.load_op_library(file)
 
 
 if __name__ == "__main__":
