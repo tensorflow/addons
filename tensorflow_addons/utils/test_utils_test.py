e61a1f043c1603414a31ef5cc71c6a3a454a40c7
hyang <hong.meng.yang@outlook.com>
2020-03-02 12:16:58 -0500

    created run distributed test1

--- tensorflow_addons/utils/test_utils_test.py
+++ tensorflow_addons/utils/test_utils_test.py
@@ -15,13 +15,35 @@
 """Tests for Keras utils."""
 
 import tensorflow as tf
-
+import numpy as np
 from tensorflow_addons.utils import test_utils
 
 
+def _train_something():
+
+    model = tf.keras.models.Sequential()
+
+    model.add(tf.keras.layers.Dense(1, input_shape=(1,)))
+
+    model.comple(loss="mse", optimizer="sgd")
+
+    x = np.zeros(shape=(32, 1))
+    y = np.zeros(shape=(32, 1))
+
+    model.fit(x, y, batch_size=2, epochs=2)
+
+
+@test_utils.run_all_distributed(2)
 class TestUtilsTest(tf.test.TestCase):
-    def test_none(self):
-        pass
+    def test_training(self):
+        _train_something()
+
+    def test_training_again(self):
+        _train_something()
+
+    @test_utils.run_in_graph_and_eager_modes
+    def test_training_graph_eager(self):
+        _train_something()
 
 
 if __name__ == "__main__":
