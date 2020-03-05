81a12a47e9fcff3d6bd6f65e1e47783c4afb2d15
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-05 11:41:22 +0100

    Used more precise pattern to avoid useless SO files.

--- tools/install_so_files.sh
+++ tools/install_so_files.sh
@@ -1,7 +1,7 @@
 set -e
 bazel build //tensorflow_addons/...
-cp ./bazel-bin/tensorflow_addons/custom_ops/activations/*.so ./tensorflow_addons/custom_ops/activations/
-cp ./bazel-bin/tensorflow_addons/custom_ops/image/*.so ./tensorflow_addons/custom_ops/image/
-cp ./bazel-bin/tensorflow_addons/custom_ops/layers/*.so ./tensorflow_addons/custom_ops/layers/
-cp ./bazel-bin/tensorflow_addons/custom_ops/seq2seq/*.so ./tensorflow_addons/custom_ops/seq2seq/
-cp ./bazel-bin/tensorflow_addons/custom_ops/text/*.so ./tensorflow_addons/custom_ops/text/
+cp ./bazel-bin/tensorflow_addons/custom_ops/activations/_*_ops.so ./tensorflow_addons/custom_ops/activations/
+cp ./bazel-bin/tensorflow_addons/custom_ops/image/_*_ops.so ./tensorflow_addons/custom_ops/image/
+cp ./bazel-bin/tensorflow_addons/custom_ops/layers/_*_ops.so ./tensorflow_addons/custom_ops/layers/
+cp ./bazel-bin/tensorflow_addons/custom_ops/seq2seq/_*_ops.so ./tensorflow_addons/custom_ops/seq2seq/
+cp ./bazel-bin/tensorflow_addons/custom_ops/text/_*_ops.so ./tensorflow_addons/custom_ops/text/
