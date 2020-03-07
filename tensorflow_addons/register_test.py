8f69692c9e74f81fdef5167f87479e4a47a7d8ff
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-07 22:27:13 +0000

    Added an example of plain test function.

--- tensorflow_addons/register_test.py
+++ tensorflow_addons/register_test.py
@@ -1,20 +1,22 @@
-import unittest
+import sys
+
+import pytest
 import tensorflow as tf
 from tensorflow_addons.register import register_all, _get_all_shared_objects
 
 
-class AssertRNNCellTest(unittest.TestCase):
-    def test_multiple_register(self):
-        register_all()
-        register_all()
+def test_multiple_register():
+    register_all()
+    register_all()
 
-    def test_get_all_shared_objects(self):
-        all_shared_objects = _get_all_shared_objects()
-        self.assertTrue(len(all_shared_objects) >= 4)
 
-        for file in all_shared_objects:
-            tf.load_op_library(file)
+def test_get_all_shared_objects():
+    all_shared_objects = _get_all_shared_objects()
+    assert len(all_shared_objects) >= 4
+
+    for file in all_shared_objects:
+        tf.load_op_library(file)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    sys.exit(pytest.main([__file__]))
