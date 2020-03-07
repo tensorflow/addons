c7f36c8f83d728dd661f8b6de8d908ca6ecbedec
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-07 11:40:20 +0000

    Dummy change.

--- .github/workflows/make_wheel_macOS.sh
+++ .github/workflows/make_wheel_macOS.sh
@@ -6,7 +6,7 @@ python3 --version
 python3 -m pip install delocate wheel setuptools
 
 bash tools/ci_testing/install_bazel_macos.sh $BAZEL_VERSION
-#bash tools/ci_testing/addons_cpu.sh
+bash tools/ci_testing/addons_cpu.sh
 
 bazel build \
   -c opt \
