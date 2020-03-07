b143ec8bccb068cd3f8f8eaa85ccd907878dcb7b
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-07 21:25:40 +0000

    Single thread.

--- tools/ci_testing/addons_gpu.sh
+++ tools/ci_testing/addons_gpu.sh
@@ -33,4 +33,4 @@ python3 -m pip install -r tools/tests_dependencies/pytest.txt
 python3 ./configure.py
 cat ./.bazelrc
 bash tools/install_so_files.sh
-python3 -m pytest --cov=tensorflow_addons -v --durations=25 -n auto ./tensorflow_addons
+python3 -m pytest --cov=tensorflow_addons -v --durations=25 ./tensorflow_addons
