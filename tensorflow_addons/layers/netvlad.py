15b66b2a3111d8ffcb3fe87a6fad43286b738fa0
joel-shor <shor.joel@gmail.com>
2020-03-06 18:35:04 -0800

    Reformat files for open source spacing.

--- tensorflow_addons/layers/netvlad.py
+++ tensorflow_addons/layers/netvlad.py
@@ -20,104 +20,104 @@ import tensorflow as tf
 
 @tf.keras.utils.register_keras_serializable(package="Addons")
 class NetVLAD(tf.keras.layers.Layer):
-  """Applies NetVLAD to the input.
+    """Applies NetVLAD to the input.
 
-    "NetVLAD: CNN architecture for weakly supervised place recognition"
-    Relja Arandjelovic, Petr Gronat, Akihiko Torii, Tomas Pajdla, Josef Sivic.
-    https://arxiv.org/abs/1511.07247
+      "NetVLAD: CNN architecture for weakly supervised place recognition"
+      Relja Arandjelovic, Petr Gronat, Akihiko Torii, Tomas Pajdla, Josef Sivic.
+      https://arxiv.org/abs/1511.07247
 
-    Arguments:
-      num_clusters: The number of clusters to use.
-    Input shape:
-      3D tensor with shape: `(batch_size, time, feature_dim)`.
-    Output shape:
-      2D tensor with shape: `(batch_size, feature_dim * num_clusters)`.
-  """
-
-  def __init__(self, num_clusters: int, **kwargs):
-    super().__init__(**kwargs)
-    if num_clusters <= 0:
-      raise ValueError(
-          "`num_clusters` must be greater than 1: %i" % num_clusters)
-    self.num_clusters = num_clusters
-
-  def build(self, input_shape):
-    feature_dim = input_shape[-1]
-    if not isinstance(feature_dim, int):
-      feature_dim = feature_dim.value
-    self.fc = tf.keras.layers.Dense(
-        units=self.num_clusters,
-        activation=tf.nn.softmax,
-        kernel_regularizer=tf.keras.regularizers.l2(l=1e-5))
-    self.cluster_centers = self.add_weight(
-        name="cluster_centers",
-        shape=(1, feature_dim, self.num_clusters),
-        initializer=tf.keras.initializers.TruncatedNormal(
-            stddev=1.0 / math.sqrt(feature_dim)),
-        trainable=True)
-    super(NetVLAD, self).build(input_shape)
-
-  def call(self, frames):
-    """Apply the NetVLAD module to the given frames.
-
-    Args:
-      frames: A tensor with shape [batch_size, max_frames, feature_dim].
-
-    Returns:
-      A tensor with shape [batch_size, feature_dim * num_clusters].
-
-    Raises:
-      ValueError: If the `feature_dim` of input is not defined.
+      Arguments:
+        num_clusters: The number of clusters to use.
+      Input shape:
+        3D tensor with shape: `(batch_size, time, feature_dim)`.
+      Output shape:
+        2D tensor with shape: `(batch_size, feature_dim * num_clusters)`.
     """
-    frames.shape.assert_has_rank(3)
-    feature_dim = frames.shape.as_list()[-1]
-    if feature_dim is None:
-      raise ValueError("Last dimension must be defined.")
-    max_frames = tf.shape(frames)[-2]
 
-    # Compute soft-assignment from frames to clusters.
-    # Essentially: softmax(w*x + b), although BN can be used instead of bias.
-    frames = tf.reshape(frames, (-1, feature_dim))
-    activation = self.fc(frames)
-    activation = tf.reshape(activation, (-1, max_frames, self.num_clusters))
+    def __init__(self, num_clusters: int, **kwargs):
+        super().__init__(**kwargs)
+        if num_clusters <= 0:
+            raise ValueError(
+                "`num_clusters` must be greater than 1: %i" % num_clusters)
+        self.num_clusters = num_clusters
 
-    # Soft-count of number of frames assigned to each cluster.
-    # Output shape: [batch_size, 1, num_clusters]
-    a_sum = tf.math.reduce_sum(activation, axis=-2, keepdims=True)
+    def build(self, input_shape):
+        feature_dim = input_shape[-1]
+        if not isinstance(feature_dim, int):
+            feature_dim = feature_dim.value
+        self.fc = tf.keras.layers.Dense(
+            units=self.num_clusters,
+            activation=tf.nn.softmax,
+            kernel_regularizer=tf.keras.regularizers.l2(l=1e-5))
+        self.cluster_centers = self.add_weight(
+            name="cluster_centers",
+            shape=(1, feature_dim, self.num_clusters),
+            initializer=tf.keras.initializers.TruncatedNormal(
+                stddev=1.0 / math.sqrt(feature_dim)),
+            trainable=True)
+        super(NetVLAD, self).build(input_shape)
 
-    # Compute sum_{i=1}^N softmax(w_k * x_i + b_k) * c_k(j),
-    # for all clusters and dimensions.
-    # Output shape: [batch_size, feature_dim, num_clusters]
-    a = a_sum * self.cluster_centers
+    def call(self, frames):
+        """Apply the NetVLAD module to the given frames.
 
-    # Compute sum_{i=1}^N softmax(w_k * x_i + b_k) * x_i(j),
-    # for all clusters and dimensions.
-    # Output shape: (batch_size, feature_dim, num_clusters)
-    frames = tf.reshape(frames, (-1, max_frames, feature_dim))
-    b = tf.transpose(
-        tf.matmul(tf.transpose(activation, perm=(0, 2, 1)), frames),
-        perm=(0, 2, 1))
+        Args:
+          frames: A tensor with shape [batch_size, max_frames, feature_dim].
 
-    # Output shape: (batch_size, feature_dim, num_clusters)
-    vlad = b - a
+        Returns:
+          A tensor with shape [batch_size, feature_dim * num_clusters].
 
-    # Normalize first across the feature dimensions.
-    vlad = tf.nn.l2_normalize(vlad, 1)
+        Raises:
+          ValueError: If the `feature_dim` of input is not defined.
+        """
+        frames.shape.assert_has_rank(3)
+        feature_dim = frames.shape.as_list()[-1]
+        if feature_dim is None:
+            raise ValueError("Last dimension must be defined.")
+        max_frames = tf.shape(frames)[-2]
 
-    # Output shape: [batch_size, feature_dim * num_clusters]
-    vlad = tf.reshape(vlad, (-1, feature_dim * self.num_clusters))
+        # Compute soft-assignment from frames to clusters.
+        # Essentially: softmax(w*x + b), although BN can be used instead of bias.
+        frames = tf.reshape(frames, (-1, feature_dim))
+        activation = self.fc(frames)
+        activation = tf.reshape(activation, (-1, max_frames, self.num_clusters))
 
-    # Renormalize across both the feature dimensions (already normalized) and
-    # the cluster centers.
-    vlad = tf.nn.l2_normalize(vlad, 1)
+        # Soft-count of number of frames assigned to each cluster.
+        # Output shape: [batch_size, 1, num_clusters]
+        a_sum = tf.math.reduce_sum(activation, axis=-2, keepdims=True)
 
-    return vlad
+        # Compute sum_{i=1}^N softmax(w_k * x_i + b_k) * c_k(j),
+        # for all clusters and dimensions.
+        # Output shape: [batch_size, feature_dim, num_clusters]
+        a = a_sum * self.cluster_centers
 
-  def compute_output_shape(self, input_shape):
-    input_shape = tf.TensorShape(input_shape).as_list()
-    return tf.TensorShape([input_shape[0], input_shape[-1] * self.num_clusters])
+        # Compute sum_{i=1}^N softmax(w_k * x_i + b_k) * x_i(j),
+        # for all clusters and dimensions.
+        # Output shape: (batch_size, feature_dim, num_clusters)
+        frames = tf.reshape(frames, (-1, max_frames, feature_dim))
+        b = tf.transpose(
+            tf.matmul(tf.transpose(activation, perm=(0, 2, 1)), frames),
+            perm=(0, 2, 1))
 
-  def get_config(self):
-    config = {"num_clusters": self.num_clusters}
-    base_config = super().get_config()
-    return dict(list(base_config.items()) + list(config.items()))
+        # Output shape: (batch_size, feature_dim, num_clusters)
+        vlad = b - a
+
+        # Normalize first across the feature dimensions.
+        vlad = tf.nn.l2_normalize(vlad, 1)
+
+        # Output shape: [batch_size, feature_dim * num_clusters]
+        vlad = tf.reshape(vlad, (-1, feature_dim * self.num_clusters))
+
+        # Renormalize across both the feature dimensions (already normalized) and
+        # the cluster centers.
+        vlad = tf.nn.l2_normalize(vlad, 1)
+
+        return vlad
+
+    def compute_output_shape(self, input_shape):
+        input_shape = tf.TensorShape(input_shape).as_list()
+        return tf.TensorShape([input_shape[0], input_shape[-1] * self.num_clusters])
+
+    def get_config(self):
+        config = {"num_clusters": self.num_clusters}
+        base_config = super().get_config()
+        return dict(list(base_config.items()) + list(config.items()))
