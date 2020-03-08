cb61e7a4ba2ecef267ac7c10be02658d29074f33
Sean Morgan <seanmorgan@outlook.com>
2020-03-08 06:09:37 -0400

    Fix typing for poincare layer (#1245)
    * Fix typing for poincare layer
    
    * precommit hook

--- tensorflow_addons/layers/poincare_test.py
+++ tensorflow_addons/layers/poincare_test.py
@@ -55,7 +55,6 @@ class PoincareNormalizeTest(tf.test.TestCase):
                 self.assertLessEqual(norm.max(), 1.0 - epsilon + tol)
 
     def testPoincareNormalizeDimArray(self):
-        self.skipTest("Failing. See https://github.com/tensorflow/addons/issues/1205")
         x_shape = [20, 7, 3]
         epsilon = 1e-5
         tol = 1e-6
