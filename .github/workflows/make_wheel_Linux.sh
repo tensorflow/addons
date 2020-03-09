4ec60e778a1ebe65863a10368396a85ae1c441da
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-09 19:29:20 +0000

    Called bash to work around permission.

--- .github/workflows/make_wheel_Linux.sh
+++ .github/workflows/make_wheel_Linux.sh
@@ -6,7 +6,7 @@ docker run -e TF_NEED_CUDA=1 -v ${PWD}:/addons -w /addons \
 
 sudo apt-get install patchelf
 python3 -m pip install -U auditwheel==2.0.0
-tools/releases/tf_auditwheel_patch.sh
+bash tools/releases/tf_auditwheel_patch.sh
 
 auditwheel repair --plat manylinux2010_x86_64 artifacts/*.whl
 
