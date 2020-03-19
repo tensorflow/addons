aca2720e5223732ad27d81e6a67cc7f1ef94eabb
vinayvr11 <vr54640@gmail.com>
2020-03-19 17:01:14 +0530

    Updates

--- tensorflow_addons/image/solarize_ops_test.py
+++ tensorflow_addons/image/solarize_ops_test.py
@@ -1,5 +1,7 @@
 """Test of solarize_ops"""
 import tensorflow as tf
+import sys
+import pytest
 from tensorflow_addons.image import solarize_ops
 from tensorflow_addons.utils import test_utils
 from absl.testing import parameterized
@@ -9,18 +11,12 @@ from absl.testing import parameterized
 class SolarizeOPSTest(tf.test.TestCase, parameterized.TestCase):
     def test_solarize(self):
         if tf.executing_eagerly():
-            test_image_file = tf.io.read_file("test_data/Yellow_Smiley_Face.png")
+            test_image_file = tf.io.read_file("image/test_data/Yellow_Smiley_Face.png")
             test_image_file = tf.io.decode_image(test_image_file, dtype=tf.uint8)
             threshold = 10
             solarize_img = solarize_ops.solarize(test_image_file, threshold)
             self.assertAllEqual(tf.shape(solarize_img), tf.shape(test_image_file))
 
 
-def main(unused_argv):
-    del unused_argv
-    test = SolarizeOPSTest()
-    test.test_solarize()
-
-
 if __name__ == "__main__":
-    main(0)
+    sys.exit(pytest.main([__file__]))
