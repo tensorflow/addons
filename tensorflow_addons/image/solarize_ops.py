9ff2b1e6eeffd59c8ddd6362a1debdb165694b46
vinayvr11 <vr54640@gmail.com>
2020-03-19 12:05:39 +0530

    Solarize_ops for Tensorflow Addons

---
+++ tensorflow_addons/image/solarize_ops.py
@@ -0,0 +1,21 @@
+
+"""This module is used to invert all pixel values above a threshold
+   which simply means segmentation."""
+
+import tensorflow as tf
+
+
+def solarize(image, threshold=128):
+  # For each pixel in the image, select the pixel
+  # if the value is less than the threshold.
+  # Otherwise, subtract 255 from the pixel.
+  return tf.where(image < threshold, image, 255 - image)
+
+def solarize_add(image, addition=0, threshold=128):
+  # For each pixel in the image less than threshold
+  # we add 'addition' amount to it and then clip the
+  # pixel value to be between 0 and 255. The value
+  # of 'addition' is between -128 and 128.
+  added_image = tf.cast(image, tf.int64) + addition
+  added_image = tf.cast(tf.clip_by_value(added_image, 0, 255), tf.uint8)
+  return tf.where(image < threshold, added_image, image)
