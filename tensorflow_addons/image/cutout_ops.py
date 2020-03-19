e870ad74084097cb7239da99d230b0c3f1028869
fsx950223 <fsx950223@gmail.com>
2020-03-19 11:56:28 +0000

    add cutout op

---
+++ tensorflow_addons/image/cutout_ops.py
@@ -0,0 +1,161 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Cutout op"""
+
+import tensorflow as tf
+from tensorflow_addons.utils.types import TensorLike, Number
+from tensorflow_addons.image.utils import from_4D_image, to_4D_image
+from tensorflow.python.keras.utils import conv_utils
+import numpy as np
+
+
+def _get_image_wh(images, data_format):
+    if tf.equal(tf.rank(images), 4):
+        if data_format == "channels_last":
+            image_height, image_width = tf.shape(images)[1], tf.shape(images)[2]
+        else:
+            image_height, image_width = tf.shape(images)[2], tf.shape(images)[3]
+    else:
+        image_height, image_width = tf.shape(images)[0], tf.shape(images)[1]
+
+    return image_height, image_width
+
+
+def random_cutout(
+    images: TensorLike,
+    mask_size: TensorLike,
+    constant_values: Number = 0,
+    seed: Number = None,
+    data_format: str = "channels_last",
+) -> tf.Tensor:
+    """Apply cutout (https://arxiv.org/abs/1708.04552) to images.
+
+    This operation applies a (2*pad_size[0] x 2*pad_size[1]) mask of zeros to
+    a random location within `img`. The pixel values filled in will be of the
+    value `replace`. The located where the mask will be applied is randomly
+    chosen uniformly over the whole images.
+
+    Args:
+      images: A tensor of shape
+        (num_images, num_rows, num_columns, num_channels)
+        (NHWC), (num_images, num_channels, num_rows, num_columns)(NCHW), (num_rows, num_columns, num_channels) (HWC), or
+        (num_rows, num_columns) (HW).
+      mask_size: Specifies how big the zero mask that will be generated is that
+        is applied to the images. The mask will be of size
+        (2*pad_size[0] x 2*pad_size[1]).
+      constant_values: What pixel value to fill in the images in the area that has
+        the cutout mask applied to it.
+      seed: A Python integer. Used in combination with `tf.random.set_seed` to
+        create a reproducible sequence of tensors across multiple calls.
+      data_format: A string, one of `channels_last` (default) or `channels_first`.
+        The ordering of the dimensions in the inputs.
+        `channels_last` corresponds to inputs with shape
+        `(batch_size, ..., channels)` while `channels_first` corresponds to
+        inputs with shape `(batch_size, channels, ...)`.
+    Returns:
+      An image Tensor.
+    """
+    if np.isscalar(mask_size):
+        mask_size = [mask_size, mask_size]
+    data_format = conv_utils.normalize_data_format(data_format)
+    # Sample the center location in the images where the zero mask will be applied.
+    image_height, image_width = _get_image_wh(images, data_format)
+    cutout_center_height = tf.random.uniform(
+        shape=[], minval=0, maxval=image_height, dtype=tf.int32, seed=seed
+    )
+    cutout_center_width = tf.random.uniform(
+        shape=[], minval=0, maxval=image_width, dtype=tf.int32, seed=seed
+    )
+    return cutout(
+        images,
+        mask_size,
+        [cutout_center_height, cutout_center_width],
+        constant_values,
+        data_format,
+    )
+
+
+def cutout(
+    images: TensorLike,
+    mask_size: TensorLike,
+    offset: TensorLike,
+    constant_values: Number = 0,
+    data_format: str = "channels_last",
+) -> tf.Tensor:
+    """Apply cutout (https://arxiv.org/abs/1708.04552) to images.
+
+    This operation applies a (2*pad_size[0] x 2*pad_size[1]) mask of zeros to
+    a random location within `img`. The pixel values filled in will be of the
+    value `replace`. The located where the mask will be applied is randomly
+    chosen uniformly over the whole images.
+
+    Args:
+      images: A tensor of shape
+        (num_images, num_rows, num_columns, num_channels)
+        (NHWC), (num_images, num_channels, num_rows, num_columns)(NCHW), (num_rows, num_columns, num_channels) (HWC), or
+        (num_rows, num_columns) (HW).
+      mask_size: Specifies how big the zero mask that will be generated is that
+        is applied to the images. The mask will be of size
+        (2*pad_size[0] x 2*pad_size[1]).
+      offset: A tuple of (height, width)
+      constant_values: What pixel value to fill in the images in the area that has
+        the cutout mask applied to it.
+      data_format: A string, one of `channels_last` (default) or `channels_first`.
+        The ordering of the dimensions in the inputs.
+        `channels_last` corresponds to inputs with shape
+        `(batch_size, ..., channels)` while `channels_first` corresponds to
+        inputs with shape `(batch_size, channels, ...)`.
+    Returns:
+      An image Tensor.
+    """
+    with tf.name_scope("cutout"):
+        if np.isscalar(mask_size):
+            mask_size = [mask_size, mask_size]
+        data_format = conv_utils.normalize_data_format(data_format)
+        image_height, image_width = _get_image_wh(images, data_format)
+        cutout_center_height = offset[0]
+        cutout_center_width = offset[1]
+
+        lower_pad = tf.maximum(0, cutout_center_height - mask_size[0])
+        upper_pad = tf.maximum(0, image_height - cutout_center_height - mask_size[0])
+        left_pad = tf.maximum(0, cutout_center_width - mask_size[1])
+        right_pad = tf.maximum(0, image_width - cutout_center_width - mask_size[1])
+
+        cutout_shape = [
+            image_height - (lower_pad + upper_pad),
+            image_width - (left_pad + right_pad),
+        ]
+        padding_dims = [[lower_pad, upper_pad], [left_pad, right_pad]]
+        mask = tf.pad(
+            tf.zeros(cutout_shape, dtype=images.dtype), padding_dims, constant_values=1
+        )
+        mask_4d = to_4D_image(mask)
+        if tf.equal(tf.rank(images), 3):
+            mask = tf.tile(from_4D_image(mask_4d, 3), [1, 1, tf.shape(images)[-1]])
+        elif tf.equal(tf.rank(images), 4):
+            if data_format == "channels_last":
+                mask = tf.tile(
+                    mask_4d, [tf.shape(images)[0], 1, 1, tf.shape(images)[-1]]
+                )
+            else:
+                mask = tf.tile(
+                    mask_4d, [tf.shape(images)[0], tf.shape(images)[1], 1, 1]
+                )
+        images = tf.where(
+            tf.equal(mask, 0),
+            tf.ones_like(images, dtype=images.dtype) * constant_values,
+            images,
+        )
+        return images
---
+++ tensorflow_addons/image/cutout_ops_test.py
@@ -0,0 +1,69 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for cutout."""
+
+import sys
+
+import pytest
+import tensorflow as tf
+import numpy as np
+from absl.testing import parameterized
+from tensorflow_addons.image.cutout_ops import cutout, random_cutout
+from tensorflow_addons.utils import test_utils
+from tensorflow_addons.image.utils import from_4D_image, to_4D_image
+
+
+@parameterized.named_parameters(
+    ("float16", np.float16), ("float32", np.float32), ("uint8", np.uint8)
+)
+@pytest.mark.usefixtures("maybe_run_functions_eagerly")
+def test_different_dtypes(dtype):
+    test_image = tf.ones([1, 40, 40, 1], dtype=dtype)
+    result_image = cutout(test_image, 2, [2, 2])
+    cutout_area = tf.zeros([4, 4], dtype=dtype)
+    cutout_area = tf.pad(cutout_area, ((0, 36), (0, 36)), constant_values=1)
+    expect_image = to_4D_image(cutout_area)
+    np.testing.assert_allclose(result_image, expect_image)
+
+
+@pytest.mark.usefixtures("maybe_run_functions_eagerly")
+def test_different_channels():
+    test_image = tf.ones([1, 40, 40, 1], dtype=np.uint8)
+    cutout_area = tf.zeros([4, 4], dtype=np.uint8)
+    cutout_area = tf.pad(cutout_area, ((0, 36), (0, 36)), constant_values=1)
+    expect_image = to_4D_image(cutout_area)
+    for channel in [0, 1, 3, 4]:
+        result_image = random_cutout(test_image, 20, seed=1234)
+        np.testing.assert_allclose(tf.shape(result_image), tf.shape(expect_image))
+
+
+@pytest.mark.usefixtures("maybe_run_functions_eagerly")
+def test_different_ranks():
+    test_image_4d = tf.ones([1, 40, 40, 1], dtype=np.uint8)
+    cutout_area = tf.zeros([4, 4], dtype=np.uint8)
+    cutout_area = tf.pad(cutout_area, ((0, 36), (0, 36)), constant_values=1)
+    expect_image_4d = to_4D_image(cutout_area)
+
+    test_image_2d = from_4D_image(test_image_4d, 2)
+    expect_image_2d = from_4D_image(expect_image_4d, 2)
+    result_image_2d = random_cutout(test_image_2d, 20, seed=1234)
+    np.testing.assert_allclose(tf.shape(result_image_2d), tf.shape(expect_image_2d))
+
+    result_image_4d = random_cutout(test_image_4d, 20, seed=1234)
+    np.testing.assert_allclose(tf.shape(result_image_4d), tf.shape(expect_image_4d))
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__]))
