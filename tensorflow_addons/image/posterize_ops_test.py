dad7c706e7d835d66ca69b88d45d6c91a123626f
vinayvr11 <vr54640@gmail.com>
2020-03-19 23:57:50 +0530

    Add Posterize_ops and Posterize__ops_test script to Tensorflow_addons

---
+++ tensorflow_addons/image/posterize_ops_test.py
@@ -0,0 +1,46 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Test of solarize_ops"""
+
+import sys
+import absl
+import tensorflow as tf
+from tensorflow_addons.image import posterize_ops
+from tensorflow_addons.utils import test_utils
+
+
+@test_utils.run_all_in_graph_and_eager_modes
+class PosterizeOpsTest(tf.test.TestCase, absl.testing.parameterized.TestCase):
+    """PosterizeOpsTest class to test the working of
+    methods images"""
+
+    def test_posterize(self):
+        """ Method to test the posterize technique on images """
+        if tf.executing_eagerly():
+            orig_image = tf.io.read_file("test_data/Yellow_Smiley_Face.png")
+            orig_image = tf.io.decode_image(orig_image, dtype=tf.uint8)
+            bits = 2
+            posterize_image = posterize_ops.posterize(orig_image, bits)
+            self.assertAllEqual(tf.shape(orig_image), tf.shape(posterize_image))
+
+
+def main(_):
+    """ main method to run the test_posterize method """
+    test = PosterizeOpsTest()
+    test.test_posterize()
+
+
+if __name__ == "__main__":
+    sys.exit(absl.app.run(main))
