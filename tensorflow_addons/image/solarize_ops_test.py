7166693508448cdf9d0838449affab23df25f67e
vinayvr11 <vr54640@gmail.com>
2020-03-19 17:50:11 +0530

    update

--- tensorflow_addons/image/solarize_ops_test.py
+++ tensorflow_addons/image/solarize_ops_test.py
@@ -12,22 +12,22 @@ from absl.testing import parameterized
 class SolarizeOPSTest(tf.test.TestCase, parameterized.TestCase):
     """SolarizeOPSTest class to test the solarize images"""
 
-  def test_solarize(self):
-    if tf.executing_eagerly():
-      test_image_file = tf.io.read_file("test_data/Yellow_Smiley_Face.png")
-      test_image_file = tf.io.decode_image(test_image_file, dtype=tf.uint8)
-      threshold = 10
-      solarize_img = solarize_ops.solarize(test_image_file, threshold)
-      self.assertAllEqual(tf.shape(solarize_img), tf.shape(test_image_file))
+    def test_solarize(self):
+        if tf.executing_eagerly():
+            test_image_file = tf.io.read_file("test_data/Yellow_Smiley_Face.png")
+            test_image_file = tf.io.decode_image(test_image_file, dtype=tf.uint8)
+            threshold = 10
+            solarize_img = solarize_ops.solarize(test_image_file, threshold)
+            self.assertAllEqual(tf.shape(solarize_img), tf.shape(test_image_file))
 
-  def test_solarize_add(self):
-    if tf.executing_eagerly():
-      test_image_file = tf.io.read_file("test_data/Yellow_Smiley_Face.png")
-      test_image_file = tf.io.decode_image(test_image_file, dtype=tf.uint8)
-      threshold = 10
-      solarize_img = solarize_ops.solarize_add(test_image_file, threshold)
-      self.assertAllEqual(tf.shape(solarize_img), tf.shape(test_image_file))
+    def test_solarize_add(self):
+        if tf.executing_eagerly():
+            test_image_file = tf.io.read_file("test_data/Yellow_Smiley_Face.png")
+            test_image_file = tf.io.decode_image(test_image_file, dtype=tf.uint8)
+            threshold = 10
+            solarize_img = solarize_ops.solarize_add(test_image_file, threshold)
+            self.assertAllEqual(tf.shape(solarize_img), tf.shape(test_image_file))
 
 
 if __name__ == "__main__":
-  sys.exit(pytest.main([__file__]))
+    sys.exit(pytest.main([__file__]))
