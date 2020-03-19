87e7cb217785593af7703d3f141a10a64278eb5b
Abhineet Choudhary <abhi.chou4@gmail.com>
2020-03-19 15:26:06 +0530

    add previous tests

--- tensorflow_addons/image/compose_ops_test.py
+++ tensorflow_addons/image/compose_ops_test.py
@@ -17,6 +17,7 @@
 import sys
 import pytest
 import tensorflow as tf
+import numpy as np
 
 from tensorflow_addons.image import compose_ops
 from tensorflow_addons.utils import test_utils
@@ -26,10 +27,53 @@ _DTYPES = {
 }
 
 
+def blend_np(image1, image2, factor):
+    image1 = image1.astype("float32")
+    image2 = image2.astype("float32")
+    difference = image2 - image1
+    scaled = factor * difference
+    temp = image1 + scaled
+    if factor >= 0.0 and factor <= 1.0:
+        temp = np.round(temp)
+        return temp.astype("uint8")
+    temp = np.round(np.clip(temp, 0.0, 255.0))
+    return temp.astype("uint8")
+
+
 @test_utils.run_all_in_graph_and_eager_modes
 class ComposeOpTest(tf.test.TestCase):
     def test_blend(self):
         for dtype in _DTYPES:
+            image1 = tf.constant(
+                [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=dtype
+            )
+            image2 = tf.constant(
+                [
+                    [255, 255, 255, 255],
+                    [255, 255, 255, 255],
+                    [255, 255, 255, 255],
+                    [255, 255, 255, 255],
+                ],
+                dtype=dtype,
+            )
+            blended = compose_ops.blend(image1, image2, 0.5)
+            self.assertAllEqual(
+                self.evaluate(blended),
+                [
+                    [128, 128, 128, 128],
+                    [128, 128, 128, 128],
+                    [128, 128, 128, 128],
+                    [128, 128, 128, 128],
+                ],
+            )
+
+            image1 = np.random.randint(0, 255, (4, 4, 3), np.uint8)
+            image2 = np.random.randint(0, 255, (4, 4, 3), np.uint8)
+            blended = compose_ops.blend(
+                tf.convert_to_tensor(image1), tf.convert_to_tensor(image2), 0.35
+            )
+            self.assertAllEqual(self.evaluate(blended), blend_np(image1, image2, 0.35))
+
             image1_file = tf.io.read_file(
                 "tensorflow_addons/image/test_data/Yellow_Smiley_Face.png"
             )
