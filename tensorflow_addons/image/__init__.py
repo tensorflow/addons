8e7624bc7757c42059b10f4ca9d85864db3c53be
vinayvr11 <vr54640@gmail.com>
2020-03-19 16:10:49 +0530

    Update __init__.py

--- tensorflow_addons/image/__init__.py
+++ tensorflow_addons/image/__init__.py
@@ -28,4 +28,5 @@ from tensorflow_addons.image.sparse_image_warp import sparse_image_warp
 from tensorflow_addons.image.transform_ops import rotate
 from tensorflow_addons.image.transform_ops import transform
 from tensorflow_addons.image.translate_ops import translate
-from tensorflow_addons.image.solarize_ops import SolarizeOPSTest
+from tensorflow_addons.image.solarize_ops import solarize
+from tensorflow_addons.image.solarize_ops import solarize_add
