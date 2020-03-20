8d74c67c2e1d2b41f98f1b2e31141028b5282334
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-20 15:34:22 +0000

    Removed run_all_in_graph_and_eager_mode in r_square_test.py

--- tensorflow_addons/metrics/r_square_test.py
+++ tensorflow_addons/metrics/r_square_test.py
@@ -22,101 +22,103 @@ import tensorflow as tf
 from sklearn.metrics import r2_score as sklearn_r2_score
 from tensorflow_addons.metrics import RSquare
 from tensorflow_addons.metrics.r_square import VALID_MULTIOUTPUT
-from tensorflow_addons.utils import test_utils
 
 
-@test_utils.run_all_in_graph_and_eager_modes
-class RSquareTest(tf.test.TestCase):
-    def test_config(self):
-        r2_obj = RSquare(name="r_square")
-        self.assertEqual(r2_obj.name, "r_square")
-        self.assertEqual(r2_obj.dtype, tf.float32)
-        # Check save and restore config
-        r2_obj2 = RSquare.from_config(r2_obj.get_config())
-        self.assertEqual(r2_obj2.name, "r_square")
-        self.assertEqual(r2_obj2.dtype, tf.float32)
+def test_config():
+    r2_obj = RSquare(name="r_square")
+    assert r2_obj.name == "r_square"
+    assert r2_obj.dtype == tf.float32
+    # Check save and restore config
+    r2_obj2 = RSquare.from_config(r2_obj.get_config())
+    assert r2_obj2.name == "r_square"
+    assert r2_obj2.dtype == tf.float32
 
-    def initialize_vars(self, y_shape=(), multioutput: str = "uniform_average"):
-        r2_obj = RSquare(y_shape=y_shape, multioutput=multioutput)
-        self.evaluate(tf.compat.v1.variables_initializer(r2_obj.variables))
-        return r2_obj
 
-    def update_obj_states(self, obj, actuals, preds, sample_weight=None):
-        update_op = obj.update_state(actuals, preds, sample_weight=sample_weight)
-        self.evaluate(update_op)
+def initialize_vars(y_shape=(), multioutput: str = "uniform_average"):
+    return RSquare(y_shape=y_shape, multioutput=multioutput)
 
-    def check_results(self, obj, value):
-        self.assertAllClose(value, self.evaluate(obj.result()), atol=1e-5)
 
-    def test_r2_perfect_score(self):
-        actuals = tf.constant([100, 700, 40, 5.7], dtype=tf.float32)
-        preds = tf.constant([100, 700, 40, 5.7], dtype=tf.float32)
-        actuals = tf.cast(actuals, dtype=tf.float32)
-        preds = tf.cast(preds, dtype=tf.float32)
-        # Initialize
-        r2_obj = self.initialize_vars()
-        # Update
-        self.update_obj_states(r2_obj, actuals, preds)
-        # Check results
-        self.check_results(r2_obj, 1.0)
+def update_obj_states(obj, actuals, preds, sample_weight=None):
+    obj.update_state(actuals, preds, sample_weight=sample_weight)
 
-    def test_r2_worst_score(self):
-        actuals = tf.constant([10, 600, 4, 9.77], dtype=tf.float32)
-        preds = tf.constant([1, 70, 40, 5.7], dtype=tf.float32)
-        actuals = tf.cast(actuals, dtype=tf.float32)
-        preds = tf.cast(preds, dtype=tf.float32)
-        # Initialize
-        r2_obj = self.initialize_vars()
-        # Update
-        self.update_obj_states(r2_obj, actuals, preds)
-        # Check results
-        self.check_results(r2_obj, -0.073607)
 
-    def test_r2_random_score(self):
-        actuals = tf.constant([10, 600, 3, 9.77], dtype=tf.float32)
-        preds = tf.constant([1, 340, 40, 5.7], dtype=tf.float32)
-        actuals = tf.cast(actuals, dtype=tf.float32)
-        preds = tf.cast(preds, dtype=tf.float32)
-        # Initialize
-        r2_obj = self.initialize_vars()
-        # Update
-        self.update_obj_states(r2_obj, actuals, preds)
-        # Check results
-        self.check_results(r2_obj, 0.7376327)
+def check_results(obj, value):
+    np.testing.assert_allclose(value, obj.result(), atol=1e-5)
 
-    def test_r2_sklearn_comparison(self):
-        """Test that RSquare behaves similarly to the scikit-learn
-        implementation of the same metric, given random input.
-        """
-        for multioutput in VALID_MULTIOUTPUT:
-            for i in range(10):
-                actuals = np.random.rand(64, 3)
-                preds = np.random.rand(64, 3)
-                sample_weight = np.random.rand(64, 1)
-                tensor_actuals = tf.constant(actuals, dtype=tf.float32)
-                tensor_preds = tf.constant(preds, dtype=tf.float32)
-                tensor_sample_weight = tf.constant(sample_weight, dtype=tf.float32)
-                tensor_actuals = tf.cast(tensor_actuals, dtype=tf.float32)
-                tensor_preds = tf.cast(tensor_preds, dtype=tf.float32)
-                tensor_sample_weight = tf.cast(tensor_sample_weight, dtype=tf.float32)
-                # Initialize
-                r2_obj = self.initialize_vars(y_shape=(3,), multioutput=multioutput)
-                # Update
-                self.update_obj_states(
-                    r2_obj,
-                    tensor_actuals,
-                    tensor_preds,
-                    sample_weight=tensor_sample_weight,
-                )
-                # Check results by comparing to results of scikit-learn r2 implementation
-                sklearn_result = sklearn_r2_score(
-                    actuals, preds, sample_weight=sample_weight, multioutput=multioutput
-                )
-                self.check_results(r2_obj, sklearn_result)
 
-    def test_unrecognized_multioutput(self):
-        with pytest.raises(ValueError):
-            self.initialize_vars(multioutput="meadian")
+def test_r2_perfect_score():
+    actuals = tf.constant([100, 700, 40, 5.7], dtype=tf.float32)
+    preds = tf.constant([100, 700, 40, 5.7], dtype=tf.float32)
+    actuals = tf.cast(actuals, dtype=tf.float32)
+    preds = tf.cast(preds, dtype=tf.float32)
+    # Initialize
+    r2_obj = initialize_vars()
+    # Update
+    update_obj_states(r2_obj, actuals, preds)
+    # Check results
+    check_results(r2_obj, 1.0)
+
+
+def test_r2_worst_score():
+    actuals = tf.constant([10, 600, 4, 9.77], dtype=tf.float32)
+    preds = tf.constant([1, 70, 40, 5.7], dtype=tf.float32)
+    actuals = tf.cast(actuals, dtype=tf.float32)
+    preds = tf.cast(preds, dtype=tf.float32)
+    # Initialize
+    r2_obj = initialize_vars()
+    # Update
+    update_obj_states(r2_obj, actuals, preds)
+    # Check results
+    check_results(r2_obj, -0.073607)
+
+
+def test_r2_random_score():
+    actuals = tf.constant([10, 600, 3, 9.77], dtype=tf.float32)
+    preds = tf.constant([1, 340, 40, 5.7], dtype=tf.float32)
+    actuals = tf.cast(actuals, dtype=tf.float32)
+    preds = tf.cast(preds, dtype=tf.float32)
+    # Initialize
+    r2_obj = initialize_vars()
+    # Update
+    update_obj_states(r2_obj, actuals, preds)
+    # Check results
+    check_results(r2_obj, 0.7376327)
+
+
+def test_r2_sklearn_comparison():
+    """Test that RSquare behaves similarly to the scikit-learn
+    implementation of the same metric, given random input.
+    """
+    for multioutput in VALID_MULTIOUTPUT:
+        for i in range(10):
+            actuals = np.random.rand(64, 3)
+            preds = np.random.rand(64, 3)
+            sample_weight = np.random.rand(64, 1)
+            tensor_actuals = tf.constant(actuals, dtype=tf.float32)
+            tensor_preds = tf.constant(preds, dtype=tf.float32)
+            tensor_sample_weight = tf.constant(sample_weight, dtype=tf.float32)
+            tensor_actuals = tf.cast(tensor_actuals, dtype=tf.float32)
+            tensor_preds = tf.cast(tensor_preds, dtype=tf.float32)
+            tensor_sample_weight = tf.cast(tensor_sample_weight, dtype=tf.float32)
+            # Initialize
+            r2_obj = initialize_vars(y_shape=(3,), multioutput=multioutput)
+            # Update
+            update_obj_states(
+                r2_obj,
+                tensor_actuals,
+                tensor_preds,
+                sample_weight=tensor_sample_weight,
+            )
+            # Check results by comparing to results of scikit-learn r2 implementation
+            sklearn_result = sklearn_r2_score(
+                actuals, preds, sample_weight=sample_weight, multioutput=multioutput
+            )
+            check_results(r2_obj, sklearn_result)
+
+
+def test_unrecognized_multioutput():
+    with pytest.raises(ValueError):
+        initialize_vars(multioutput="meadian")
 
 
 if __name__ == "__main__":
