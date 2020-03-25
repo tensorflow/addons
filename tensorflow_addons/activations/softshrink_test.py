9f85a345c68facd2a3432894a4153b5773e03ab8
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-03-25 21:42:22 +0100

    Moved test out of run_in_graph_and_eager_mode in softshrink. (#1403)
    See #1328

--- tensorflow_addons/activations/softshrink_test.py
+++ tensorflow_addons/activations/softshrink_test.py
@@ -60,29 +60,31 @@ class SoftshrinkTest(tf.test.TestCase, parameterized.TestCase):
         theoretical, numerical = tf.test.compute_gradient(softshrink, [x])
         self.assertAllCloseAccordingToType(theoretical, numerical, atol=1e-4)
 
-    @parameterized.named_parameters(("float32", np.float32), ("float64", np.float64))
-    def test_same_as_py_func(self, dtype):
-        np.random.seed(1234)
-        for _ in range(20):
-            self.verify_funcs_are_equivalent(dtype)
 
-    def verify_funcs_are_equivalent(self, dtype):
-        x_np = np.random.uniform(-10, 10, size=(4, 4)).astype(dtype)
-        x = tf.convert_to_tensor(x_np)
-        lower = np.random.uniform(-10, 10)
-        upper = lower + np.random.uniform(0, 10)
+@pytest.mark.parametrize("dtype", [np.float16, np.float32])
+def test_same_as_py_func(dtype):
+    np.random.seed(1234)
+    for _ in range(20):
+        verify_funcs_are_equivalent(dtype)
 
-        with tf.GradientTape(persistent=True) as t:
-            t.watch(x)
-            y_native = softshrink(x, lower, upper)
-            y_py = _softshrink_py(x, lower, upper)
 
-        self.assertAllCloseAccordingToType(y_native, y_py)
+def verify_funcs_are_equivalent(dtype):
+    x_np = np.random.uniform(-10, 10, size=(4, 4)).astype(dtype)
+    x = tf.convert_to_tensor(x_np)
+    lower = np.random.uniform(-10, 10)
+    upper = lower + np.random.uniform(0, 10)
 
-        grad_native = t.gradient(y_native, x)
-        grad_py = t.gradient(y_py, x)
+    with tf.GradientTape(persistent=True) as t:
+        t.watch(x)
+        y_native = softshrink(x, lower, upper)
+        y_py = _softshrink_py(x, lower, upper)
 
-        self.assertAllCloseAccordingToType(grad_native, grad_py)
+    test_utils.assert_allclose_according_to_type(y_native, y_py)
+
+    grad_native = t.gradient(y_native, x)
+    grad_py = t.gradient(y_py, x)
+
+    test_utils.assert_allclose_according_to_type(grad_native, grad_py)
 
 
 if __name__ == "__main__":
