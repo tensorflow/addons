9993804ee1d84d76864a9a0fc01abd1545b63317
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-03-26 13:32:45 +0100

    Moved tests out of run_in_graph_and_eager_mode in mish. (#1400)

--- tensorflow_addons/activations/mish_test.py
+++ tensorflow_addons/activations/mish_test.py
@@ -16,7 +16,6 @@
 import sys
 
 import pytest
-from absl.testing import parameterized
 
 import numpy as np
 import tensorflow as tf
@@ -25,26 +24,23 @@ from tensorflow_addons.activations.mish import _mish_py
 from tensorflow_addons.utils import test_utils
 
 
-@test_utils.run_all_in_graph_and_eager_modes
-class MishTest(tf.test.TestCase, parameterized.TestCase):
-    @parameterized.named_parameters(
-        ("float16", np.float16), ("float32", np.float32), ("float64", np.float64)
+@pytest.mark.parametrize("dtype", [np.float16, np.float32, np.float64])
+def test_mish(dtype):
+    x = tf.constant([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=dtype)
+    expected_result = tf.constant(
+        [-0.2525015, -0.30340144, 0.0, 0.86509836, 1.943959], dtype=dtype
     )
-    def test_mish(self, dtype):
-        x = tf.constant([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=dtype)
-        expected_result = tf.constant(
-            [-0.2525015, -0.30340144, 0.0, 0.86509836, 1.943959], dtype=dtype
-        )
-        self.assertAllCloseAccordingToType(mish(x), expected_result)
+    test_utils.assert_allclose_according_to_type(mish(x), expected_result)
 
-    @parameterized.named_parameters(("float32", np.float32), ("float64", np.float64))
-    def test_theoretical_gradients(self, dtype):
-        # Only test theoretical gradients for float32 and float64
-        # because of the instability of float16 while computing jacobian
-        x = tf.constant([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=dtype)
 
-        theoretical, numerical = tf.test.compute_gradient(mish, [x])
-        self.assertAllCloseAccordingToType(theoretical, numerical, atol=1e-4)
+@pytest.mark.parametrize("dtype", [np.float32, np.float64])
+def test_theoretical_gradients(dtype):
+    # Only test theoretical gradients for float32 and float64
+    # because of the instability of float16 while computing jacobian
+    x = tf.constant([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=dtype)
+
+    theoretical, numerical = tf.test.compute_gradient(mish, [x])
+    test_utils.assert_allclose_according_to_type(theoretical, numerical, atol=1e-4)
 
 
 @pytest.mark.parametrize("dtype", [np.float32, np.float64])
