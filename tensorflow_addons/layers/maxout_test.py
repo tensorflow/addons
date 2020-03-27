ea30be0d94f318953a766b0735c8e666cfa22a9b
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-03-27 11:05:15 +0100

    Moved test out of run_in_graph_and_eager_mode in maxout. (#1434)
    See #1328

--- tensorflow_addons/layers/maxout_test.py
+++ tensorflow_addons/layers/maxout_test.py
@@ -18,49 +18,45 @@ import sys
 
 import pytest
 import numpy as np
-import tensorflow as tf
 
 from tensorflow_addons.layers.maxout import Maxout
 from tensorflow_addons.utils import test_utils
 
 
-@test_utils.run_all_in_graph_and_eager_modes
-class MaxOutTest(tf.test.TestCase):
-    def test_simple(self):
-        test_utils.layer_test(
-            Maxout, kwargs={"num_units": 3}, input_shape=(5, 4, 2, 18)
-        )
+pytestmark = pytest.mark.usefixtures("maybe_run_functions_eagerly")
 
-    def test_nchw(self):
-        test_utils.layer_test(
-            Maxout, kwargs={"num_units": 4, "axis": 1}, input_shape=(2, 20, 3, 6)
-        )
 
-        test_utils.layer_test(
-            Maxout, kwargs={"num_units": 4, "axis": -3}, input_shape=(2, 20, 3, 6)
-        )
+def test_simple():
+    test_utils.layer_test(Maxout, kwargs={"num_units": 3}, input_shape=(5, 4, 2, 18))
 
-    def test_unknown(self):
-        inputs = np.random.random((5, 4, 2, 18)).astype("float32")
-        test_utils.layer_test(
-            Maxout,
-            kwargs={"num_units": 3},
-            input_shape=(5, 4, 2, None),
-            input_data=inputs,
-        )
 
-        test_utils.layer_test(
-            Maxout,
-            kwargs={"num_units": 3},
-            input_shape=(None, None, None, None),
-            input_data=inputs,
-        )
+def test_nchw():
+    test_utils.layer_test(
+        Maxout, kwargs={"num_units": 4, "axis": 1}, input_shape=(2, 20, 3, 6)
+    )
 
-    def test_invalid_shape(self):
-        with self.assertRaisesRegexp(ValueError, r"number of features"):
-            test_utils.layer_test(
-                Maxout, kwargs={"num_units": 3}, input_shape=(5, 4, 2, 7)
-            )
+    test_utils.layer_test(
+        Maxout, kwargs={"num_units": 4, "axis": -3}, input_shape=(2, 20, 3, 6)
+    )
+
+
+def test_unknown():
+    inputs = np.random.random((5, 4, 2, 18)).astype("float32")
+    test_utils.layer_test(
+        Maxout, kwargs={"num_units": 3}, input_shape=(5, 4, 2, None), input_data=inputs,
+    )
+
+    test_utils.layer_test(
+        Maxout,
+        kwargs={"num_units": 3},
+        input_shape=(None, None, None, None),
+        input_data=inputs,
+    )
+
+
+def test_invalid_shape():
+    with pytest.raises(ValueError, match="number of features"):
+        test_utils.layer_test(Maxout, kwargs={"num_units": 3}, input_shape=(5, 4, 2, 7))
 
 
 if __name__ == "__main__":
