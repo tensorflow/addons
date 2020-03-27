cdd945cb6a0a501e01be7d60b57bf3fd8f41f1d4
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-03-27 17:39:31 +0100

    Moved test out of run_in_graph_and_eager_mode in giou (#1456)
    See #1328

--- tensorflow_addons/losses/giou_loss_test.py
+++ tensorflow_addons/losses/giou_loss_test.py
@@ -25,26 +25,24 @@ from tensorflow_addons.utils import test_utils
 from tensorflow_addons.losses import giou_loss, GIoULoss
 
 
+def test_config():
+    gl_obj = GIoULoss(reduction=tf.keras.losses.Reduction.NONE, name="giou_loss")
+    assert gl_obj.name == "giou_loss"
+    assert gl_obj.reduction == tf.keras.losses.Reduction.NONE
+
+
+@pytest.mark.parametrize("dtype", [np.float16, np.float32, np.float64])
+def test_iou(dtype):
+    boxes1 = tf.constant([[4.0, 3.0, 7.0, 5.0], [5.0, 6.0, 10.0, 7.0]], dtype=dtype)
+    boxes2 = tf.constant([[3.0, 4.0, 6.0, 8.0], [14.0, 14.0, 15.0, 15.0]], dtype=dtype)
+    expected_result = tf.constant([0.875, 1.0], dtype=dtype)
+    loss = giou_loss(boxes1, boxes2, mode="iou")
+    test_utils.assert_allclose_according_to_type(loss, expected_result)
+
+
 @test_utils.run_all_in_graph_and_eager_modes
 class GIoULossTest(tf.test.TestCase, parameterized.TestCase):
     """GIoU test class."""
-
-    def test_config(self):
-        gl_obj = GIoULoss(reduction=tf.keras.losses.Reduction.NONE, name="giou_loss")
-        self.assertEqual(gl_obj.name, "giou_loss")
-        self.assertEqual(gl_obj.reduction, tf.keras.losses.Reduction.NONE)
-
-    @parameterized.named_parameters(
-        ("float16", np.float16), ("float32", np.float32), ("float64", np.float64)
-    )
-    def test_iou(self, dtype):
-        boxes1 = tf.constant([[4.0, 3.0, 7.0, 5.0], [5.0, 6.0, 10.0, 7.0]], dtype=dtype)
-        boxes2 = tf.constant(
-            [[3.0, 4.0, 6.0, 8.0], [14.0, 14.0, 15.0, 15.0]], dtype=dtype
-        )
-        expected_result = tf.constant([0.875, 1.0], dtype=dtype)
-        loss = giou_loss(boxes1, boxes2, mode="iou")
-        self.assertAllCloseAccordingToType(loss, expected_result)
 
     @parameterized.named_parameters(
         ("float16", np.float16), ("float32", np.float32), ("float64", np.float64)
