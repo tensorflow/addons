7bd60d50754b12cf53434607c71c01c2262b5ce2
ghosalsattam <ghosalsattam@gmail.com>
2020-03-30 01:46:31 +0530

    git check

--- tensorflow_addons/image/gaussian_filter_ops_test.py
+++ tensorflow_addons/image/gaussian_filter_ops_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for gaussian blur."""
+"""  Tests for gaussian blur.  """
 
 
 from tensorflow_addons.image.gaussian_filter_ops import gaussian_blur
