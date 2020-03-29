37c64a00c89f909168e51fd2ea5411b687776f45
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-03-29 19:33:06 +0200

    Using buildkit all the time when building docker images. (#1480)
    * Dummy_pr
    
    * Trying buildkit in the google cloud build.
    
    * Fixing the gpu build.
    
    * Use docker buildkit everywhere.

--- tools/run_cpu_tests.sh
+++ tools/run_cpu_tests.sh
@@ -1,11 +1,6 @@
 # usage: bash tools/run_cpu_tests.sh
-# by default uses docker buildkit.
-# to disable it:
-# DOCKER_BUILDKIT=0 bash tools/run_cpu_tests.sh
+
 set -e
 
-if [ "$DOCKER_BUILDKIT" == "" ]; then
-  export DOCKER_BUILDKIT=1
-fi
-
+export DOCKER_BUILDKIT=1
 docker build --progress=plain -f tools/docker/cpu_tests.Dockerfile ./
--- tools/run_gpu_tests.sh
+++ tools/run_gpu_tests.sh
@@ -1,13 +1,7 @@
 # usage: bash tools/run_gpu_tests.sh
-# by default uses docker buildkit.
-# to disable it:
-# DOCKER_BUILDKIT=0 bash tools/run_gpu_tests.sh
 
 set -x -e
 
-if [ "$DOCKER_BUILDKIT" == "" ]; then
-  export DOCKER_BUILDKIT=1
-fi
-
+export DOCKER_BUILDKIT=1
 docker build -f tools/docker/gpu_tests.Dockerfile -t tfa_gpu_tests ./
 docker run --rm -t --runtime=nvidia tfa_gpu_tests
