5391dddf916ee51ab0963fd15895c3a09b22dbcd
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-30 21:09:07 +0000

    Added a minimal check to ensure we don't use tf private API.

--- tools/testing/check_file_name.py
+++ tools/testing/check_file_name.py
@@ -15,6 +15,7 @@
 #
 # Test that checks if we have any issues with case insensitive filesystems.
 
+import glob
 import os
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
@@ -38,5 +39,51 @@ def main():
             )
 
 
+def check_no_private_tf_api():
+
+    source_dir = os.path.join(BASE_DIR, "tensorflow_addons")
+    for path in glob.glob(source_dir + "/**/*.py", recursive=True):
+        if in_blacklist_private_api(path):
+            continue
+
+        with open(path) as f:
+            for i, line in enumerate(f):
+                if (
+                    "import tensorflow.python" in line
+                    or "from tensorflow.python" in line
+                ):
+                    raise ImportError(
+                        "A private tensorflow API import was found in {} at line {}.\n"
+                        "tensorflow.python refers to TensorFlow's internal source "
+                        "code and private functions/classes.\n"
+                        "The use of those is forbidden in Addons for stability reasons."
+                        "\nYou should find a public alternative or ask the "
+                        "TensorFlow team to expose publicly the function/class "
+                        "that you are using.\n"
+                        "If you're trying to do `import tensorflow.python.keras` "
+                        "it can be replaced with `import tensorflow.keras`."
+                        "".format(path, i + 1)
+                    )
+
+
+def in_blacklist_private_api(file_path):
+    # TODO: remove all elements of the list and remove the blacklist
+    blacklist = [
+        "tensorflow_addons/image/cutout_ops.py",
+        "tensorflow_addons/optimizers/novograd.py",
+        "tensorflow_addons/optimizers/moving_average.py",
+        "tensorflow_addons/metrics/r_square.py",
+        "tensorflow_addons/utils/test_utils.py",
+        "tensorflow_addons/losses/giou_loss.py",
+        "tensorflow_addons/seq2seq/decoder.py",
+        "tensorflow_addons/seq2seq/attention_wrapper.py",
+    ]
+    for blacklisted_file in blacklist:
+        if file_path.endswith(blacklisted_file):
+            return True
+    return False
+
+
 if __name__ == "__main__":
     main()
+    check_no_private_tf_api()
