1074573ee6496748bf47bc05dd7fedc25dfbdc47
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-03-31 12:02:49 +0000

    Forgot to set variable.

--- tools/run_gpu_tests.sh
+++ tools/run_gpu_tests.sh
@@ -3,5 +3,5 @@
 set -x -e
 
 export DOCKER_BUILDKIT=1
-docker build -f tools/docker/gpu_tests.Dockerfile -t tfa_gpu_tests ./
+docker build -f tools/docker/gpu_tests.Dockerfile --build-arg PY_VERSION=3.5 -t tfa_gpu_tests ./
 docker run --rm -t --runtime=nvidia tfa_gpu_tests
