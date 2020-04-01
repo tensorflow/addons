8cf744845030a83f0179a3dc6a8dee210eaeda1f
Jhuo IH <41447049+autoih@users.noreply.github.com>
2020-04-01 01:11:38 -0700

    Removed run_in_graph_and_eager_mode in contrastive_test (#1519)

--- tensorflow_addons/losses/contrastive_test.py
+++ tensorflow_addons/losses/contrastive_test.py
@@ -20,7 +20,6 @@ import numpy as np
 import pytest
 import tensorflow as tf
 from tensorflow_addons.losses import contrastive
-from tensorflow_addons.utils import test_utils
 
 
 def test_config():
@@ -39,62 +38,59 @@ def test_zero_loss():
     np.testing.assert_allclose(loss, 0.0)
 
 
-@test_utils.run_all_in_graph_and_eager_modes
-class ContrastiveLossTest(tf.test.TestCase):
-    def test_unweighted(self):
-        cl_obj = contrastive.ContrastiveLoss()
-        y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
-        y_pred = tf.constant([0.1, 0.3, 1.3, 0.7, 1.1, 0.5], dtype=tf.dtypes.float32)
-        loss = cl_obj(y_true, y_pred)
+def test_unweighted():
+    cl_obj = contrastive.ContrastiveLoss()
+    y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
+    y_pred = tf.constant([0.1, 0.3, 1.3, 0.7, 1.1, 0.5], dtype=tf.dtypes.float32)
+    loss = cl_obj(y_true, y_pred)
 
-        # Loss = y * (y`)^2 + (1 - y) * (max(m - y`, 0))^2
-        #      = [max(1 - 0.1, 0)^2, max(1 - 0.3, 0)^2,
-        #         1.3^2, 0.7^2, max(1 - 1.1, 0)^2, 0.5^2]
-        #      = [0.9^2, 0.7^2, 1.3^2, 0.7^2, 0^2, 0.5^2]
-        #      = [0.81, 0.49, 1.69, 0.49, 0, 0.25]
-        # Reduced loss = (0.81 + 0.49 + 1.69 + 0.49 + 0 + 0.25) / 6
-        #              = 0.621666
+    # Loss = y * (y`)^2 + (1 - y) * (max(m - y`, 0))^2
+    #      = [max(1 - 0.1, 0)^2, max(1 - 0.3, 0)^2,
+    #         1.3^2, 0.7^2, max(1 - 1.1, 0)^2, 0.5^2]
+    #      = [0.9^2, 0.7^2, 1.3^2, 0.7^2, 0^2, 0.5^2]
+    #      = [0.81, 0.49, 1.69, 0.49, 0, 0.25]
+    # Reduced loss = (0.81 + 0.49 + 1.69 + 0.49 + 0 + 0.25) / 6
+    #              = 0.621666
 
-        self.assertAllClose(loss, 0.621666)
+    np.testing.assert_allclose(loss, 0.621666, atol=1e-6, rtol=1e-6)
 
-    def test_sample_weighted(self):
-        cl_obj = contrastive.ContrastiveLoss()
-        y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
-        y_pred = tf.constant([0.1, 0.3, 1.3, 0.7, 1.1, 0.5], dtype=tf.dtypes.float32)
-        sample_weight = tf.constant(
-            [1.2, 0.8, 0.5, 0.4, 1.5, 1.0], dtype=tf.dtypes.float32
-        )
-        loss = cl_obj(y_true, y_pred, sample_weight=sample_weight)
 
-        # Loss = y * (y`)^2 + (1 - y) * (max(m - y`, 0))^2
-        #      = [max(1 - 0.1, 0)^2, max(1 - 0.3, 0)^2,
-        #         1.3^2, 0.7^2, max(1 - 1.1, 0)^2, 0.5^2]
-        #      = [0.9^2, 0.7^2, 1.3^2, 0.7^2, 0^2, 0.5^2]
-        #      = [0.81, 0.49, 1.69, 0.49, 0, 0.25]
-        # Weighted loss = [0.81 * 1.2, 0.49 * 0.8, 1.69 * 0.5,
-        #                  0.49 * 0.4, 0 * 1.5, 0.25 * 1.0]
-        #               = [0.972, 0.392, 0.845, 0.196, 0, 0.25]
-        # Reduced loss = (0.972 + 0.392 + 0.845 + 0.196 + 0 + 0.25) / 6
-        #              = 0.4425
+def test_sample_weighted():
+    cl_obj = contrastive.ContrastiveLoss()
+    y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
+    y_pred = tf.constant([0.1, 0.3, 1.3, 0.7, 1.1, 0.5], dtype=tf.dtypes.float32)
+    sample_weight = tf.constant([1.2, 0.8, 0.5, 0.4, 1.5, 1.0], dtype=tf.dtypes.float32)
+    loss = cl_obj(y_true, y_pred, sample_weight=sample_weight)
 
-        self.assertAllClose(loss, 0.4425)
+    # Loss = y * (y`)^2 + (1 - y) * (max(m - y`, 0))^2
+    #      = [max(1 - 0.1, 0)^2, max(1 - 0.3, 0)^2,
+    #         1.3^2, 0.7^2, max(1 - 1.1, 0)^2, 0.5^2]
+    #      = [0.9^2, 0.7^2, 1.3^2, 0.7^2, 0^2, 0.5^2]
+    #      = [0.81, 0.49, 1.69, 0.49, 0, 0.25]
+    # Weighted loss = [0.81 * 1.2, 0.49 * 0.8, 1.69 * 0.5,
+    #                  0.49 * 0.4, 0 * 1.5, 0.25 * 1.0]
+    #               = [0.972, 0.392, 0.845, 0.196, 0, 0.25]
+    # Reduced loss = (0.972 + 0.392 + 0.845 + 0.196 + 0 + 0.25) / 6
+    #              = 0.4425
 
-    def test_non_default_margin(self):
-        cl_obj = contrastive.ContrastiveLoss(margin=2.0)
-        y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
-        y_pred = tf.constant([0.1, 0.3, 1.3, 0.7, 1.1, 0.5], dtype=tf.dtypes.float32)
-        loss = cl_obj(y_true, y_pred)
+    np.testing.assert_allclose(loss, 0.4425)
 
-        # Loss = y * (y`)^2 + (1 - y) * (max(m - y`, 0))^2
-        #      = [max(2 - 0.1, 0)^2, max(2 - 0.3, 0)^2,
-        #         1.3^2, 0.7^2, max(2 - 1.1, 0)^2, 0.5^2]
-        #      = [1.9^2, 1.7^2, 1.3^2, 0.7^2, 0.9^2, 0.5^2]
-        #      = [3.61, 2.89, 1.69, 0.49, 0.81, 0.25]
-        # Reduced loss = (3.61 + 2.89 + 1.69 + 0.49 + 0.81 + 0.25) / 6
-        #              = 1.623333
 
-        loss = self.evaluate(loss)
-        self.assertAllClose(loss, 1.623333)
+def test_non_default_margin():
+    cl_obj = contrastive.ContrastiveLoss(margin=2.0)
+    y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
+    y_pred = tf.constant([0.1, 0.3, 1.3, 0.7, 1.1, 0.5], dtype=tf.dtypes.float32)
+    loss = cl_obj(y_true, y_pred)
+
+    # Loss = y * (y`)^2 + (1 - y) * (max(m - y`, 0))^2
+    #      = [max(2 - 0.1, 0)^2, max(2 - 0.3, 0)^2,
+    #         1.3^2, 0.7^2, max(2 - 1.1, 0)^2, 0.5^2]
+    #      = [1.9^2, 1.7^2, 1.3^2, 0.7^2, 0.9^2, 0.5^2]
+    #      = [3.61, 2.89, 1.69, 0.49, 0.81, 0.25]
+    # Reduced loss = (3.61 + 2.89 + 1.69 + 0.49 + 0.81 + 0.25) / 6
+    #              = 1.623333
+
+    np.testing.assert_allclose(loss, 1.623333, atol=1e-6, rtol=1e-6)
 
 
 def test_scalar_weighted():
