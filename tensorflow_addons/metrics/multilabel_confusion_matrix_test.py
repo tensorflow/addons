36387f81ca58e8300e3fee1d424e2c382391a9d8
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-04-01 21:49:36 +0000

    Removed the boilerplate.

--- tensorflow_addons/metrics/multilabel_confusion_matrix_test.py
+++ tensorflow_addons/metrics/multilabel_confusion_matrix_test.py
@@ -14,9 +14,6 @@
 # ==============================================================================
 """Tests for Multilabel Confusion Matrix Metric."""
 
-import sys
-
-import pytest
 import tensorflow as tf
 from tensorflow_addons.metrics import MultiLabelConfusionMatrix
 from tensorflow_addons.utils import test_utils
@@ -157,7 +154,3 @@ class MultiLabelConfusionMatrixTest(tf.test.TestCase):
                     [[8, 0], [0, 2]],
                 ],
             )
-
-
-if __name__ == "__main__":
-    sys.exit(pytest.main([__file__]))
