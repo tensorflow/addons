eb24f740ae240b319dd2d94989cd991d6c969194
autoih <ihjhuo@ibm.com>
2020-04-02 13:04:29 -0700

    Moved test out of run_in_graph_and_eager_mode in conditional_gradient

--- tensorflow_addons/optimizers/conditional_gradient_test.py
+++ tensorflow_addons/optimizers/conditional_gradient_test.py
@@ -14,41 +14,56 @@
 # ==============================================================================
 """Tests for Conditional Gradient."""
 
+import numpy as np
+import pytest
+import platform
+
 import tensorflow as tf
 from tensorflow_addons.utils import test_utils
-import numpy as np
 from tensorflow_addons.optimizers import conditional_gradient as cg_lib
-import platform
+
+
+WINDOWS_CPU = platform.system() == "Windows"
+
+pytestmark = pytest.mark.skipif(
+    WINDOWS_CPU,
+    reason="Doesn't work on Windows CPU version, tf.half is not registered for tf.linalg.svd function on Windows CPU version. So we have to remove tf.half when testing with Windows CPU version. See https://github.com/tensorflow/tensorflow/issues/36764",
+)
+
+
+def _dtypes_to_test(use_gpu):
+    if use_gpu:
+        return [tf.float32, tf.float64]
+    else:
+        return [tf.half, tf.float32, tf.float64]
+
+
+def _dtypes_with_checking_system(use_gpu, system):
+    if system == "Windows":
+        return [tf.float32, tf.float64]
+    else:
+        return _dtypes_to_test(use_gpu)
+
+
+@pytest.mark.usefixtures("maybe_run_functions_eagerly")
+def test_like_dist_belief_nuclear_cg01():
+    db_grad, db_out = _db_params_nuclear_cg01()
+    num_samples = len(db_grad)
+    var0 = tf.Variable([0.0] * num_samples)
+    grads0 = tf.constant([0.0] * num_samples)
+    ord = "nuclear"
+    cg_opt = cg_lib.ConditionalGradient(learning_rate=0.1, lambda_=0.1, ord=ord)
+
+    for i in range(num_samples):
+        grads0 = tf.constant(db_grad[i])
+        cg_opt.apply_gradients(zip([grads0], [var0]))
+        np.testing.assert_allclose(
+            np.array(db_out[i]), var0.numpy(), rtol=1e-6, atol=1e-6
+        )
 
 
 @test_utils.run_all_in_graph_and_eager_modes
 class ConditionalGradientTest(tf.test.TestCase):
-
-    # Based on issue #347 in the following link,
-    #        "https://github.com/tensorflow/addons/issues/347"
-    # tf.half is not registered for 'ResourceScatterUpdate' OpKernel
-    # for 'GPU' devices.
-    # So we have to remove tf.half when testing with gpu.
-    # The function "_DtypesToTest" is from
-    #       "https://github.com/tensorflow/tensorflow/blob/5d4a6cee737a1dc6c20172a1dc1
-    #        5df10def2df72/tensorflow/python/kernel_tests/conv_ops_3d_test.py#L53-L62"
-    def _DtypesToTest(self, use_gpu):
-        if use_gpu:
-            return [tf.float32, tf.float64]
-        else:
-            return [tf.half, tf.float32, tf.float64]
-
-    # Based on issue #36764 in the following link,
-    #        "https://github.com/tensorflow/tensorflow/issues/36764"
-    # tf.half is not registered for tf.linalg.svd function on Windows
-    # CPU version.
-    # So we have to remove tf.half when testing with Windows CPU version.
-    def _DtypesWithCheckingSystem(self, use_gpu, system):
-        if system == "Windows":
-            return [tf.float32, tf.float64]
-        else:
-            return self._DtypesToTest(use_gpu)
-
     def doTestBasicFrobenius(self, use_resource=False, use_callable_params=False):
         for i, dtype in enumerate([tf.half, tf.float32, tf.float64]):
             if use_resource:
@@ -154,7 +169,7 @@ class ConditionalGradientTest(tf.test.TestCase):
         # TODO:
         #       to address issue #36764
         for i, dtype in enumerate(
-            self._DtypesWithCheckingSystem(
+            _dtypes_with_checking_system(
                 use_gpu=tf.test.is_gpu_available(), system=platform.system()
             )
         ):
@@ -329,12 +344,12 @@ class ConditionalGradientTest(tf.test.TestCase):
         # operation. And it will call the 'ResourceScatterUpdate' OpKernel
         # for 'GPU' devices. However, tf.half is not registered in this case,
         # based on issue #347.
-        # Thus, we will call the "_DtypesToTest" function.
+        # Thus, we will call the "_dtypes_to_test" function.
         #
         # TODO:
         #       Wait for the solving of issue #347. After that, we will test
         #       for the dtype to be tf.half, with 'GPU' devices.
-        for dtype in self._DtypesToTest(use_gpu=tf.test.is_gpu_available()):
+        for dtype in _dtypes_to_test(use_gpu=tf.test.is_gpu_available()):
             var0 = tf.Variable([[1.0, 2.0]], dtype=dtype)
 
             def loss():
@@ -377,7 +392,7 @@ class ConditionalGradientTest(tf.test.TestCase):
     def testMinimizeSparseResourceVariableNuclear(self):
         # TODO:
         #       to address issue #347 and #36764.
-        for dtype in self._DtypesWithCheckingSystem(
+        for dtype in _dtypes_with_checking_system(
             use_gpu=tf.test.is_gpu_available(), system=platform.system()
         ):
             var0 = tf.Variable([[1.0, 2.0]], dtype=dtype)
@@ -576,7 +591,7 @@ class ConditionalGradientTest(tf.test.TestCase):
                 )
 
     def testTensorLearningRateAndConditionalGradientNuclear(self):
-        for dtype in self._DtypesWithCheckingSystem(
+        for dtype in _dtypes_with_checking_system(
             use_gpu=tf.test.is_gpu_available(), system=platform.system()
         ):
             # TODO:
@@ -955,290 +970,10 @@ class ConditionalGradientTest(tf.test.TestCase):
                     cg_update.run(feed_dict={grads0: db_grad[i]})
                 self.assertAllClose(np.array(db_out[i]), self.evaluate(var0))
 
-    def _dbParamsNuclearCG01(self):
-        """Return dist-belief conditional_gradient values.
-
-        Return values been generated from the dist-belief
-        conditional_gradient unittest, running with a learning rate of 0.1
-        and a lambda_ of 0.1.
-
-        These values record how a parameter vector of size 10, initialized
-        with 0.0, gets updated with 10 consecutive conditional_gradient
-        steps.
-        It uses random gradients.
-
-        Returns:
-            db_grad: The gradients to apply
-            db_out: The parameters after the conditional_gradient update.
-        """
-        db_grad = [[]] * 10
-        db_out = [[]] * 10
-        db_grad[0] = [
-            0.00096264342,
-            0.17914793,
-            0.93945462,
-            0.41396621,
-            0.53037018,
-            0.93197989,
-            0.78648776,
-            0.50036013,
-            0.55345792,
-            0.96722615,
-        ]
-        db_out[0] = [
-            -4.1552783e-05,
-            -7.7334875e-03,
-            -4.0554535e-02,
-            -1.7870164e-02,
-            -2.2895109e-02,
-            -4.0231861e-02,
-            -3.3951234e-02,
-            -2.1599628e-02,
-            -2.3891764e-02,
-            -4.1753381e-02,
-        ]
-        db_grad[1] = [
-            0.17075552,
-            0.88821375,
-            0.20873757,
-            0.25236958,
-            0.57578111,
-            0.15312378,
-            0.5513742,
-            0.94687688,
-            0.16012503,
-            0.22159521,
-        ]
-        db_out[1] = [
-            -0.00961733,
-            -0.0507779,
-            -0.01580694,
-            -0.01599489,
-            -0.03470477,
-            -0.01264373,
-            -0.03443632,
-            -0.05546713,
-            -0.01140388,
-            -0.01665068,
-        ]
-        db_grad[2] = [
-            0.35077485,
-            0.47304362,
-            0.44412705,
-            0.44368884,
-            0.078527533,
-            0.81223965,
-            0.31168157,
-            0.43203235,
-            0.16792089,
-            0.24644311,
-        ]
-        db_out[2] = [
-            -0.02462724,
-            -0.03699233,
-            -0.03154433,
-            -0.03153357,
-            -0.00876844,
-            -0.05606324,
-            -0.02447166,
-            -0.03469437,
-            -0.0124694,
-            -0.01829169,
-        ]
-        db_grad[3] = [
-            0.9694621,
-            0.75035888,
-            0.28171822,
-            0.83813518,
-            0.53807181,
-            0.3728098,
-            0.81454384,
-            0.03848977,
-            0.89759839,
-            0.93665648,
-        ]
-        db_out[3] = [
-            -0.04124615,
-            -0.03371741,
-            -0.0144246,
-            -0.03668303,
-            -0.02240246,
-            -0.02052062,
-            -0.03503307,
-            -0.00500922,
-            -0.03715545,
-            -0.0393002,
-        ]
-        db_grad[4] = [
-            0.38578293,
-            0.8536852,
-            0.88722926,
-            0.66276771,
-            0.13678469,
-            0.94036359,
-            0.69107032,
-            0.81897682,
-            0.5433259,
-            0.67860287,
-        ]
-        db_out[4] = [
-            -0.01979207,
-            -0.0380417,
-            -0.03747472,
-            -0.0305847,
-            -0.00779536,
-            -0.04024221,
-            -0.03156913,
-            -0.0337613,
-            -0.02578116,
-            -0.03148951,
-        ]
-        db_grad[5] = [
-            0.27885768,
-            0.76100707,
-            0.24625534,
-            0.81354135,
-            0.18959245,
-            0.48038563,
-            0.84163809,
-            0.41172323,
-            0.83259648,
-            0.44941229,
-        ]
-        db_out[5] = [
-            -0.01555188,
-            -0.04084422,
-            -0.01573331,
-            -0.04265549,
-            -0.01000746,
-            -0.02740575,
-            -0.04412147,
-            -0.02341569,
-            -0.0431026,
-            -0.02502293,
-        ]
-        db_grad[6] = [
-            0.27233034,
-            0.056316052,
-            0.5039115,
-            0.24105175,
-            0.35697976,
-            0.75913221,
-            0.73577434,
-            0.16014607,
-            0.57500273,
-            0.071136251,
-        ]
-        db_out[6] = [
-            -0.01890448,
-            -0.00767214,
-            -0.03367592,
-            -0.01962219,
-            -0.02374278,
-            -0.05110246,
-            -0.05128598,
-            -0.01254396,
-            -0.04094184,
-            -0.00703416,
-        ]
-        db_grad[7] = [
-            0.58697265,
-            0.2494842,
-            0.08106143,
-            0.39954534,
-            0.15892942,
-            0.12683646,
-            0.74053431,
-            0.16033,
-            0.66625422,
-            0.73515922,
-        ]
-        db_out[7] = [
-            -0.03772915,
-            -0.01599993,
-            -0.00831695,
-            -0.0263572,
-            -0.01207801,
-            -0.01285448,
-            -0.05034329,
-            -0.01104364,
-            -0.04477356,
-            -0.04558992,
-        ]
-        db_grad[8] = [
-            0.8215279,
-            0.41994119,
-            0.95172721,
-            0.68000203,
-            0.79439718,
-            0.43384039,
-            0.55561525,
-            0.22567581,
-            0.93331909,
-            0.29438227,
-        ]
-        db_out[8] = [
-            -0.03919835,
-            -0.01970845,
-            -0.04187151,
-            -0.03195836,
-            -0.03546333,
-            -0.01999326,
-            -0.02899324,
-            -0.01083582,
-            -0.04472339,
-            -0.01725317,
-        ]
-        db_grad[9] = [
-            0.68297005,
-            0.67758518,
-            0.1748755,
-            0.13266537,
-            0.70697063,
-            0.055731893,
-            0.68593478,
-            0.50580865,
-            0.12602448,
-            0.093537711,
-        ]
-        db_out[9] = [
-            -0.04510314,
-            -0.04282944,
-            -0.0147322,
-            -0.0111956,
-            -0.04617687,
-            -0.00535998,
-            -0.0442614,
-            -0.031584,
-            -0.01207165,
-            -0.00736567,
-        ]
-        return db_grad, db_out
-
-    def testLikeDistBeliefNuclearCG01(self):
-        with self.cached_session():
-            db_grad, db_out = self._dbParamsNuclearCG01()
-            num_samples = len(db_grad)
-            var0 = tf.Variable([0.0] * num_samples)
-            grads0 = tf.constant([0.0] * num_samples)
-            ord = "nuclear"
-            cg_opt = cg_lib.ConditionalGradient(learning_rate=0.1, lambda_=0.1, ord=ord)
-            if not tf.executing_eagerly():
-                cg_update = cg_opt.apply_gradients(zip([grads0], [var0]))
-                self.evaluate(tf.compat.v1.global_variables_initializer())
-
-            for i in range(num_samples):
-                if tf.executing_eagerly():
-                    grads0 = tf.constant(db_grad[i])
-                    cg_opt.apply_gradients(zip([grads0], [var0]))
-                else:
-                    cg_update.run(feed_dict={grads0: db_grad[i]})
-                self.assertAllClose(np.array(db_out[i]), self.evaluate(var0))
-
     def testSparseFrobenius(self):
         # TODO:
         #       To address the issue #347.
-        for dtype in self._DtypesToTest(use_gpu=tf.test.is_gpu_available()):
+        for dtype in _dtypes_to_test(use_gpu=tf.test.is_gpu_available()):
             with self.cached_session():
                 var0 = tf.Variable(tf.zeros([4, 2], dtype=dtype))
                 var1 = tf.Variable(tf.constant(1.0, dtype, [4, 2]))
@@ -1359,7 +1094,7 @@ class ConditionalGradientTest(tf.test.TestCase):
     def testSparseNuclear(self):
         # TODO:
         #       To address the issue #347 and issue #36764.
-        for dtype in self._DtypesWithCheckingSystem(
+        for dtype in _dtypes_with_checking_system(
             use_gpu=tf.test.is_gpu_available(), system=platform.system()
         ):
             with self.cached_session():
@@ -1641,7 +1376,7 @@ class ConditionalGradientTest(tf.test.TestCase):
     def testSharingNuclear(self):
         # TODO:
         #       To address the issue #36764.
-        for dtype in self._DtypesWithCheckingSystem(
+        for dtype in _dtypes_with_checking_system(
             use_gpu=tf.test.is_gpu_available(), system=platform.system()
         ):
             with self.cached_session():
@@ -1773,5 +1508,262 @@ class ConditionalGradientTest(tf.test.TestCase):
                 )
 
 
-if __name__ == "__main__":
-    tf.test.main()
+def _db_params_nuclear_cg01():
+    """Return dist-belief conditional_gradient values.
+
+    Return values been generated from the dist-belief
+    conditional_gradient unittest, running with a learning rate of 0.1
+    and a lambda_ of 0.1.
+
+    These values record how a parameter vector of size 10, initialized
+    with 0.0, gets updated with 10 consecutive conditional_gradient
+    steps.
+    It uses random gradients.
+
+    Returns:
+        db_grad: The gradients to apply
+        db_out: The parameters after the conditional_gradient update.
+    """
+    db_grad = [[]] * 10
+    db_out = [[]] * 10
+    db_grad[0] = [
+        0.00096264342,
+        0.17914793,
+        0.93945462,
+        0.41396621,
+        0.53037018,
+        0.93197989,
+        0.78648776,
+        0.50036013,
+        0.55345792,
+        0.96722615,
+    ]
+    db_out[0] = [
+        -4.1552783e-05,
+        -7.7334875e-03,
+        -4.0554535e-02,
+        -1.7870164e-02,
+        -2.2895109e-02,
+        -4.0231861e-02,
+        -3.3951234e-02,
+        -2.1599628e-02,
+        -2.3891764e-02,
+        -4.1753381e-02,
+    ]
+    db_grad[1] = [
+        0.17075552,
+        0.88821375,
+        0.20873757,
+        0.25236958,
+        0.57578111,
+        0.15312378,
+        0.5513742,
+        0.94687688,
+        0.16012503,
+        0.22159521,
+    ]
+    db_out[1] = [
+        -0.00961733,
+        -0.0507779,
+        -0.01580694,
+        -0.01599489,
+        -0.03470477,
+        -0.01264373,
+        -0.03443632,
+        -0.05546713,
+        -0.01140388,
+        -0.01665068,
+    ]
+    db_grad[2] = [
+        0.35077485,
+        0.47304362,
+        0.44412705,
+        0.44368884,
+        0.078527533,
+        0.81223965,
+        0.31168157,
+        0.43203235,
+        0.16792089,
+        0.24644311,
+    ]
+    db_out[2] = [
+        -0.02462724,
+        -0.03699233,
+        -0.03154433,
+        -0.03153357,
+        -0.00876844,
+        -0.05606324,
+        -0.02447166,
+        -0.03469437,
+        -0.0124694,
+        -0.01829169,
+    ]
+    db_grad[3] = [
+        0.9694621,
+        0.75035888,
+        0.28171822,
+        0.83813518,
+        0.53807181,
+        0.3728098,
+        0.81454384,
+        0.03848977,
+        0.89759839,
+        0.93665648,
+    ]
+    db_out[3] = [
+        -0.04124615,
+        -0.03371741,
+        -0.0144246,
+        -0.03668303,
+        -0.02240246,
+        -0.02052062,
+        -0.03503307,
+        -0.00500922,
+        -0.03715545,
+        -0.0393002,
+    ]
+    db_grad[4] = [
+        0.38578293,
+        0.8536852,
+        0.88722926,
+        0.66276771,
+        0.13678469,
+        0.94036359,
+        0.69107032,
+        0.81897682,
+        0.5433259,
+        0.67860287,
+    ]
+    db_out[4] = [
+        -0.01979207,
+        -0.0380417,
+        -0.03747472,
+        -0.0305847,
+        -0.00779536,
+        -0.04024221,
+        -0.03156913,
+        -0.0337613,
+        -0.02578116,
+        -0.03148951,
+    ]
+    db_grad[5] = [
+        0.27885768,
+        0.76100707,
+        0.24625534,
+        0.81354135,
+        0.18959245,
+        0.48038563,
+        0.84163809,
+        0.41172323,
+        0.83259648,
+        0.44941229,
+    ]
+    db_out[5] = [
+        -0.01555188,
+        -0.04084422,
+        -0.01573331,
+        -0.04265549,
+        -0.01000746,
+        -0.02740575,
+        -0.04412147,
+        -0.02341569,
+        -0.0431026,
+        -0.02502293,
+    ]
+    db_grad[6] = [
+        0.27233034,
+        0.056316052,
+        0.5039115,
+        0.24105175,
+        0.35697976,
+        0.75913221,
+        0.73577434,
+        0.16014607,
+        0.57500273,
+        0.071136251,
+    ]
+    db_out[6] = [
+        -0.01890448,
+        -0.00767214,
+        -0.03367592,
+        -0.01962219,
+        -0.02374278,
+        -0.05110246,
+        -0.05128598,
+        -0.01254396,
+        -0.04094184,
+        -0.00703416,
+    ]
+    db_grad[7] = [
+        0.58697265,
+        0.2494842,
+        0.08106143,
+        0.39954534,
+        0.15892942,
+        0.12683646,
+        0.74053431,
+        0.16033,
+        0.66625422,
+        0.73515922,
+    ]
+    db_out[7] = [
+        -0.03772915,
+        -0.01599993,
+        -0.00831695,
+        -0.0263572,
+        -0.01207801,
+        -0.01285448,
+        -0.05034329,
+        -0.01104364,
+        -0.04477356,
+        -0.04558992,
+    ]
+    db_grad[8] = [
+        0.8215279,
+        0.41994119,
+        0.95172721,
+        0.68000203,
+        0.79439718,
+        0.43384039,
+        0.55561525,
+        0.22567581,
+        0.93331909,
+        0.29438227,
+    ]
+    db_out[8] = [
+        -0.03919835,
+        -0.01970845,
+        -0.04187151,
+        -0.03195836,
+        -0.03546333,
+        -0.01999326,
+        -0.02899324,
+        -0.01083582,
+        -0.04472339,
+        -0.01725317,
+    ]
+    db_grad[9] = [
+        0.68297005,
+        0.67758518,
+        0.1748755,
+        0.13266537,
+        0.70697063,
+        0.055731893,
+        0.68593478,
+        0.50580865,
+        0.12602448,
+        0.093537711,
+    ]
+    db_out[9] = [
+        -0.04510314,
+        -0.04282944,
+        -0.0147322,
+        -0.0111956,
+        -0.04617687,
+        -0.00535998,
+        -0.0442614,
+        -0.031584,
+        -0.01207165,
+        -0.00736567,
+    ]
+    return db_grad, db_out
