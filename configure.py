11f99154456140881afa6173df4bef95b1d313b5
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-04-10 09:36:08 +0000

    Fix configure.py

--- configure.py
+++ configure.py
@@ -23,8 +23,6 @@ import logging
 
 import tensorflow as tf
 
-_DEFAULT_CUDA_VERISON = "10.1"
-_DEFAULT_CUDNN_VERSION = "7"
 _TFA_BAZELRC = ".bazelrc"
 
 
@@ -44,13 +42,6 @@ def is_macos():
 
 def is_windows():
     return platform.system() == "Windows"
-
-
-def get_input(question):
-    try:
-        return input(question)
-    except EOFError:
-        return ""
 
 
 def get_tf_header_dir():
@@ -107,89 +98,23 @@ def create_build_configuration():
     write_to_bazelrc("build --strategy=Genrule=standalone")
     write_to_bazelrc("build -c opt")
 
-    _TF_NEED_CUDA = os.getenv("TF_NEED_CUDA")
-
-    while _TF_NEED_CUDA is None:
-        print()
-        answer = get_input("Do you want to build GPU ops? [y/N] ")
-        if answer in ("Y", "y"):
-            print("> Building GPU & CPU ops")
-            _TF_NEED_CUDA = "1"
-        elif answer in ("N", "n", ""):
-            print("> Building only CPU ops")
-            _TF_NEED_CUDA = "0"
-        else:
-            print("Invalid selection:", answer)
-
-    if _TF_NEED_CUDA == "1":
+    if os.getenv("TF_NEED_CUDA", "0") == "1":
+        print("> Building GPU & CPU ops")
         configure_cuda()
+    else:
+        print("> Building only CPU ops")
 
     print()
-    print("Build configurations successfully written to", _TFA_BAZELRC)
+    print("Build configurations successfully written to", _TFA_BAZELRC, ":\n")
     print(pathlib.Path(_TFA_BAZELRC).read_text())
-    print()
-
-
-def get_cuda_toolkit_path():
-    default = "/usr/local/cuda"
-    cuda_toolkit_path = os.getenv("CUDA_TOOLKIT_PATH")
-    if cuda_toolkit_path is None:
-        answer = get_input(
-            "Please specify the location of CUDA. [Default is {}]: ".format(default)
-        )
-        cuda_toolkit_path = answer or default
-    print("> CUDA installation path:", cuda_toolkit_path)
-    print()
-    return cuda_toolkit_path
-
-
-def get_cudnn_install_path():
-    default = "/usr/lib/x86_64-linux-gnu"
-    cudnn_install_path = os.getenv("CUDNN_INSTALL_PATH")
-    if cudnn_install_path is None:
-        answer = get_input(
-            "Please specify the location of cuDNN installation. [Default is {}]: ".format(
-                default
-            )
-        )
-        cudnn_install_path = answer or default
-    print("> cuDNN installation path:", cudnn_install_path)
-    print()
-    return cudnn_install_path
 
 
 def configure_cuda():
-    _TF_CUDA_VERSION = os.getenv("TF_CUDA_VERSION")
-    _TF_CUDNN_VERSION = os.getenv("TF_CUDNN_VERSION")
-
-    print()
-    print("Configuring GPU setup...")
-
-    if _TF_CUDA_VERSION is None:
-        answer = get_input(
-            "Please specify the CUDA version [Default is {}]: ".format(
-                _DEFAULT_CUDA_VERISON
-            )
-        )
-        _TF_CUDA_VERSION = answer or _DEFAULT_CUDA_VERISON
-    print("> Using CUDA version:", _TF_CUDA_VERSION)
-    print()
-
-    if _TF_CUDNN_VERSION is None:
-        answer = get_input(
-            "Please specify the cuDNN major version [Default is {}]: ".format(
-                _DEFAULT_CUDNN_VERSION
-            )
-        )
-        _TF_CUDNN_VERSION = answer or _DEFAULT_CUDNN_VERSION
-    print("> Using cuDNN version:", _TF_CUDNN_VERSION)
-    print()
-
     write_action_env_to_bazelrc("TF_NEED_CUDA", "1")
-    write_action_env_to_bazelrc("CUDA_TOOLKIT_PATH", get_cuda_toolkit_path())
-    write_action_env_to_bazelrc("CUDNN_INSTALL_PATH", get_cudnn_install_path())
-    write_action_env_to_bazelrc("TF_CUDA_VERSION", _TF_CUDA_VERSION)
-    write_action_env_to_bazelrc("TF_CUDNN_VERSION", _TF_CUDNN_VERSION)
+    write_action_env_to_bazelrc("CUDA_TOOLKIT_PATH", os.getenv("CUDA_TOOLKIT_PATH", "/usr/local/cuda"))
+    write_action_env_to_bazelrc("CUDNN_INSTALL_PATH", os.getenv("CUDNN_INSTALL_PATH", "/usr/lib/x86_64-linux-gnu"))
+    write_action_env_to_bazelrc("TF_CUDA_VERSION", os.getenv("TF_CUDA_VERSION", "10.1"))
+    write_action_env_to_bazelrc("TF_CUDNN_VERSION", os.getenv("TF_CUDNN_VERSION", "7"))
 
     write_to_bazelrc("test --config=cuda")
     write_to_bazelrc("build --config=cuda")
