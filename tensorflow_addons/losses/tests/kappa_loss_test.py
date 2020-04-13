ebfea4c46ec1715aa9663f3cc095ffe7af54a437
Wenmin Wu <wuwenmin1991@gmail.com>
2020-04-13 17:17:29 +0800

    [SoftKappaLoss] unnecessary change to tigger CI

--- tensorflow_addons/losses/tests/kappa_loss_test.py
+++ tensorflow_addons/losses/tests/kappa_loss_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for Weighted Kappa loss."""
+"""Tests for Weighted Kappa Loss."""
 
 import pytest
 import numpy as np
