e8d537dec40bf29fd728ba7a868751405a1c0e3e
autoih <ihjhuo@ibm.com>
2020-04-13 11:36:29 -0700

    combine cpu and gpu test

--- tensorflow_addons/seq2seq/tests/beam_search_ops_test.py
+++ tensorflow_addons/seq2seq/tests/beam_search_ops_test.py
@@ -47,33 +47,8 @@ def test_gather_tree_one():
     np.testing.assert_equal(expected_result, beams.numpy())
 
 
-def test_bad_parent_values_on_cpu():
-    # (batch_size = 1, max_time = 4, beams = 3)
-    # bad parent in beam 1 time 1
-    end_token = 10
-    step_ids = _transpose_batch_time([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [-1, -1, -1]]])
-    parent_ids = _transpose_batch_time(
-        [[[0, 0, 0], [0, -1, 1], [2, 1, 2], [-1, -1, -1]]]
-    )
-    max_sequence_lengths = [3]
-    with tf.device("/cpu:0"):
-        with pytest.raises(
-            tf.errors.InvalidArgumentError,
-            match="parent id -1 at (batch, time, beam) == (0, 0, 1)",
-        ):
-            _ = gather_tree(
-                step_ids=step_ids,
-                parent_ids=parent_ids,
-                max_sequence_lengths=max_sequence_lengths,
-                end_token=end_token,
-            )
-
-
+@pytest.mark.usefixtures("cpu_and_gpu")
 def test_bad_parent_values_on_gpu():
-    # Only want to run this test on CUDA devices, as gather_tree is not
-    # registered for SYCL devices.
-    if not tf.test.is_gpu_available(cuda_only=True):
-        return
     # (max_time = 4, batch_size = 1, beams = 3)
     # bad parent in beam 1 time 1; appears as a negative index at time 0
     end_token = 10
@@ -85,14 +60,14 @@ def test_bad_parent_values_on_gpu():
     expected_result = _transpose_batch_time(
         [[[2, -1, 2], [6, 5, 6], [7, 8, 9], [10, 10, 10]]]
     )
-    with tf.device("/device:GPU:0"):
+    with pytest.raises(tf.errors.InvalidArgumentError):
         beams = gather_tree(
             step_ids=step_ids,
             parent_ids=parent_ids,
             max_sequence_lengths=max_sequence_lengths,
             end_token=end_token,
         )
-        assert expected_result == beams
+        np.testing.assert_equal(expected_result, beams.numpy())
 
 
 def test_gather_tree_batch():
