065dbbd61585c3c189e6982406c5d279d9f1b176
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-04-15 15:25:36 +0530

    Added tests for checking model weights

--- tensorflow_addons/callbacks/tests/avg_model_checkpoint_test.py
+++ tensorflow_addons/callbacks/tests/avg_model_checkpoint_test.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow_addons.callbacks import AverageModelCheckpoint
@@ -32,7 +33,7 @@ def get_data_and_model(optimizer="moving_avg"):
 
 
 def test_compatibility_with_some_opts_only(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model(optimizer="rmsprop")
     avg_model_ckpt = AverageModelCheckpoint(
         update_weights=True, filepath=test_model_filepath
@@ -48,7 +49,7 @@ def test_compatibility_with_some_opts_only(tmp_path):
 
 
 def test_model_file_creation(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model()
     avg_model_ckpt = AverageModelCheckpoint(
         update_weights=True, filepath=test_model_filepath
@@ -58,7 +59,7 @@ def test_model_file_creation(tmp_path):
 
 
 def test_mode_auto(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model()
     monitor = "val_loss"
     save_best_only = False
@@ -82,7 +83,7 @@ def test_mode_auto(tmp_path):
 
 
 def test_mode_min(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model()
     monitor = "val_loss"
     save_best_only = False
@@ -106,7 +107,7 @@ def test_mode_min(tmp_path):
 
 
 def test_mode_max(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model()
     mode = "max"
     monitor = "val_acc"
@@ -130,7 +131,7 @@ def test_mode_max(tmp_path):
 
 
 def test_save_best_only(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model()
     save_best_only = True
     avg_model_ckpt = AverageModelCheckpoint(
@@ -148,7 +149,7 @@ def test_save_best_only(tmp_path):
 
 
 def test_metric_unavailable(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     x, y, model = get_data_and_model()
     monitor = "unknown"
     avg_model_ckpt = AverageModelCheckpoint(
@@ -169,7 +170,7 @@ def test_metric_unavailable(tmp_path):
 
 
 def test_save_freq(tmp_path):
-    test_filepath = str(tmp_path.join("test_model.{epoch:02d}.h5"))
+    test_filepath = str(tmp_path / "test_model.{epoch:02d}.h5")
     x, y, model = get_data_and_model()
     save_freq = "epoch"
     avg_model_ckpt = AverageModelCheckpoint(
@@ -191,9 +192,72 @@ def test_save_freq(tmp_path):
 
 
 def test_invalid_save_freq(tmp_path):
-    test_model_filepath = str(tmp_path.join("test_model.h5"))
+    test_model_filepath = str(tmp_path / "test_model.h5")
     save_freq = "invalid_save_freq"
     with pytest.raises(ValueError, match="Unrecognized save_freq"):
         AverageModelCheckpoint(
             update_weights=True, filepath=test_model_filepath, save_freq=save_freq
         )
+
+
+def _get_dummy_resource_for_checkpoint_testing(tmp_path):
+    def get_input_datasets():
+        # Simple training input.
+        train_input = [[1.0]] * 16
+        train_label = [[0.0]] * 16
+        ds = tf.data.Dataset.from_tensor_slices((train_input, train_label))
+        return ds.batch(8, drop_remainder=True)
+
+    # Very simple model to eliminate randomness.
+    optimizer = MovingAverage(
+        tf.keras.optimizers.SGD(lr=2.0), sequential_update=True, average_decay=0.5
+    )
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(units=1, input_shape=(1,)))
+    model.compile(loss="mae", optimizer=optimizer, metrics=["mae"])
+    train_ds = get_input_datasets()
+    filepath = str(tmp_path / "test_model.{epoch:02d}.h5")
+    callback = AverageModelCheckpoint(
+        update_weights=False, filepath=filepath, save_weights_only=True
+    )
+    return model, train_ds, callback, filepath
+
+
+def _run_load_weights_on_restart_test_common_iterations():
+    (model, train_ds, callback, filepath) = _get_dummy_resource_for_checkpoint_testing()
+    initial_epochs = 3
+    model.fit(train_ds, epochs=initial_epochs, callbacks=[callback])
+    # The files should exist after fitting with callback.
+    for epoch in range(initial_epochs):
+        assert os.path.exists(filepath.format(epoch=epoch + 1))
+    model.fit(train_ds, epochs=1)
+    weights_after_one_more_epoch = model.get_weights()
+    # The filepath should continue to exist after fitting without callback.
+    for epoch in range(initial_epochs):
+        assert os.path.exists(filepath.format(epoch=epoch + 1))
+    return model, train_ds, filepath, weights_after_one_more_epoch
+
+
+def get_checkpoint_load_weights_on_restart_true_test():
+    (
+        model,
+        train_ds,
+        filepath,
+        weights_after_one_more_epoch,
+    ) = _run_load_weights_on_restart_test_common_iterations()
+    callback = AverageModelCheckpoint(
+        update_weights=False,
+        filepath=filepath,
+        save_weights_only=True,
+        load_weights_on_restart=True,
+    )
+    model.fit(train_ds, epochs=1, callbacks=[callback])
+    weights_after_model_restoring_and_one_more_epoch = model.get_weights()
+    model.fit(train_ds, epochs=1, callbacks=[callback])
+    weights_with_one_final_extra_epoch = model.get_weights()
+    np.testing.assert_almost_equal(
+        weights_after_one_more_epoch, weights_after_model_restoring_and_one_more_epoch
+    )
+    not np.testing.assert_almost_equal(
+        weights_after_one_more_epoch, weights_with_one_final_extra_epoch
+    )
