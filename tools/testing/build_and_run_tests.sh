e8206f3d431f9a6f084f9b73e0e322af416966b2
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-04-16 15:37:21 +0200

    Enable pytest-xdist when nvidia-smi is not found. (#1681)
    * Enable pytest-xdist when nvidia-smi is not found.
    * Fix a bug when the order of the test changes.
    * Update build_and_run_tests.sh

--- tools/testing/build_and_run_tests.sh
+++ tools/testing/build_and_run_tests.sh
@@ -24,4 +24,12 @@ python -m pip install -r tools/install_deps/pytest.txt -e ./
 python ./configure.py
 bash tools/install_so_files.sh
 python -c "import tensorflow as tf; print(tf.config.list_physical_devices())"
-python -m pytest -v --durations=25 ./tensorflow_addons
+
+# If there are no gpus, we can use multiple workers
+# Multiple workers will be supported with gpus later.
+if ! [ -x "$(command -v nvidia-smi)" ]; then
+  EXTRA_ARGS="-n auto"
+fi
+
+
+python -m pytest -v --durations=25 $EXTRA_ARGS ./tensorflow_addons
