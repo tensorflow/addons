7eda45ac3840ae7101d2d65365e56ff42e0d6f11
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-04-17 22:29:38 +0200

    Removed run_all_in_graph_and_eager_mode multilable confusion matrix. (#1694)

--- tensorflow_addons/metrics/tests/multilabel_confusion_matrix_test.py
+++ tensorflow_addons/metrics/tests/multilabel_confusion_matrix_test.py
@@ -14,143 +14,118 @@
 # ==============================================================================
 """Tests for Multilabel Confusion Matrix Metric."""
 
+import numpy as np
+import pytest
 import tensorflow as tf
 from tensorflow_addons.metrics import MultiLabelConfusionMatrix
-from tensorflow_addons.utils import test_utils
 
 
-@test_utils.run_all_in_graph_and_eager_modes
-class MultiLabelConfusionMatrixTest(tf.test.TestCase):
-    def test_config(self):
-        mcm_obj = MultiLabelConfusionMatrix(num_classes=3)
-        self.assertEqual(mcm_obj.num_classes, 3)
-        self.assertEqual(mcm_obj.dtype, tf.float32)
-        # Check save and restore config
-        mcm_obj2 = MultiLabelConfusionMatrix.from_config(mcm_obj.get_config())
-        self.assertEqual(mcm_obj2.num_classes, 3)
-        self.assertEqual(mcm_obj2.dtype, tf.float32)
+def test_config():
+    mcm_obj = MultiLabelConfusionMatrix(num_classes=3)
+    assert mcm_obj.num_classes == 3
+    assert mcm_obj.dtype == tf.float32
+    # Check save and restore config
+    mcm_obj2 = MultiLabelConfusionMatrix.from_config(mcm_obj.get_config())
+    assert mcm_obj2.num_classes == 3
+    assert mcm_obj2.dtype == tf.float32
 
-    def initialize_vars(self, n_classes, input_dtype):
-        mcm_obj = MultiLabelConfusionMatrix(num_classes=n_classes, dtype=input_dtype)
-        self.evaluate(tf.compat.v1.variables_initializer(mcm_obj.variables))
-        return mcm_obj
 
-    def update_obj_states(self, obj, actuals, preds):
-        update_op = obj.update_state(actuals, preds)
-        self.evaluate(update_op)
+def check_results(obj, value):
+    np.testing.assert_allclose(value, obj.result().numpy(), atol=1e-6)
 
-    def check_results(self, obj, value):
-        self.assertAllClose(value, self.evaluate(obj.result()), atol=1e-5)
 
-    def test_mcm_3_classes(self):
-        for input_dtype in [tf.int32, tf.int64, tf.float32, tf.float64]:
-            actuals = tf.constant(
-                [[1, 0, 1], [0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=input_dtype
-            )
-            preds = tf.constant(
-                [[1, 0, 0], [0, 1, 1], [1, 0, 0], [0, 1, 1]], dtype=input_dtype
-            )
-            # Initialize
-            mcm_obj = self.initialize_vars(n_classes=3, input_dtype=input_dtype)
-            # Update
-            self.update_obj_states(mcm_obj, actuals, preds)
-            # Check results
-            self.check_results(
-                mcm_obj, [[[2, 0], [0, 2]], [[2, 0], [0, 2]], [[0, 2], [2, 0]]]
-            )
+@pytest.mark.parametrize("dtype", [tf.int32, tf.int64, tf.float32, tf.float64])
+def test_mcm_3_classes(dtype):
+    actuals = tf.constant([[1, 0, 1], [0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=dtype)
+    preds = tf.constant([[1, 0, 0], [0, 1, 1], [1, 0, 0], [0, 1, 1]], dtype=dtype)
+    # Initialize
+    mcm_obj = MultiLabelConfusionMatrix(num_classes=3, dtype=dtype)
+    mcm_obj.update_state(actuals, preds)
+    # Check results
+    check_results(mcm_obj, [[[2, 0], [0, 2]], [[2, 0], [0, 2]], [[0, 2], [2, 0]]])
 
-    def test_mcm_4_classes(self):
-        for input_dtype in [tf.int32, tf.int64, tf.float32, tf.float64]:
-            actuals = tf.constant(
-                [
-                    [1, 0, 0, 1],
-                    [0, 0, 1, 1],
-                    [1, 0, 0, 1],
-                    [1, 1, 0, 0],
-                    [0, 1, 0, 1],
-                    [1, 0, 0, 1],
-                    [0, 0, 1, 1],
-                    [1, 0, 0, 1],
-                    [0, 1, 1, 0],
-                    [0, 1, 0, 1],
-                ],
-                dtype=input_dtype,
-            )
-            preds = tf.constant(
-                [
-                    [1, 0, 1, 0],
-                    [0, 0, 1, 1],
-                    [0, 0, 0, 1],
-                    [1, 1, 0, 0],
-                    [1, 0, 0, 0],
-                    [1, 0, 0, 1],
-                    [0, 0, 1, 1],
-                    [1, 0, 0, 1],
-                    [0, 1, 0, 0],
-                    [0, 0, 0, 1],
-                ],
-                dtype=input_dtype,
-            )
 
-            # Initialize
-            mcm_obj = self.initialize_vars(n_classes=4, input_dtype=input_dtype)
-            # Update
-            self.update_obj_states(mcm_obj, actuals, preds)
-            # Check results
-            self.check_results(
-                mcm_obj,
-                [
-                    [[4, 1], [1, 4]],
-                    [[6, 0], [2, 2]],
-                    [[6, 1], [1, 2]],
-                    [[2, 0], [2, 6]],
-                ],
-            )
+@pytest.mark.parametrize("dtype", [tf.int32, tf.int64, tf.float32, tf.float64])
+def test_mcm_4_classes(dtype):
+    actuals = tf.constant(
+        [
+            [1, 0, 0, 1],
+            [0, 0, 1, 1],
+            [1, 0, 0, 1],
+            [1, 1, 0, 0],
+            [0, 1, 0, 1],
+            [1, 0, 0, 1],
+            [0, 0, 1, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+            [0, 1, 0, 1],
+        ],
+        dtype=dtype,
+    )
+    preds = tf.constant(
+        [
+            [1, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 1],
+            [0, 0, 1, 1],
+            [1, 0, 0, 1],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=dtype,
+    )
 
-    def test_multiclass(self):
-        for input_dtype in [tf.int32, tf.int64, tf.float32, tf.float64]:
-            actuals = tf.constant(
-                [
-                    [1, 0, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                    [0, 1, 0, 0],
-                    [0, 1, 0, 0],
-                    [1, 0, 0, 0],
-                    [0, 0, 1, 0],
-                    [1, 0, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ],
-                dtype=input_dtype,
-            )
-            preds = tf.constant(
-                [
-                    [1, 0, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                    [1, 0, 0, 0],
-                    [1, 0, 0, 0],
-                    [1, 0, 0, 0],
-                    [0, 0, 1, 0],
-                    [1, 0, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 0, 1],
-                ],
-                dtype=input_dtype,
-            )
+    # Initialize
+    mcm_obj = MultiLabelConfusionMatrix(num_classes=4, dtype=dtype)
+    mcm_obj.update_state(actuals, preds)
+    # Check results
+    check_results(
+        mcm_obj,
+        [[[4, 1], [1, 4]], [[6, 0], [2, 2]], [[6, 1], [1, 2]], [[2, 0], [2, 6]],],
+    )
 
-            # Initialize
-            mcm_obj = self.initialize_vars(n_classes=4, input_dtype=input_dtype)
-            # Update
-            self.update_obj_states(mcm_obj, actuals, preds)
-            # Check results
-            self.check_results(
-                mcm_obj,
-                [
-                    [[5, 2], [0, 3]],
-                    [[7, 1], [2, 0]],
-                    [[7, 0], [1, 2]],
-                    [[8, 0], [0, 2]],
-                ],
-            )
+
+@pytest.mark.parametrize("dtype", [tf.int32, tf.int64, tf.float32, tf.float64])
+def test_multiclass(dtype):
+    actuals = tf.constant(
+        [
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=dtype,
+    )
+    preds = tf.constant(
+        [
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=dtype,
+    )
+
+    # Initialize
+    mcm_obj = MultiLabelConfusionMatrix(num_classes=4, dtype=dtype)
+    mcm_obj.update_state(actuals, preds)
+    # Check results
+    check_results(
+        mcm_obj,
+        [[[5, 2], [0, 3]], [[7, 1], [2, 0]], [[7, 0], [1, 2]], [[8, 0], [0, 2]],],
+    )
