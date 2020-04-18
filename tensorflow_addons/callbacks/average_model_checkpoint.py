Fixed average_model_checkpoint to have integer save_freq

tensorflow_addons/callbacks/average_model_checkpoint.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # =============================================================================
 
+from typing import Optional
 import tensorflow as tf
 from typeguard import typechecked
 from tensorflow_addons.optimizers.average_wrapper import AveragedOptimizerWrapper
@@ -42,7 +43,7 @@ class AverageModelCheckpoint(tf.keras.callbacks.ModelCheckpoint):
         save_best_only: bool = False,
         save_weights_only: bool = False,
         mode: str = "auto",
-        save_freq: str = "epoch",
+        save_freq: Optional[str, int] = "epoch",
         **kwargs
     ):
         self.update_weights = update_weights