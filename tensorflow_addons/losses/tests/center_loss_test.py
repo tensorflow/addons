8948915ed407984e5e48cc76b5e1588d28ddff90
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-04-19 11:04:02 +0530

    resolving errors

--- tensorflow_addons/losses/tests/center_loss_test.py
+++ tensorflow_addons/losses/tests/center_loss_test.py
@@ -16,20 +16,20 @@
 
 import tensorflow as tf
 from tensorflow_addons.utils import test_utils
-from tensorflow_addons.losses import center_loss
+from tensorflow_addons.losses import center_loss, CenterLoss
 
 
 @test_utils.run_all_in_graph_and_eager_modes
 class CenterLossTest(tf.test.TestCase):
     def test_config(self):
-        center_obj = center_loss.CenterLoss(
+        center_obj = CenterLoss(
             reduction=tf.keras.losses.Reduction.NONE, name="center_loss"
         )
         self.assertEqual(center_obj.name, "center_loss")
         self.assertEqual(center_obj.reduction, tf.keras.losses.Reduction.NONE)
 
     def test_zero_loss(self):
-        center_obj = center_loss.CenterLoss()
+        center_obj = CenterLoss()
         y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
         y_pred = tf.constant([1.0, 1.0, 0.0, 0.0, 1.0, 0.0], dtype=tf.dtypes.float32)
         loss = center_obj(y_true, y_pred)
@@ -37,9 +37,9 @@ class CenterLossTest(tf.test.TestCase):
 
     def test_keras_model_compile(self):
         model = tf.keras.models.Sequential(
-            [tf.keras.layers.Input(shape=(784,)), tf.keras.layers.Dense(10),]
+            [tf.keras.layers.Dense(400), tf.keras.layers.Dense(10),]
         )
-        model.compile(loss="Addons>center_loss", optimizer="adam")
+        model.compile(loss=center_loss, optimizer="adam")
 
 
 if __name__ == "__main__":
