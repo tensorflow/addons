210f24e0fcc6736be18be4fd4c44ab3785dfbaf6
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-04-19 10:49:55 +0530

    Resolving errors

--- tensorflow_addons/losses/tests/center_loss_test.py
+++ tensorflow_addons/losses/tests/center_loss_test.py
@@ -14,28 +14,33 @@
 # ==============================================================================
 """Tests for center loss."""
 
-import numpy as np
 import tensorflow as tf
-from tensorflow_addons.losses import center_loss, CenterLoss
+from tensorflow_addons.utils import test_utils
+from tensorflow_addons.losses import center_loss
 
 
-def test_config():
-    center_obj = CenterLoss(
-        reduction=tf.keras.losses.Reduction.NONE, name="center_loss"
-    )
-    np.testing.assert_equal(center_obj.name, "center_loss")
-    np.testing.assert_equal(center_obj.reduction, tf.keras.losses.Reduction.NONE)
+@test_utils.run_all_in_graph_and_eager_modes
+class CenterLossTest(tf.test.TestCase):
+    def test_config(self):
+        center_obj = center_loss.CenterLoss(
+            reduction=tf.keras.losses.Reduction.NONE, name="center_loss"
+        )
+        self.assertEqual(center_obj.name, "center_loss")
+        self.assertEqual(center_obj.reduction, tf.keras.losses.Reduction.NONE)
+
+    def test_zero_loss(self):
+        center_obj = center_loss.CenterLoss()
+        y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
+        y_pred = tf.constant([1.0, 1.0, 0.0, 0.0, 1.0, 0.0], dtype=tf.dtypes.float32)
+        loss = center_obj(y_true, y_pred)
+        self.assertAllClose(loss, 0.0)
+
+    def test_keras_model_compile(self):
+        model = tf.keras.models.Sequential(
+            [tf.keras.layers.Input(shape=(784,)), tf.keras.layers.Dense(10),]
+        )
+        model.compile(loss="Addons>center_loss", optimizer="adam")
 
 
-def test_zero_loss():
-    y_true = tf.constant([0, 0, 1, 1, 0, 1], dtype=tf.dtypes.int64)
-    y_pred = tf.constant([1.0, 1.0, 0.0, 0.0, 1.0, 0.0], dtype=tf.dtypes.float32)
-    loss = center_loss(y_true, y_pred)
-    np.testing.assert_allclose(loss, 0.0)
-
-
-def test_keras_model_compile():
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Dense(400))
-    model.add(tf.keras.layers.Dense(10))
-    model.compile(loss=center_loss, optimizer="adam")
+if __name__ == "__main__":
+    tf.test.main()
