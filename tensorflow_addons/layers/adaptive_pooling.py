3d29d03bfac9db5e93ba49f61401b9538ea5b541
Susmit-A <susmit600@gmail.com>
2020-04-20 04:33:15 +0530

    Added get_config

--- tensorflow_addons/layers/adaptive_pooling.py
+++ tensorflow_addons/layers/adaptive_pooling.py
@@ -21,8 +21,7 @@ import tensorflow as tf
 class AdaptiveAveragePooling2D(tf.keras.layers.Layer):
     """Average Pooling with adaptive kernel size and strides.
         Arguments:
-            h_bins: Output Height
-            w_bins: Output Width
+            output_shape: Tuple of integers specifying (Output Height, Output Width)
 
         Input shape:
             - If `data_format='channels_last'`:
@@ -37,14 +36,11 @@ class AdaptiveAveragePooling2D(tf.keras.layers.Layer):
                 4D tensor with shape `(batch_size, features, h_bins, w_bins)`.
     """
 
-    def __init__(self, h_bins, w_bins, data_format="channels_last", **kwargs):
-        self._h_bins = h_bins
-        self._w_bins = w_bins
+    def __init__(self, output_shape, data_format="channels_last", **kwargs):
+        self._h_bins = output_shape[0]
+        self._w_bins = output_shape[1]
         self.data_format = data_format
         super().__init__(**kwargs)
-
-    def build(self, input_shape):
-        super().build(input_shape)
 
     def call(self, inputs, *args):
         if self.data_format == "channels_last":
@@ -60,3 +56,15 @@ class AdaptiveAveragePooling2D(tf.keras.layers.Layer):
             split_rows = tf.stack(split_rows, axis=4)
             out_vect = tf.reduce_mean(split_rows, axis=[3, 5])
         return out_vect
+
+    def compute_output_shape(self, input_shape):
+        input_shape = input_shape.as_list()
+        return [input_shape[0], self._h_bins, self._w_bins, input_shape[-1]]
+
+    def get_config(self):
+        config = {
+            'output_shape': (self._h_bins, self._w_bins),
+            'data_format': self.data_format
+        }
+        base_config = super().get_config()
+        return {**base_config, **config}
