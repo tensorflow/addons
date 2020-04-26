102e46695e847d33ddaa2e51e472c4f362895c22
Henry Eigen <eigen.henry@gmail.com>
2020-04-26 13:59:49 -0400

    added hard angular test

--- tensorflow_addons/losses/tests/triplet_test.py
+++ tensorflow_addons/losses/tests/triplet_test.py
@@ -183,6 +183,43 @@ def triplet_semihard_loss_angular_np(labels, embedding, margin):
     return loss_np
 
 
+def triplet_hard_loss_angular_np(labels, embedding, margin, soft=False):
+
+    num_data = embedding.shape[0]
+    # Reshape labels to compute adjacency matrix.
+    labels_reshaped = np.reshape(labels.astype(np.float32), (labels.shape[0], 1))
+
+    adjacency = np.equal(labels_reshaped, labels_reshaped.T)
+    pdist_matrix = angular_distance_np(embedding, squared=True)
+    loss_np = 0.0
+    for i in range(num_data):
+        pos_distances = []
+        neg_distances = []
+        for j in range(num_data):
+            if adjacency[i][j] == 0:
+                neg_distances.append(pdist_matrix[i][j])
+            if adjacency[i][j] > 0.0 and i != j:
+                pos_distances.append(pdist_matrix[i][j])
+
+        # if there are no positive pairs, distance is 0
+        if len(pos_distances) == 0:
+            pos_distances.append(0)
+
+        # Sort by distance.
+        neg_distances.sort()
+        min_neg_distance = neg_distances[0]
+        pos_distances.sort(reverse=True)
+        max_pos_distance = pos_distances[0]
+
+        if soft:
+            loss_np += np.log1p(np.exp(max_pos_distance - min_neg_distance))
+        else:
+            loss_np += np.maximum(0.0, max_pos_distance - min_neg_distance + margin)
+
+    loss_np /= num_data
+    return loss_np
+
+
 @pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
 def test_semihard_tripled_loss(dtype):
     num_data = 10
