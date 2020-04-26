89111ff7e05a3ec72d75190e58913913c6c6655f
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-04-26 11:14:35 +0000

    CRF layer v3.0

--- tensorflow_addons/layers/tests/crf_test.py
+++ tensorflow_addons/layers/tests/crf_test.py
@@ -24,6 +24,7 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow_addons.layers.crf import CRF, CRFLossLayer
+from tensorflow_addons.text.crf import crf_log_likelihood
 
 
 def get_test_data():
@@ -111,6 +112,82 @@ def test_unmasked_viterbi_decode():
     assert decoded_sequence.dtype == np.int32
 
 
+def expand_1d(data):
+    """Expands 1-dimensional `Tensor`s into 2-dimensional `Tensor`s."""
+
+    def _expand_single_1d_tensor(t):
+        # Leaves `CompositeTensor`s as-is.
+        if (
+            isinstance(t, tf.Tensor)
+            and isinstance(t.shape, tf.TensorShape)
+            and t.shape.rank == 1
+        ):
+            return tf.expand_dims(t, axis=-1)
+        return t
+
+    return tf.nest.map_structure(_expand_single_1d_tensor, data)
+
+
+def unpack_x_y_sample_weight(data):
+    """Unpacks user-provided data tuple."""
+    if not isinstance(data, tuple):
+        return data, None, None
+    elif len(data) == 1:
+        return data[0], None, None
+    elif len(data) == 2:
+        return data[0], data[1], None
+    elif len(data) == 3:
+        return data[0], data[1], data[2]
+
+    raise ValueError("Data not understood.")
+
+
+class ModelWithCRFLoss(tf.keras.models.Model):
+    """Wrapper around the base model for custom training logic."""
+
+    def __init__(self, base_model):
+        super().__init__()
+        self.base_model = base_model
+
+    def call(self, inputs):
+        return self.base_model(inputs)
+
+    def compute_loss(self, x, y, sample_weights, training):
+        y_pred = self(x, training=training)
+        _, potentials, sequence_length, chain_kernel = y_pred
+
+        # we now add the CRF loss:
+        crf_loss = -crf_log_likelihood(potentials, y, sequence_length, chain_kernel)[0]
+
+        if sample_weights is not None:
+            crf_loss = crf_loss * sample_weights
+
+        return tf.reduce_sum(crf_loss), sum(self.losses)
+
+    def train_step(self, data):
+        data = expand_1d(data)
+        x, y, sample_weight = unpack_x_y_sample_weight(data)
+
+        with tf.GradientTape() as tape:
+            crf_loss, internal_losses = self.compute_loss(
+                x, y, sample_weight, training=True
+            )
+            total_loss = crf_loss + internal_losses
+
+        gradients = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        return {"crf_loss": crf_loss, "internal_losses": internal_losses}
+
+    def test_step(self, data):
+        data = expand_1d(data)
+        x, y, sample_weight = unpack_x_y_sample_weight(data)
+        crf_loss, internal_losses = self.compute_loss(
+            x, y, sample_weight, training=False
+        )
+        return {"crf_loss_val": crf_loss, "internal_losses_val": internal_losses}
+
+
 @pytest.mark.usefixtures("maybe_run_functions_eagerly")
 def test_traing():
     logits, tags, _, _, crf_layer = get_test_data_extended()
@@ -125,23 +202,16 @@ def test_traing2():
 
 def train_some_model(x_np, y_np):
     x_input = tf.keras.layers.Input(shape=x_np.shape[1:])
-    y_input = tf.keras.layers.Input(shape=y_np.shape[1:])
-
     crf_outputs = CRF(5, name="L")(x_input)
-    decoded_sequence, potentials, sequence_length, chain_kernel = crf_outputs
+    base_model = tf.keras.Model(x_input, crf_outputs)
 
-    crf_loss = CRFLossLayer()([potentials, y_input, sequence_length, chain_kernel])
+    wrapper_model = ModelWithCRFLoss(base_model)
 
-    inference_model = tf.keras.Model(x_input, decoded_sequence)
-    training_model = tf.keras.Model([x_input, y_input], crf_loss)
-
-    training_model.compile("adam", loss="mae")
-    training_model.fit((x_np, y_np), y=np.zeros((2,)))
-    training_model.evaluate((x_np, y_np), y=np.zeros((2,)))
-
-    inference_model.predict(x_np)
-
-    return inference_model, training_model
+    wrapper_model.compile("adam")
+    wrapper_model.fit(x=x_np, y=y_np)
+    wrapper_model.evaluate(x_np, y_np)
+    wrapper_model.predict(x_np)
+    return wrapper_model
 
 
 @pytest.mark.usefixtures("maybe_run_functions_eagerly")
@@ -189,48 +259,27 @@ def test_mask_left_padding():
     assert "CRF layer do not support left padding" in str(context.value)
 
 
-def clone(model: tf.keras.Model, save_format: str):
+def clone(model: tf.keras.Model):
 
     with tempfile.TemporaryDirectory() as tmpdir:
         file_path = os.path.join(tmpdir, "my_model.tf")
-        model.save(file_path, save_format=save_format)
+        model.save(file_path)
         return tf.keras.models.load_model(file_path)
 
 
 def assert_all_equal(array_list1, array_list2):
     for arr1, arr2 in zip(array_list1, array_list2):
-        np.testing.assert_equal(arr1, arr2)
+        np.testing.assert_equal(np.array(arr1), np.array(arr2))
 
 
-@pytest.mark.parametrize("save_format", ["h5", "tf"])
-def test_serialization(save_format):
+def test_serialization():
 
     x_np, y_np = get_test_data()
-    inference_model, training_model = train_some_model(x_np, y_np)
+    model = train_some_model(x_np, y_np)
 
-    assert inference_model.get_layer("L") == training_model.get_layer("L")
-
-    new_inference_model = clone(inference_model, save_format)
-    np.testing.assert_equal(
-        inference_model(x_np).numpy(), new_inference_model(x_np).numpy()
-    )
-    assert_all_equal(
-        inference_model.get_layer("L").get_weights(),
-        new_inference_model.get_layer("L").get_weights(),
-    )
-
-    new_training_model = clone(training_model, save_format)
-
-    if save_format == "tf":
-        pytest.skip("TODO: fixme. Some strange bug with TF model saving.")
-
-    np.testing.assert_equal(
-        training_model([x_np, y_np]).numpy(), new_training_model([x_np, y_np]).numpy()
-    )
-    assert_all_equal(
-        training_model.get_layer("L").get_weights(),
-        new_training_model.get_layer("L").get_weights(),
-    )
+    new_model = clone(model)
+    assert_all_equal(model.predict(x_np), new_model.predict(x_np))
+    assert_all_equal(model.get_weights(), new_model.get_weights())
 
 
 def test_numerical_accuracy():
