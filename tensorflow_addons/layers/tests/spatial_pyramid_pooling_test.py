0218640bb55b9f5b730a209fe355cb9a59914de5
Susmit-A <susmit600@gmail.com>
2020-04-27 16:41:08 +0530

    Corrected documentation

--- tensorflow_addons/layers/tests/spatial_pyramid_pooling_test.py
+++ tensorflow_addons/layers/tests/spatial_pyramid_pooling_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for AdaptivePooling layers."""
+"""Tests for SpatialPyramidPooling layers"""
 
 import pytest
 import numpy as np
