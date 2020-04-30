3472e92f2539ad6c7759c4967bc1f78fb662883e
Sean Morgan <seanmorgan@outlook.com>
2020-04-30 14:58:06 -0400

    Fix master: Keep pip version constant (#1760)
    * Remove pip upgrade
    
    * Remove pip upgrades that are not needed
    
    * debug
    
    * debug
    
    * debug
    
    * Try with github token
    
    * Fix bug
    
    * temp
    
    * Update

--- tools/install_deps/bazel_macos.sh
+++
@@ -1,4 +0,0 @@
-set -e -x
-wget --quiet -nc https://github.com/bazelbuild/bazel/releases/download/${1}/bazel-${1}-installer-darwin-x86_64.sh
-chmod +x bazel-${1}-installer-darwin-x86_64.sh
-./bazel-${1}-installer-darwin-x86_64.sh --user
