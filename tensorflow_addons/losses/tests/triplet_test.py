fcbd8f9cb5f40dc74f1374d54c56875c7653bec0
Henry Eigen <eigen.henry@gmail.com>
2020-04-29 20:35:53 -0400

    updated tests

--- tensorflow_addons/losses/tests/triplet_test.py
+++ tensorflow_addons/losses/tests/triplet_test.py
@@ -51,6 +51,14 @@ def pairwise_distance_np(feature, squared=False):
     return pairwise_distances
 
 
+def L_2_dists(embs):
+    return pairwise_distance_np(embs, True)
+
+
+def L_1_dists(embs):
+    return pairwise_distance_np(embs, False)
+
+
 def angular_distance_np(feature):
     """Computes the angular distance matrix in numpy.
     Args:
@@ -70,14 +78,14 @@ def angular_distance_np(feature):
     return inverse_cos_sim
 
 
-def triplet_semihard_loss_np(labels, embedding, margin):
+def triplet_semihard_loss_np(labels, embedding, margin, dist_func):
 
     num_data = embedding.shape[0]
     # Reshape labels to compute adjacency matrix.
     labels_reshaped = np.reshape(labels.astype(np.float32), (labels.shape[0], 1))
 
     adjacency = np.equal(labels_reshaped, labels_reshaped.T)
-    pdist_matrix = pairwise_distance_np(embedding, squared=True)
+    pdist_matrix = dist_func(embedding)
     loss_np = 0.0
     num_positives = 0.0
     for i in range(num_data):
@@ -107,14 +115,14 @@ def triplet_semihard_loss_np(labels, embedding, margin):
     return loss_np
 
 
-def triplet_hard_loss_np(labels, embedding, margin, soft=False):
+def triplet_hard_loss_np(labels, embedding, margin, dist_func, soft=False):
 
     num_data = embedding.shape[0]
     # Reshape labels to compute adjacency matrix.
     labels_reshaped = np.reshape(labels.astype(np.float32), (labels.shape[0], 1))
 
     adjacency = np.equal(labels_reshaped, labels_reshaped.T)
-    pdist_matrix = pairwise_distance_np(embedding, squared=True)
+    pdist_matrix = dist_func(embedding)
     loss_np = 0.0
     for i in range(num_data):
         pos_distances = []
@@ -144,81 +152,9 @@ def triplet_hard_loss_np(labels, embedding, margin, soft=False):
     return loss_np
 
 
-def triplet_semihard_loss_angular_np(labels, embedding, margin):
-
-    num_data = embedding.shape[0]
-    # Reshape labels to compute adjacency matrix.
-    labels_reshaped = np.reshape(labels.astype(np.float32), (labels.shape[0], 1))
-
-    adjacency = np.equal(labels_reshaped, labels_reshaped.T)
-    pdist_matrix = angular_distance_np(embedding)
-    loss_np = 0.0
-    num_positives = 0.0
-    for i in range(num_data):
-        for j in range(num_data):
-            if adjacency[i][j] > 0.0 and i != j:
-                num_positives += 1.0
-
-                pos_distance = pdist_matrix[i][j]
-                neg_distances = []
-
-                for k in range(num_data):
-                    if adjacency[i][k] == 0:
-                        neg_distances.append(pdist_matrix[i][k])
-
-                # Sort by distance.
-                neg_distances.sort()
-                chosen_neg_distance = neg_distances[0]
-
-                for l in range(len(neg_distances)):
-                    chosen_neg_distance = neg_distances[l]
-                    if chosen_neg_distance > pos_distance:
-                        break
-                loss_np += np.maximum(0.0, margin - chosen_neg_distance + pos_distance)
-
-    loss_np /= num_positives
-    return loss_np
-
-
-def triplet_hard_loss_angular_np(labels, embedding, margin, soft=False):
-
-    num_data = embedding.shape[0]
-    # Reshape labels to compute adjacency matrix.
-    labels_reshaped = np.reshape(labels.astype(np.float32), (labels.shape[0], 1))
-
-    adjacency = np.equal(labels_reshaped, labels_reshaped.T)
-    pdist_matrix = angular_distance_np(embedding)
-    loss_np = 0.0
-    for i in range(num_data):
-        pos_distances = []
-        neg_distances = []
-        for j in range(num_data):
-            if adjacency[i][j] == 0:
-                neg_distances.append(pdist_matrix[i][j])
-            if adjacency[i][j] > 0.0 and i != j:
-                pos_distances.append(pdist_matrix[i][j])
-
-        # if there are no positive pairs, distance is 0
-        if len(pos_distances) == 0:
-            pos_distances.append(0)
-
-        # Sort by distance.
-        neg_distances.sort()
-        min_neg_distance = neg_distances[0]
-        pos_distances.sort(reverse=True)
-        max_pos_distance = pos_distances[0]
-
-        if soft:
-            loss_np += np.log1p(np.exp(max_pos_distance - min_neg_distance))
-        else:
-            loss_np += np.maximum(0.0, max_pos_distance - min_neg_distance + margin)
-
-    loss_np /= num_data
-    return loss_np
-
-
+# test cosine similarity
 @pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
-def test_semihard_tripled_loss(dtype):
+def test_semihard_tripled_loss_angular(dtype):
     num_data = 10
     feat_dim = 6
     margin = 1.0
@@ -228,12 +164,56 @@ def test_semihard_tripled_loss(dtype):
     labels = np.random.randint(0, num_classes, size=(num_data))
 
     # Compute the loss in NP.
-    loss_np = triplet_semihard_loss_np(labels, embedding, margin)
+    loss_np = triplet_semihard_loss_np(labels, embedding, margin, angular_distance_np)
+
+    # Compute the loss in TF.
+    y_true = tf.constant(labels)
+    y_pred = tf.constant(embedding, dtype=dtype)
+    cce_obj = triplet.TripletSemiHardLoss(distance_metric="angular")
+    loss = cce_obj(y_true, y_pred)
+    test_utils.assert_allclose_according_to_type(loss.numpy(), loss_np)
+
+
+# test L-2 distance
+@pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
+def test_semihard_tripled_loss_l2(dtype):
+    num_data = 10
+    feat_dim = 6
+    margin = 1.0
+    num_classes = 4
+
+    embedding = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data))
+
+    # Compute the loss in NP.
+    loss_np = triplet_semihard_loss_np(labels, embedding, margin, L_2_dists)
 
     # Compute the loss in TF.
     y_true = tf.constant(labels)
     y_pred = tf.constant(embedding, dtype=dtype)
     cce_obj = triplet.TripletSemiHardLoss()
+    loss = cce_obj(y_true, y_pred)
+    test_utils.assert_allclose_according_to_type(loss.numpy(), loss_np)
+
+
+# test L-1 distance
+@pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
+def test_semihard_tripled_loss_l1(dtype):
+    num_data = 10
+    feat_dim = 6
+    margin = 1.0
+    num_classes = 4
+
+    embedding = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data))
+
+    # Compute the loss in NP.
+    loss_np = triplet_semihard_loss_np(labels, embedding, margin, L_1_dists)
+
+    # Compute the loss in TF.
+    y_true = tf.constant(labels)
+    y_pred = tf.constant(embedding, dtype=dtype)
+    cce_obj = triplet.TripletSemiHardLoss(distance_metric="L_1")
     loss = cce_obj(y_true, y_pred)
     test_utils.assert_allclose_according_to_type(loss.numpy(), loss_np)
 
@@ -250,9 +230,10 @@ def test_serialization_semihard():
     tf.keras.losses.deserialize(tf.keras.losses.serialize(loss))
 
 
+# test cosine similarity
 @pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
 @pytest.mark.parametrize("soft", [False, True])
-def test_hard_tripled_loss(dtype, soft):
+def test_hard_tripled_loss_angular(dtype, soft):
     num_data = 20
     feat_dim = 6
     margin = 1.0
@@ -262,12 +243,58 @@ def test_hard_tripled_loss(dtype, soft):
     labels = np.random.randint(0, num_classes, size=(num_data))
 
     # Compute the loss in NP.
-    loss_np = triplet_hard_loss_np(labels, embedding, margin, soft)
+    loss_np = triplet_hard_loss_np(labels, embedding, margin, angular_distance_np, soft)
 
     # Compute the loss in TF.
     y_true = tf.constant(labels)
     y_pred = tf.constant(embedding, dtype=dtype)
-    cce_obj = triplet.TripletHardLoss(soft=soft)
+    cce_obj = triplet.TripletHardLoss(soft=soft, distance_metric="angular")
+    loss = cce_obj(y_true, y_pred)
+    test_utils.assert_allclose_according_to_type(loss.numpy(), loss_np)
+
+
+# test L-2 distance
+@pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
+@pytest.mark.parametrize("soft", [False, True])
+def test_hard_tripled_loss_l2(dtype, soft):
+    num_data = 20
+    feat_dim = 6
+    margin = 1.0
+    num_classes = 4
+
+    embedding = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data))
+
+    # Compute the loss in NP.
+    loss_np = triplet_hard_loss_np(labels, embedding, margin, L_2_dists, soft)
+
+    # Compute the loss in TF.
+    y_true = tf.constant(labels)
+    y_pred = tf.constant(embedding, dtype=dtype)
+    cce_obj = triplet.TripletHardLoss(soft=soft, distance_metric="L_2")
+    loss = cce_obj(y_true, y_pred)
+    test_utils.assert_allclose_according_to_type(loss.numpy(), loss_np)
+
+
+# test L-1 distance
+@pytest.mark.parametrize("dtype", [tf.float32, tf.float16, tf.bfloat16])
+@pytest.mark.parametrize("soft", [False, True])
+def test_hard_tripled_loss_l1(dtype, soft):
+    num_data = 20
+    feat_dim = 6
+    margin = 1.0
+    num_classes = 4
+
+    embedding = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data))
+
+    # Compute the loss in NP.
+    loss_np = triplet_hard_loss_np(labels, embedding, margin, L_1_dists, soft)
+
+    # Compute the loss in TF.
+    y_true = tf.constant(labels)
+    y_pred = tf.constant(embedding, dtype=dtype)
+    cce_obj = triplet.TripletHardLoss(soft=soft, distance_metric="L_1")
     loss = cce_obj(y_true, y_pred)
     test_utils.assert_allclose_according_to_type(loss.numpy(), loss_np)
 
