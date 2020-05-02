d47e40f43cde987bb7282d3db5cf4eaaffe211d7
bhack <bhack@users.noreply.github.com>
2020-05-02 19:47:50 +0200

    Add a small script to change version

---
+++ update_release_version.sh
@@ -0,0 +1,7 @@
+sed -ri "s/(TF_VERSION=|tensorflow(-cpu)*(~|=)=|tf-version: \[')[0-9]+[a-zA-Z0-9_.-]+/\1$1/g" \
+	.github/workflows/release.yml \
+	CONTRIBUTING.md \
+	tools/docker/cpu_tests.Dockerfile \
+	tools/install_deps/tensorflow-cpu.txt \
+	tools/install_deps/tensorflow.txt \
+	tools/run_gpu_tests.sh
