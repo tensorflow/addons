61559b359150c4aecd5d203f8e03c8febb7cde39
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-05-03 08:29:56 +0000

    Upload the cache.

--- .github/workflows/make_wheel_Linux.sh
+++ .github/workflows/make_wheel_Linux.sh
@@ -1,13 +1,18 @@
-set -e -x
+set -e
 
 df -h
 docker info
 # to get more disk space
 rm -rf /usr/share/dotnet &
 
-DOCKER_BUILDKIT=1 docker build \
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx create --driver docker-container --use
+
+docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
+docker buildx build \
     -f tools/docker/build_wheel.Dockerfile \
     --output type=local,dest=wheelhouse \
+    --cache-to=type=registry,ref=gabrieldemarmiesse/my_pretty_cache,mode=max \
     --build-arg PY_VERSION \
     --build-arg TF_VERSION \
     --build-arg NIGHTLY_FLAG \
