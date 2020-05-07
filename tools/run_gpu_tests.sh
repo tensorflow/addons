9252760ed74354cf8ca641a3acd69989810d34f1
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2020-05-07 15:05:37 +0000

    Bump the tensorflow version to stable.

--- tools/run_gpu_tests.sh
+++ tools/run_gpu_tests.sh
@@ -6,7 +6,7 @@ export DOCKER_BUILDKIT=1
 docker build \
        -f tools/docker/build_wheel.Dockerfile \
        --target tfa_gpu_tests \
-       --build-arg TF_VERSION=2.2.0rc4 \
+       --build-arg TF_VERSION=2.2.0 \
        --build-arg PY_VERSION=3.5 \
        -t tfa_gpu_tests ./
 docker run --rm -t -v cache_bazel:/root/.cache/bazel --gpus=all tfa_gpu_tests
