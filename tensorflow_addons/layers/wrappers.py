deea3cdd1d85fc44fefead39c1520bf571382220
Mokke Meguru <meguru.mokke@gmail.com>
2020-05-08 16:33:05 +0900

    format for black

--- tensorflow_addons/layers/wrappers.py
+++ tensorflow_addons/layers/wrappers.py
@@ -23,7 +23,6 @@ from typeguard import typechecked
 class WeightNormalization(tf.keras.layers.Wrapper):
     """This wrapper reparameterizes a layer by decoupling the weight's
     magnitude and direction.
-
     This speeds up convergence by improving the
     conditioning of the optimization problem.
     Weight Normalization: A Simple Reparameterization to Accelerate
@@ -64,7 +63,8 @@ class WeightNormalization(tf.keras.layers.Wrapper):
         if self.data_init and self.is_rnn:
             logging.warning(
                 "WeightNormalization: Using `data_init=True` with RNNs "
-                "is advised against by the paper. Use `data_init=False`.")
+                "is advised against by the paper. Use `data_init=False`."
+            )
 
     def build(self, input_shape):
         """Build `Layer`"""
@@ -77,8 +77,10 @@ class WeightNormalization(tf.keras.layers.Wrapper):
         kernel_layer = self.layer.cell if self.is_rnn else self.layer
 
         if not hasattr(kernel_layer, "kernel"):
-            raise ValueError("`WeightNormalization` must wrap a layer that"
-                             " contains a `kernel` for weights")
+            raise ValueError(
+                "`WeightNormalization` must wrap a layer that"
+                " contains a `kernel` for weights"
+            )
 
         if self.is_rnn:
             kernel = kernel_layer.recurrent_kernel
@@ -94,7 +96,8 @@ class WeightNormalization(tf.keras.layers.Wrapper):
             shape=(self.layer_depth,),
             initializer="ones",
             dtype=kernel.dtype,
-            trainable=True)
+            trainable=True,
+        )
         self.v = kernel
 
         self._initialized = self.add_weight(
@@ -102,7 +105,8 @@ class WeightNormalization(tf.keras.layers.Wrapper):
             shape=None,
             initializer="zeros",
             dtype=tf.dtypes.bool,
-            trainable=False)
+            trainable=False,
+        )
 
         if self.data_init:
             # Used for data initialization in self._data_dep_init.
@@ -154,9 +158,13 @@ class WeightNormalization(tf.keras.layers.Wrapper):
         The initial value of g could either from the initial value in v,
         or by the input value if self.data_init is True.
         """
-        with tf.control_dependencies([
+        with tf.control_dependencies(
+            [
                 tf.debugging.assert_equal(  # pylint: disable=bad-continuation
-                    self._initialized, False, message='The layer has been initialized.')]):
+                    self._initialized, False, message="The layer has been initialized."
+                )
+            ]
+        ):
             if self.data_init:
                 assign_tensors = self._data_dep_init(inputs)
             else:
@@ -184,19 +192,16 @@ class WeightNormalization(tf.keras.layers.Wrapper):
         if replica_ctx:
             # define each GPU's variables
             local_sum = tf.reduce_sum(y, axis=axes, keepdims=True)
-            local_squared_sum = tf.reduce_sum(tf.math.square(y), axes,
-                                              keepdims=True)
+            local_squared_sum = tf.reduce_sum(tf.math.square(y), axes, keepdims=True)
             batch_size = tf.cast(tf.shape(y)[0], tf.float32)
 
             # reduce all GPUs' variables
-            y_sum, y_squared_sum, global_batch_size = (
-                replica_ctx.all_reduce(
-                    tf.distribute.ReduceOp.SUM,
-                    [local_sum, local_squared_sum, batch_size]))
+            y_sum, y_squared_sum, global_batch_size = replica_ctx.all_reduce(
+                tf.distribute.ReduceOp.SUM, [local_sum, local_squared_sum, batch_size]
+            )
 
             axes_vals = [(tf.shape(y))[i] for i in range(1, len(axes))]
-            multiplier = tf.cast(tf.reduce_prod(axes_vals),
-                                 tf.float32)
+            multiplier = tf.cast(tf.reduce_prod(axes_vals), tf.float32)
             multiplier = multiplier * global_batch_size
             mean = y_sum / multiplier
             y_squared_mean = y_squared_sum / multiplier
@@ -205,10 +210,8 @@ class WeightNormalization(tf.keras.layers.Wrapper):
         else:
             mean = tf.reduce_meam(y, axes, keepdims=True)
             variance = tf.reduce_mean(
-                tf.squared_difference(
-                    y, tf.stop_gradient(mean)),
-                axes,
-                keepdims=True)
+                tf.squared_difference(y, tf.stop_gradient(mean)), axes, keepdims=True
+            )
         if x.dtype == tf.float16:
             return (tf.cast(mean, tf.float16), tf.cast(variance, tf.float16))
         else:
@@ -222,7 +225,7 @@ class WeightNormalization(tf.keras.layers.Wrapper):
             data_norm_axes = list(range(x_init.shape.rank - 1))
 
             m_init, v_init = self._calculate_moments(x_init, data_norm_axes)
-            scale_init = 1. / tf.math.sqrt(v_init + 1e-10)
+            scale_init = 1.0 / tf.math.sqrt(v_init + 1e-10)
 
             # RNNs have fused kernels that are tiled
             # Repeat scale_init to match the shape of fused kernel
@@ -248,11 +251,12 @@ class WeightNormalization(tf.keras.layers.Wrapper):
     def remove(self):
         kernel = tf.Variable(
             tf.nn.l2_normalize(self.v, axis=self.kernel_norm_axes) * self.g,
-            name="recurrent_kernel" if self.is_rnn else "kernel")
+            name="recurrent_kernel" if self.is_rnn else "kernel",
+        )
 
         if self.is_rnn:
             self.layer.cell.recurrent_kernel = kernel
         else:
             self.layer.kernel = kernel
 
-        return self.layer
+        return self.layer  # Copyright 2019 The TensorFlow Authors. All Rights Reserved.
