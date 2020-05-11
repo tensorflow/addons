b78795c3b4bcf5f79ee951c51bd9a083106f62f1
pedrolarben <pedrolarabenitez@gmail.com>
2020-05-11 23:55:47 +0200

    Improved ESNCell error message description

--- tensorflow_addons/rnn/cell.py
+++ tensorflow_addons/rnn/cell.py
@@ -704,7 +704,10 @@ class ESNCell(keras.layers.AbstractRNNCell):
     def build(self, inputs_shape):
         input_size = tf.compat.dimension_value(tf.TensorShape(inputs_shape)[-1])
         if input_size is None:
-            raise ValueError("Could not infer input size from inputs.get_shape()[-1]")
+            raise ValueError(
+                "Could not infer input size from inputs.get_shape()[-1]. Shape received is %s"
+                % inputs_shape
+            )
 
         def _esn_recurrent_initializer(shape, dtype, partition_info=None):
             recurrent_weights = tf.keras.initializers.get(self.recurrent_initializer)(
