eb9d6e5e85e17bab5523aeef6763d49eb91625c0
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2020-05-17 02:02:35 +0530

    Remove `sequential_update` from AverageWrapper (#1807)
    * Remove `sequential_update` from AverageWrapper
    
    In TF2.0, sequential_update is redundant. This allows
    the removal of `tf.control_dependencies` from
    average_wrapper and its subclasses: moving_average
    and stochastic_weight_averaging.
    
    * Revert "Remove `sequential_update` from AverageWrapper"
    
    This reverts commit 7cf4201d8335b7cffce7ae4df49d3b70fde0d5d6.
    
    * Remove `tf.control_dependencies` from AverageWrapper
    
    Add deprecation warning for `sequential_update`.
    
    * Set type of sequential_update to Optional[bool]
    
    `sequential_update` is no longer part of the optimizer's
    configuration. Loading an older configuration throws the
    DeprecationWarning.
    
    * black format

--- tensorflow_addons/optimizers/tests/moving_average_test.py
+++ tensorflow_addons/optimizers/tests/moving_average_test.py
@@ -22,8 +22,7 @@ from tensorflow_addons.optimizers import MovingAverage
 
 
 @pytest.mark.usefixtures("maybe_run_functions_eagerly")
-@pytest.mark.parametrize("sequential_update", [True, False])
-def test_run(sequential_update):
+def test_run():
     var0 = tf.Variable([1.0, 2.0])
     var1 = tf.Variable([3.0, 4.0])
 
@@ -32,11 +31,7 @@ def test_run(sequential_update):
 
     grads_and_vars = list(zip([grads0, grads1], [var0, var1]))
 
-    opt = MovingAverage(
-        tf.keras.optimizers.SGD(lr=2.0),
-        sequential_update=sequential_update,
-        average_decay=0.5,
-    )
+    opt = MovingAverage(tf.keras.optimizers.SGD(lr=2.0), average_decay=0.5,)
 
     opt.apply_gradients(grads_and_vars)
     opt.apply_gradients(grads_and_vars)
@@ -47,34 +42,30 @@ def test_run(sequential_update):
     ema_var0 = opt.get_slot(var0, "average")
     ema_var1 = opt.get_slot(var1, "average")
 
-    if sequential_update:
-        np.testing.assert_allclose(ema_var0.read_value(), [0.75, 1.75])
-        np.testing.assert_allclose(ema_var1.read_value(), [2.975, 3.975])
+    np.testing.assert_allclose(ema_var0.read_value(), [0.75, 1.75])
+    np.testing.assert_allclose(ema_var1.read_value(), [2.975, 3.975])
 
     _ = opt.assign_average_vars([var0, var1])
 
-    if sequential_update:
-        np.testing.assert_allclose(var0.read_value(), [0.75, 1.75])
-        np.testing.assert_allclose(var1.read_value(), [2.975, 3.975])
+    np.testing.assert_allclose(var0.read_value(), [0.75, 1.75])
+    np.testing.assert_allclose(var1.read_value(), [2.975, 3.975])
 
     var0.assign_add([1.0, 1.0]),
     var1.assign_add([2.0, 2.0]),
     ema_var0.assign_add([3.0, 3.0]),
     ema_var1.assign_add([4.0, 4.0]),
 
-    if sequential_update:
-        np.testing.assert_allclose(var0.read_value(), [1.75, 2.75])
-        np.testing.assert_allclose(var1.read_value(), [4.975, 5.975])
-        np.testing.assert_allclose(ema_var0.read_value(), [3.75, 4.75])
-        np.testing.assert_allclose(ema_var1.read_value(), [6.975, 7.975])
+    np.testing.assert_allclose(var0.read_value(), [1.75, 2.75])
+    np.testing.assert_allclose(var1.read_value(), [4.975, 5.975])
+    np.testing.assert_allclose(ema_var0.read_value(), [3.75, 4.75])
+    np.testing.assert_allclose(ema_var1.read_value(), [6.975, 7.975])
 
 
 @pytest.mark.usefixtures("maybe_run_functions_eagerly")
-@pytest.mark.parametrize("sequential_update", [True, False])
-def test_opt_failure(sequential_update):
+def test_opt_failure():
     base_opt = None
     with pytest.raises(TypeError):
-        MovingAverage(base_opt, sequential_update, 0.5)
+        MovingAverage(base_opt, 0.5)
 
 
 @pytest.mark.usefixtures("maybe_run_functions_eagerly")
@@ -126,14 +117,11 @@ def test_optimizer_string():
 
 def test_config():
     sgd_opt = tf.keras.optimizers.SGD(lr=2.0, nesterov=True, momentum=0.3, decay=0.1)
-    opt = MovingAverage(
-        sgd_opt, average_decay=0.5, num_updates=None, sequential_update=False
-    )
+    opt = MovingAverage(sgd_opt, average_decay=0.5, num_updates=None)
     config = opt.get_config()
 
     assert config["average_decay"] == 0.5
     assert config["num_updates"] is None
-    assert config["sequential_update"] is False
 
     new_opt = MovingAverage.from_config(config)
     old_sgd_config = opt._optimizer.get_config()
@@ -173,9 +161,7 @@ def test_fit_simple_linear_model():
 
 def test_serialization():
     sgd_opt = tf.keras.optimizers.SGD(lr=2.0, nesterov=True, momentum=0.3, decay=0.1)
-    optimizer = MovingAverage(
-        sgd_opt, average_decay=0.5, num_updates=None, sequential_update=False
-    )
+    optimizer = MovingAverage(sgd_opt, average_decay=0.5, num_updates=None)
     config = tf.keras.optimizers.serialize(optimizer)
     new_optimizer = tf.keras.optimizers.deserialize(config)
     assert new_optimizer.get_config() == optimizer.get_config()
