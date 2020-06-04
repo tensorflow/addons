90b132a3b7b13ed84529f7bae5a3991c4677c41b
bhack <bhack@users.noreply.github.com>
2020-06-04 12:50:28 +0000

    Clean bazel

--- tools/testing/build_and_run_tests.sh
+++ tools/testing/build_and_run_tests.sh
@@ -22,7 +22,7 @@ export CC_OPT_FLAGS='-mavx'
 
 python -m pip install -r tools/install_deps/pytest.txt -e ./
 python ./configure.py
-#bash tools/install_so_files.sh
+bash tools/install_so_files.sh
 python -c "import tensorflow as tf; print(tf.config.list_physical_devices())"
 
 # use 10 workers if a gpu is available, otherwise,
@@ -34,5 +34,5 @@ if ! [ -x "$(command -v nvidia-smi)" ]; then
   EXTRA_ARGS="-n auto"
 fi
 
-
+bazel clean
 python -m pytest -v --functions-durations=20 --modules-durations=5 $EXTRA_ARGS ./tensorflow_addons
