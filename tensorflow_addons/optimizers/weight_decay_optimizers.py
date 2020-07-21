cd51483747df12cac48c7a228714aab9b4094979
Michael Stadler <michael.stadler@postgrad.manchester.ac.uk>
2020-07-21 23:28:07 +0100

    Added missing from_config method to ensure lr & wd are correctly deserialized

--- tensorflow_addons/optimizers/weight_decay_optimizers.py
+++ tensorflow_addons/optimizers/weight_decay_optimizers.py
@@ -93,6 +93,19 @@ class DecoupledWeightDecayExtension:
         )
         return config
 
+    @classmethod
+    def from_config(cls, config, custom_objects=None):
+        # LR handling copied from optimizer_v2.OptimizerV2
+        if "learning_rate" in config:
+            if isinstance(config["learning_rate"], dict):
+                config["learning_rate"] = tf.keras.optimizers.schedules.deserialize(config["learning_rate"], custom_objects=custom_objects)
+        
+        if "weight_decay" in config:
+              if isinstance(config["weight_decay"], dict):
+                config["weight_decay"] = tf.keras.optimizers.schedules.deserialize(config["weight_decay"], custom_objects=custom_objects)
+        
+        return cls(**config)
+
     def minimize(self, loss, var_list, grad_loss=None, name=None, decay_var_list=None):
         """Minimize `loss` by updating `var_list`.
 
