5639b712c81e81ef2ec93d592c0ea704074a3c67
MHStadler <michael.stadler@postgrad.manchester.ac.uk>
2020-07-28 17:50:02 +0100

    Fixed lr/wd schedules for DecoupledWeightDecayExtension running on GPU (#2053)
    * Fixed lr/wd schedules for DecoupledWeightDecayExtension running on GPU
    
    * Removed use of private tf api

--- tensorflow_addons/optimizers/weight_decay_optimizers.py
+++ tensorflow_addons/optimizers/weight_decay_optimizers.py
@@ -167,34 +167,59 @@ class DecoupledWeightDecayExtension:
         )
         return super().apply_gradients(grads_and_vars, name=name, **kwargs)
 
-    def _decay_weights_op(self, var):
+    def _decay_weights_op(self, var, apply_state=None):
         if not self._decay_var_list or var.ref() in self._decay_var_list:
-            return var.assign_sub(self._decayed_wd(var.dtype) * var, self._use_locking)
+            var_device, var_dtype = var.device, var.dtype.base_dtype
+            coefficients = (apply_state or {}).get(
+                (var_device, var_dtype)
+            ) or self._fallback_apply_state(var_device, var_dtype)
+
+            return var.assign_sub(coefficients["wd_t"] * var, self._use_locking)
         return tf.no_op()
 
-    def _decay_weights_sparse_op(self, var, indices):
+    def _decay_weights_sparse_op(self, var, indices, apply_state=None):
         if not self._decay_var_list or var.ref() in self._decay_var_list:
-            update = -self._decayed_wd(var.dtype) * tf.gather(var, indices)
+            var_device, var_dtype = var.device, var.dtype.base_dtype
+            coefficients = (apply_state or {}).get(
+                (var_device, var_dtype)
+            ) or self._fallback_apply_state(var_device, var_dtype)
+
+            update = -coefficients["wd_t"] * tf.gather(var, indices)
             return self._resource_scatter_add(var, indices, update)
         return tf.no_op()
 
+    def _prepare_local(self, var_device, var_dtype, apply_state):
+        super(DecoupledWeightDecayExtension, self)._prepare_local(
+            var_device, var_dtype, apply_state
+        )
+
+        if "weight_decay" in self._hyper:
+            wd_t = tf.identity(self._decayed_wd(var_dtype))
+            apply_state[(var_device, var_dtype)]["wd_t"] = wd_t
+
     def _decayed_wd(self, var_dtype):
         wd_t = self._get_hyper("weight_decay", var_dtype)
+
         if isinstance(wd_t, tf.keras.optimizers.schedules.LearningRateSchedule):
             wd_t = tf.cast(wd_t(self.iterations), var_dtype)
+
         return wd_t
 
     # Here, we overwrite the apply functions that the base optimizer calls.
     # super().apply_x resolves to the apply_x function of the BaseOptimizer.
 
-    def _resource_apply_dense(self, grad, var):
-        with tf.control_dependencies([self._decay_weights_op(var)]):
-            return super()._resource_apply_dense(grad, var)
+    def _resource_apply_dense(self, grad, var, apply_state=None):
+        with tf.control_dependencies(
+            [self._decay_weights_op(var, apply_state=apply_state)]
+        ):
+            return super()._resource_apply_dense(grad, var, apply_state=apply_state)
 
-    def _resource_apply_sparse(self, grad, var, indices):
-        decay_op = self._decay_weights_sparse_op(var, indices)
+    def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
+        decay_op = self._decay_weights_sparse_op(var, indices, apply_state=apply_state)
         with tf.control_dependencies([decay_op]):
-            return super()._resource_apply_sparse(grad, var, indices)
+            return super()._resource_apply_sparse(
+                grad, var, indices, apply_state=apply_state
+            )
 
 
 @typechecked
