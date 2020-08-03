0149bce8041e591fc94a5107a8d83104f95ae1bb
Rehan Guha <rehanguha29@gmail.com>
2020-08-03 17:05:08 +0530

    COCOB Backprop Optimizer

---
+++ tensorflow_addons/optimizers/cocob.py
@@ -0,0 +1,107 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+'''
+COntinuos COin Betting (COCOB) Backprop optimizer
+'''
+
+from tensorflow.python.ops import state_ops
+from tensorflow.python.ops import control_flow_ops
+import tensorflow as tf
+
+
+@tf.keras.utils.register_keras_serializable(package="Addons")
+class COCOB(tf.keras.optimizers.Optimizer):
+    """
+    Optimizer that implements COntinuos COin Betting (COCOB) Backprop Algorithm
+
+    Reference:
+        - [Training Deep Networks without Learning Rates Through Coin Betting
+](http://papers.nips.cc/paper/6811-training-deep-networks-without-learning-rates-through-coin-betting)
+    """
+    def __init__(self, alpha=100, use_locking=False, name='COCOB', **kwargs):
+        """
+        Constructs a new COCOB-Backprop optimizer
+
+        Arguments:
+            `aplha`: Default value is set to 100 as per paper. 
+                     This has the effect of restricting the value of the parameters in the first iterations of the algorithm.
+                     (Refer to Paper for indepth understanding)
+        
+        Rasies:
+            `ValueError`: If the value of `alpha` is less than 1.
+            
+            `NotImplementedError`: If the data is in sparse format.
+
+        """
+
+        if alpha < 1:
+            raise ValueError("`alpha` must be greater than Zero")
+
+        super().__init__(name, **kwargs)
+        self._set_hyper('alpha', alpha)
+        self._alpha = alpha
+
+    def _create_slots(self, var_list):
+        for v in var_list:
+            self.add_slot(v, "L")
+            self.add_slot(v, "grad_norm_sum")
+            self.add_slot(v, "gradients_sum")
+            self.add_slot(v, "tilde_w")
+            self.add_slot(v, "reward")
+
+    def _resource_apply_dense(self, grad, handle, apply_state=None):
+        gradients_sum = self.get_slot(handle, "gradients_sum")
+        grad_norm_sum = self.get_slot(handle, "grad_norm_sum")
+        tilde_w = self.get_slot(handle, "tilde_w")
+        L = self.get_slot(handle, "L")
+        reward = self.get_slot(handle, "reward")
+
+        L_update = tf.maximum(L, tf.abs(grad))
+        gradients_sum_update = gradients_sum + grad
+        grad_norm_sum_update = grad_norm_sum + tf.abs(grad)
+        reward_update = tf.maximum(reward-grad*tilde_w, 0)
+        new_w = -gradients_sum_update / \
+            (L_update*(tf.maximum(grad_norm_sum_update+L_update,
+                                  self._alpha*L_update)))*(reward_update+L_update)
+        var_update = handle-tilde_w+new_w
+        tilde_w_update = new_w
+
+        gradients_sum_update_op = state_ops.assign(
+            gradients_sum, gradients_sum_update)
+        grad_norm_sum_update_op = state_ops.assign(
+            grad_norm_sum, grad_norm_sum_update)
+        var_update_op = state_ops.assign(handle, var_update)
+        tilde_w_update_op = state_ops.assign(tilde_w, tilde_w_update)
+        L_update_op = state_ops.assign(L, L_update)
+        reward_update_op = state_ops.assign(reward, reward_update)
+
+        return control_flow_ops.group(*[gradients_sum_update_op,
+                                        var_update_op,
+                                        grad_norm_sum_update_op,
+                                        tilde_w_update_op,
+                                        reward_update_op,
+                                        L_update_op])
+
+
+    def _resource_apply_sparse(self, grad, handle, indices, apply_state=None):
+        raise NotImplementedError()
+
+    def get_config(self):
+
+        config = {
+            'alpha': self._serialize_hyperparameter('alpha'),
+        }
+        base_config = super().get_config()
+        return {**base_config, **config}
---
+++ tensorflow_addons/optimizers/tests/cocob_test.py
@@ -0,0 +1,59 @@
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+'''
+Tests for COntinuos COin Betting (COCOB) Backprop optimizer
+'''
+
+import numpy as np
+import pytest
+import tensorflow as tf
+from cocob_f import COCOB
+
+def run_dense_sample(iterations, expected, optimizer):
+    var_0 = tf.Variable([1.0, 2.0], dtype=tf.dtypes.float32)
+    var_1 = tf.Variable([3.0, 4.0], dtype=tf.dtypes.float32)
+
+    grad_0 = tf.constant([0.1, 0.2], dtype=tf.dtypes.float32)
+    grad_1 = tf.constant([0.03, 0.04], dtype=tf.dtypes.float32)
+
+    grads_and_vars = list(zip([grad_0, grad_1], [var_0, var_1]))
+
+    for _ in range(iterations):
+        optimizer.apply_gradients(grads_and_vars)
+    
+    np.testing.assert_allclose(var_0.read_value(), expected[0], atol=2e-4)
+    np.testing.assert_allclose(var_1.read_value(), expected[1], atol=2e-4)
+
+
+def test_dense_sample_with_default_alpha():
+    run_dense_sample(
+        iterations=100,
+        expected=[[-4.183396e+16, -4.183396e+16], [-4.1833639e+16, -4.1833858e+16]],
+        optimizer=COCOB()
+        )
+
+def test_dense_sample_with_custom_int_alpha():
+    run_dense_sample(
+        iterations=100,
+        expected=[[-8.60311e+26, -8.60311e+26], [-8.603111e+26, -8.603099e+26]],
+        optimizer=COCOB(20)
+        )
+
+def test_dense_sample_with_custom_float_alpha():
+    run_dense_sample(
+        iterations=78,
+        expected=[[-6.7280056e+15, -6.7280056e+15], [-6.7279933e+15, -6.7280217e+15]],
+        optimizer=COCOB(55.7)
+        )
