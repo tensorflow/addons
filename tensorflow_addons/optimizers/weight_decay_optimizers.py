45dbbc9b9ededf18ea32aa7545ab6de1e9410e81
omalleyt12 <omalleyt@google.com>
2020-08-06 11:44:44 -0700

    Pass tape to minimize

--- tensorflow_addons/optimizers/weight_decay_optimizers.py
+++ tensorflow_addons/optimizers/weight_decay_optimizers.py
@@ -110,7 +110,13 @@ class DecoupledWeightDecayExtension:
 
         return cls(**config)
 
-    def minimize(self, loss, var_list, grad_loss=None, name=None, decay_var_list=None):
+    def minimize(self,
+                 loss,
+                 var_list,
+                 grad_loss=None,
+                 name=None,
+                 decay_var_list=None,
+                 tape=None):
         """Minimize `loss` by updating `var_list`.
 
         This method simply computes gradient using `tf.GradientTape` and calls
@@ -119,8 +125,9 @@ class DecoupledWeightDecayExtension:
         instead of using this function.
 
         Args:
-            loss: A callable taking no arguments which returns the value to
-                minimize.
+            loss: `Tensor` or callable. If a callable, `loss` should take no
+                arguments and return the value to minimize. If a `Tensor`, the
+                `tape` argument must be passed.
             var_list: list or tuple of `Variable` objects to update to
                 minimize `loss`, or a callable returning the list or tuple of
                 `Variable` objects. Use callable when the variable list would
@@ -131,6 +138,8 @@ class DecoupledWeightDecayExtension:
             decay_var_list: Optional list of variables to be decayed. Defaults
                 to all variables in var_list.
             name: Optional name for the returned operation.
+            tape: (Optional) `tf.GradientTape`. If `loss` is provided as a
+                `Tensor`, the tape that computed the `loss` must be provided.
         Returns:
             An Operation that updates the variables in `var_list`.
         Raises:
@@ -139,7 +148,8 @@ class DecoupledWeightDecayExtension:
         self._decay_var_list = (
             set([v.ref() for v in decay_var_list]) if decay_var_list else False
         )
-        return super().minimize(loss, var_list=var_list, grad_loss=grad_loss, name=name)
+        return super().minimize(
+            loss, var_list=var_list, grad_loss=grad_loss, name=name, tape=tape)
 
     def apply_gradients(self, grads_and_vars, name=None, decay_var_list=None, **kwargs):
         """Apply gradients to variables.
