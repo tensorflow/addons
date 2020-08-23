dda46afbcfde41573918ef8a316e47208f7afeac
nataliyah123 <nataliyah123@users.noreply.github.com>
2020-08-23 13:55:44 +0500

    removed-blank-spaces-multihead_attension

--- tensorflow_addons/layers/multihead_attention.py
+++ tensorflow_addons/layers/multihead_attention.py
@@ -28,21 +28,17 @@ class MultiHeadAttention(tf.keras.layers.Layer):
     between them:
     
     >>> mha = MultiHeadAttention(head_size=128, num_heads=12)
-
     >>> query = tf.random.uniform((3, 5, 4)) # (batch_size, query_elements, query_depth)
     >>> key = tf.random.uniform((3, 6, 5)) # (batch_size, key_elements, key_depth)
     >>> value = tf.random.uniform((3, 5, 6)) # (batch_size, key_elements, value_depth)
 
     >>> attention = mha([query, key, value]) # (batch_size, query_elements, value_depth)
     
-
     If `value` is not given then internally `value = key` will be used:
     
     >>> mha = MultiHeadAttention(head_size=128, num_heads=12)
-
     >>> query = tf.random.uniform((3, 5, 5)) # (batch_size, query_elements, query_depth)
     >>> key = tf.random.uniform((3, 6, 10)) # (batch_size, key_elements, key_depth)
-
     >>> attention = mha([query, key]) # (batch_size, query_elements, key_depth)
     
 
