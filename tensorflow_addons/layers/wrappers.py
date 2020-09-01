6f4a7e033fdf16dc7d7d0079f61baf3c6e5c1709
nataliyah123 <nataliyah123@users.noreply.github.com>
2020-09-01 13:40:51 +0500

    added-value-for-x

--- tensorflow_addons/layers/wrappers.py
+++ tensorflow_addons/layers/wrappers.py
@@ -29,15 +29,18 @@ class WeightNormalization(tf.keras.layers.Wrapper):
     conditioning of the optimization problem.
 
     See [Weight Normalization: A Simple Reparameterization to Accelerate Training of Deep Neural Networks](https://arxiv.org/abs/1602.07868).
+    >>> input_shape = (1,4, 5, 1)
+    >>> x = tf.random.normal(input_shape)
+    >>> n_classes = 10
     >>> net = WeightNormalization(
     ... tf.keras.layers.Conv2D(2, 2, activation='relu'),
-    ... input_shape=(32, 32, 3),
+    ... input_shape=input_shape[2:],
     ... data_init=True)(x)
     >>> net = WeightNormalization(
-    ... tf.keras.layers.Conv2D(16, 5, activation='relu'),
+    ... tf.keras.layers.Conv2D(3, 5, activation='relu'),
     ... data_init=True)(net)
     >>> net = WeightNormalization(
-    ... tf.keras.layers.Dense(120, activation='relu'),
+    ... tf.keras.layers.Dense(5, activation='relu'),
     ... data_init=True)(net)
     >>> net = WeightNormalization(
     ... tf.keras.layers.Dense(n_classes),
