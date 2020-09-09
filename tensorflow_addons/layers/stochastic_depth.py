6321ce4e279efdf38b02c65ce122aed4ec6c8463
Michael Stadler <michael.stadler@postgrad.manchester.ac.uk>
2020-09-09 20:28:27 +0100

    Changes after code review

--- tensorflow_addons/layers/stochastic_depth.py
+++ tensorflow_addons/layers/stochastic_depth.py
@@ -14,36 +14,32 @@ class StochasticDepth(tf.keras.layers.Layer):
     Residual architectures with fixed depth, use residual branches that are merged back into the main network
     by adding the residual branch back to the input:
 
-    ```python
-    residual = tf.keras.layers.Conv2D(...)(input)
-
-    return tf.keras.layers.Add()([input, residual])
-    ```
+    >>> input = np.ones((3, 3, 1))
+    >>> residual = tf.keras.layers.Conv2D(1, 1)(input)
+    >>> tfa.layers.StochasticDepth()([input, residual])
 
     StochasticDepth acts as a drop-in replacement for the addition:
 
-    ```python
-    residual = tf.keras.layers.Conv2D(...)(input)
-
-    return tfa.layers.StochasticDepth()([input, residual])
-    ```
+    >>> input = np.ones((3, 3, 1))
+    >>> residual = tf.keras.layers.Conv2D(1, 1)(input)
+    >>> tfa.layers.StochasticDepth()([input, residual])
 
     At train time, StochasticDepth returns:
 
-    ```python
+    $$
     x[0] + b_l * x[1]
-    ```
+    $$
 
-    , where b_l is a random Bernoulli variable with probability p(b_l == 1) == p_l
+    , where $b_l$ is a random Bernoulli variable with probability $p(b_l == 1) == p_l$
 
-    At test time, StochasticDepth rescales the activations of the residual branch based on the survival probability:
+    At test time, StochasticDepth rescales the activations of the residual branch based on the survival probability ($p_l$):
 
-    ```python
+    $$
     x[0] + p_l * x[1]
-    ```
+    $$
 
     Arguments:
-        p_l: float, the probability of the residual branch being kept.
+        survival_probability: float, the probability of the residual branch being kept.
 
     Call Arguments:
         inputs:  List of `[shortcut, residual]` where
@@ -54,37 +50,43 @@ class StochasticDepth(tf.keras.layers.Layer):
     """
 
     @typechecked
-    def __init__(self, p_l: float = 0.5, **kwargs):
+    def __init__(self, survival_probability: float = 0.5, **kwargs):
         super().__init__(**kwargs)
 
-        self.p_l = p_l
+        self.survival_probability = survival_probability
 
     def call(self, x, training=None):
-        assert isinstance(x, list)
+        assert isinstance(x, list):
+            raise ValueError("Input must be a list")
+        assert len(x) == 2:
+            raise ValueError("Input must have exactly two entries")
 
         shortcut, residual = x
 
-        # Random bernoulli variable with probability p_l, indiciathing wheter the branch should be kept or not or not
-        b_l = tf.keras.backend.random_binomial([], p=self.p_l)
+        # Random bernoulli variable indicating whether the branch should be kept or not or not
+        b_l = tf.keras.backend.random_binomial([], p=self.survival_probability)
 
         def _call_train():
             return shortcut + b_l * residual
 
         def _call_test():
-            return shortcut + self.p_l * residual
+            return shortcut + self.survival_probability * residual
 
         return tf.keras.backend.in_train_phase(
             _call_train, _call_test, training=training
         )
 
     def compute_output_shape(self, input_shape):
-        assert isinstance(input_shape, list)
+        assert isinstance(x, list):
+            raise ValueError("Input must be a list")
+        assert len(x) == 2:
+            raise ValueError("Input must have exactly two entries")
 
         return input_shape[0]
 
     def get_config(self):
         base_config = super().get_config()
 
-        config = {"p_l": self.p_l}
+        config = {"survival_probability": self.survival_probability}
 
         return {**base_config, **config}
--- tensorflow_addons/layers/tests/stochastic_depth_test.py
+++ tensorflow_addons/layers/tests/stochastic_depth_test.py
@@ -5,26 +5,26 @@ import tensorflow as tf
 from tensorflow_addons.layers.stochastic_depth import StochasticDepth
 from tensorflow_addons.utils import test_utils
 
-KEEP_SEED = 1111
-DROP_SEED = 2222
+_KEEP_SEED = 1111
+_DROP_SEED = 2222
 
 
-@pytest.mark.parametrize("seed", [KEEP_SEED, DROP_SEED])
+@pytest.mark.parametrize("seed", [_KEEP_SEED, _DROP_SEED])
 @pytest.mark.parametrize("training", [True, False])
 def stochastic_depth_test(seed, training):
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
-    p_l = 0.5
+    survival_probability = 0.5
 
     shortcut = np.asarray([[0.2, 0.1, 0.4]]).astype(np.float32)
     residual = np.asarray([[0.2, 0.4, 0.5]]).astype(np.float32)
 
     if training:
-        if seed == KEEP_SEED:
+        if seed == _KEEP_SEED:
             # shortcut + residual
             expected_output = np.asarray([[0.4, 0.5, 0.9]]).astype(np.float32)
-        elif seed == DROP_SEED:
+        elif seed == _DROP_SEED:
             # shortcut
             expected_output = np.asarray([[0.2, 0.1, 0.4]]).astype(np.float32)
     else:
@@ -33,14 +33,14 @@ def stochastic_depth_test(seed, training):
 
     test_utils.layer_test(
         StochasticDepth,
-        kwargs={"p_l": p_l},
+        kwargs={"survival_probability": survival_probability},
         input_data=[shortcut, residual],
         expected_output=expected_output,
     )
 
 
 def test_serialization():
-    stoch_depth = StochasticDepth(p_l=0.5)
+    stoch_depth = StochasticDepth(survival_probability=0.5)
     serialized_stoch_depth = tf.keras.layers.serialize(stoch_depth)
     new_layer = tf.keras.layers.deserialize(serialized_stoch_depth)
     assert stoch_depth.get_config() == new_layer.get_config()
