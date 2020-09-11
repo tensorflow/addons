5bdaf40fd6c1c20a15692af7caab1a40eef92498
Kaixi Hou <kaixihou@gmail.com>
2020-09-10 17:21:48 -0700

    Avoid unnecessary reshapes for instance norm (#2158)
    * Avoid unnecessary reshapes for instance norm
    
    * flake8 format changes
    
    * Fixed failed tests
    
    * flake8 format update

--- tensorflow_addons/layers/normalizations.py
+++ tensorflow_addons/layers/normalizations.py
@@ -125,7 +125,11 @@ class GroupNormalization(tf.keras.layers.Layer):
 
         normalized_inputs = self._apply_normalization(reshaped_inputs, input_shape)
 
-        outputs = tf.reshape(normalized_inputs, tensor_input_shape)
+        is_instance_norm = (input_shape[self.axis] // self.groups) == 1
+        if not is_instance_norm:
+            outputs = tf.reshape(normalized_inputs, tensor_input_shape)
+        else:
+            outputs = normalized_inputs
 
         return outputs
 
@@ -156,17 +160,25 @@ class GroupNormalization(tf.keras.layers.Layer):
     def _reshape_into_groups(self, inputs, input_shape, tensor_input_shape):
 
         group_shape = [tensor_input_shape[i] for i in range(len(input_shape))]
-        group_shape[self.axis] = input_shape[self.axis] // self.groups
-        group_shape.insert(self.axis, self.groups)
-        group_shape = tf.stack(group_shape)
-        reshaped_inputs = tf.reshape(inputs, group_shape)
-        return reshaped_inputs, group_shape
+        is_instance_norm = (input_shape[self.axis] // self.groups) == 1
+        if not is_instance_norm:
+            group_shape[self.axis] = input_shape[self.axis] // self.groups
+            group_shape.insert(self.axis, self.groups)
+            group_shape = tf.stack(group_shape)
+            reshaped_inputs = tf.reshape(inputs, group_shape)
+            return reshaped_inputs, group_shape
+        else:
+            return inputs, group_shape
 
     def _apply_normalization(self, reshaped_inputs, input_shape):
 
         group_shape = tf.keras.backend.int_shape(reshaped_inputs)
         group_reduction_axes = list(range(1, len(group_shape)))
-        axis = -2 if self.axis == -1 else self.axis - 1
+        is_instance_norm = (input_shape[self.axis] // self.groups) == 1
+        if not is_instance_norm:
+            axis = -2 if self.axis == -1 else self.axis - 1
+        else:
+            axis = -1 if self.axis == -1 else self.axis - 1
         group_reduction_axes.pop(axis)
 
         mean, variance = tf.nn.moments(
@@ -274,8 +286,12 @@ class GroupNormalization(tf.keras.layers.Layer):
 
     def _create_broadcast_shape(self, input_shape):
         broadcast_shape = [1] * len(input_shape)
-        broadcast_shape[self.axis] = input_shape[self.axis] // self.groups
-        broadcast_shape.insert(self.axis, self.groups)
+        is_instance_norm = (input_shape[self.axis] // self.groups) == 1
+        if not is_instance_norm:
+            broadcast_shape[self.axis] = input_shape[self.axis] // self.groups
+            broadcast_shape.insert(self.axis, self.groups)
+        else:
+            broadcast_shape[self.axis] = self.groups
         return broadcast_shape
 
 
