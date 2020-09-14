58eae9f77672122d889b1713a0287d73c8721939
nataliyah123 <nataliyah123@users.noreply.github.com>
2020-09-14 14:04:52 +0500

    assigned metric.update_Status to result

--- tensorflow_addons/metrics/cohens_kappa.py
+++ tensorflow_addons/metrics/cohens_kappa.py
@@ -44,13 +44,13 @@ class CohenKappa(Metric):
     >>> weights = np.array([1, 1, 2, 5, 10, 2, 3, 3], dtype=np.int32)
 
     >>> metric = tfa.metrics.CohenKappa(num_classes=5, sparse_labels=True)
-    >>> # metric.update_state(y_true = actuals, y_pred = preds)
-    >>> # print('Final result: ', metric.result().numpy())
+    >>> result = metric.update_state(y_true = actuals, y_pred = preds)
+    >>> # print('Final result: ', result.result().numpy())
     >>> # Final result: 0.61904764
 
     >>> # To use this with weights, sample_weight argument can be used.
     >>> metric = tfa.metrics.CohenKappa(num_classes=5, sparse_labels=True)
-    >>> # metric.update_state(actuals, preds, sample_weight=weights)
+    >>> result = metric.update_state(actuals, preds, sample_weight=weights)
     >>> # print('Final result: ', metric.result().numpy())
     >>> # Final result: 0.37209308
 
