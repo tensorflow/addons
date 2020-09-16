07ef1a1870ab6cb2f4daf62380f86555650d8d3b
MHStadler <michael.stadler@postgrad.manchester.ac.uk>
2020-09-17 00:51:17 +0100

    Added stochastic depth layer (#2154)
    * Added stochastic depth layer
    
    * Fixed code style and added missing __init__ entry
    
    * Fixed tests and style
    
    * Fixed code style
    
    * Updated CODEOWNERS
    
    * Added codeowners for tests
    
    * Changes after code review
    
    * Test and formatting fixes
    
    * Fixed doc string
    
    * Added mixed precision test
    
    * Further code review changes
    
    * Code review changes

--- tensorflow_addons/layers/__init__.py
+++ tensorflow_addons/layers/__init__.py
@@ -38,4 +38,5 @@ from tensorflow_addons.layers.spatial_pyramid_pooling import SpatialPyramidPooli
 from tensorflow_addons.layers.tlu import TLU
 from tensorflow_addons.layers.wrappers import WeightNormalization
 from tensorflow_addons.layers.esn import ESN
+from tensorflow_addons.layers.stochastic_depth import StochasticDepth
 from tensorflow_addons.layers.noisy_dense import NoisyDense
