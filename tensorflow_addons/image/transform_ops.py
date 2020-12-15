49f8cc084457b36b39da3b72a755b1117841318c
Tzu-Wei Sung <windqaq@gmail.com>
2020-12-15 14:54:14 -0800

    Mark unsupported things for TF<2.4.0

--- tensorflow_addons/image/transform_ops.py
+++ tensorflow_addons/image/transform_ops.py
@@ -14,6 +14,9 @@
 # ==============================================================================
 """Image transform ops."""
 
+import warnings
+from distutils.version import LooseVersion
+
 import tensorflow as tf
 from tensorflow_addons.image import utils as img_utils
 from tensorflow_addons.utils.types import TensorLike
@@ -120,17 +123,36 @@ def transform(
                 % len(transforms.get_shape())
             )
 
-        fill_value = tf.convert_to_tensor(
-            fill_value, dtype=tf.float32, name="fill_value"
-        )
-        output = tf.raw_ops.ImageProjectiveTransformV3(
-            images=images,
-            transforms=transforms,
-            output_shape=output_shape,
-            interpolation=interpolation.upper(),
-            fill_mode=fill_mode.upper(),
-            fill_value=fill_value,
-        )
+        if LooseVersion(tf.__version__) >= LooseVersion("2.4.0"):
+            fill_value = tf.convert_to_tensor(
+                fill_value, dtype=tf.float32, name="fill_value"
+            )
+            output = tf.raw_ops.ImageProjectiveTransformV3(
+                images=images,
+                transforms=transforms,
+                output_shape=output_shape,
+                interpolation=interpolation.upper(),
+                fill_mode=fill_mode.upper(),
+                fill_value=fill_value,
+            )
+        else:
+            fill_mode = fill_mode.upper()
+            # TODO(WindQAQ): Get rid of the check once we drop TensorFlow < 2.4 support.
+            if fill_mode == "CONSTANT":
+                warnings.warn(
+                    "fill_value is not supported and is always 0 for TensorFlow < 2.4.0."
+                )
+            if fill_mode == "NEAREST":
+                raise ValueError(
+                    "NEAREST fill mode is not supported for TensorFlow < 2.4.0."
+                )
+            output = tf.raw_ops.ImageProjectiveTransformV2(
+                images=images,
+                transforms=transforms,
+                output_shape=output_shape,
+                interpolation=interpolation.upper(),
+                fill_mode=fill_mode,
+            )
         return img_utils.from_4D_image(output, original_ndims)
 
 
