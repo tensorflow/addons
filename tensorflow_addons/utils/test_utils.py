0dff8f83c96f6eb41f8ecfb687b936c1b143d7ff
Tzu-Wei Sung <windqaq@gmail.com>
2020-12-15 14:37:43 -0800

    Remove support of TF<2.2 (#2294)

--- tensorflow_addons/utils/test_utils.py
+++ tensorflow_addons/utils/test_utils.py
@@ -22,8 +22,6 @@ import numpy as np
 import pytest
 import tensorflow as tf
 
-from distutils.version import LooseVersion
-
 from tensorflow_addons import options
 from tensorflow_addons.utils import resource_loader
 
@@ -111,8 +109,6 @@ def run_custom_and_py_ops(request):
 
 @pytest.fixture(scope="function", params=["float32", "mixed_float16"])
 def run_with_mixed_precision_policy(request):
-    if is_gpu_available() and LooseVersion(tf.__version__) <= "2.2.0":
-        pytest.xfail("See https://github.com/tensorflow/tensorflow/issues/39775")
     tf.keras.mixed_precision.experimental.set_policy(request.param)
     yield
     tf.keras.mixed_precision.experimental.set_policy("float32")
