3594e5952737f7aa3cb161ce304ee1bc6e8429ce
NanuSai <gnanasai.n18@iiits.in>
2021-02-20 15:38:49 +0530

    - rm TF_ADDONS_PY_OPS in distort_image_ops.py

--- tensorflow_addons/image/distort_image_ops.py
+++ tensorflow_addons/image/distort_image_ops.py
@@ -182,7 +182,7 @@ def adjust_hsv_in_yiq(
         orig_dtype = image.dtype
         image = tf.image.convert_image_dtype(image, tf.float32)
 
-        if not options.TF_ADDONS_PY_OPS:
+        if not options.is_custom_kernel_disabled():
             warnings.warn(
                 "C++/CUDA kernel of `adjust_hsv_in_yiq` will be removed in Addons `0.13`.",
                 DeprecationWarning,
