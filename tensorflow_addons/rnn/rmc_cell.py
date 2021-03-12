d252f602236bccdc0f21df512e6268ad22cfaaa8
Gustavo Rosa <gth.rosa@uol.com.br>
2021-03-12 14:22:35 -0300

    Still need to implement the MultiHeadAttention.

--- tensorflow_addons/rnn/rmc_cell.py
+++ tensorflow_addons/rnn/rmc_cell.py
@@ -16,9 +16,6 @@
 
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras.layers import AbstractRNNCell, Dense, LayerNormalization
-from tensorflow.python.keras import (activations, constraints, initializers,
-                                     regularizers)
 
 from tensorflow_addons.utils.types import (
     Activation,
@@ -42,6 +39,17 @@ class RMCCell(keras.layers.AbstractRNNCell):
     A. Santoro, et al.
     "Relational recurrent neural networks".
     Advances in neural information processing systems, 2018.
+
+    Example:
+
+    >>> inputs = np.random.random([30,23,9]).astype(np.float32)
+    >>> RMCCell = tfa.rnn.RMCCell(2, 2, 1)
+    >>> rnn = tf.keras.layers.RNN(RMCCell, return_sequences=True, return_state=True)
+    >>> outputs, memory_state = rnn(inputs, initial_state=RMCCell.get_initial_state(30))
+    >>> outputs.shape
+    TensorShape([30, 23, 4])
+    >>> memory_state.shape
+    TensorShape([30, 4])
 
     """
 
@@ -98,27 +106,26 @@ class RMCCell(keras.layers.AbstractRNNCell):
         self.units = self.slot_size * n_slots
         self.n_gates = 2 * self.slot_size
 
-        self.activation = activations.get(activation)
-        self.recurrent_activation = activations.get(recurrent_activation)
+        self.activation = activation
+        self.recurrent_activation = recurrent_activation
         self.forget_bias = forget_bias
 
-        self.kernel_initializer = initializers.get(kernel_initializer)
-        self.recurrent_initializer = initializers.get(recurrent_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_initializer = kernel_initializer
+        self.recurrent_initializer = recurrent_initializer
+        self.bias_initializer = bias_initializer
 
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.kernel_regularizer = kernel_regularizer
+        self.recurrent_regularizer = recurrent_regularizer
+        self.bias_regularizer = bias_regularizer
         
-        self.kernel_constraint = constraints.get(kernel_constraint)
-        self.recurrent_constraint = constraints.get(recurrent_constraint)
-        self.bias_constraint = constraints.get(bias_constraint)
+        self.kernel_constraint = kernel_constraint
+        self.recurrent_constraint = recurrent_constraint
+        self.bias_constraint = bias_constraint
 
-        self.projector = Dense(self.slot_size)
-        self.before_norm = LayerNormalization()
-        self.linear = [Dense(self.slot_size, activation="relu")
-                       for _ in range(n_layers)]
-        self.after_norm = LayerNormalization()
+        self.projector = keras.layers.Dense(self.slot_size)
+        self.before_norm = keras.layers.LayerNormalization()
+        self.linear = [keras.layers.Dense(self.slot_size, activation="relu") for _ in range(n_layers)]
+        self.after_norm = keras.layers.LayerNormalization()
         self.attn = MultiHeadAttention(self.slot_size, self.n_heads)
 
     @property
@@ -158,136 +165,75 @@ class RMCCell(keras.layers.AbstractRNNCell):
         self.built = True
 
     def _attend_over_memory(self, inputs, memory):
-        """Performs an Attention mechanism over the current memory.
-
-        Args:
-            inputs (tf.Tensor): An input tensor.
-            memory (tf.Tensor): Current memory tensor.
-
-        Returns:
-            Updated current memory based on Multi-Head Attention mechanism.
-
-        """
-
-        # For every feed-forward network
         for _ in range(self.n_blocks):
-            # Concatenates the inputs with the memory
             concat_memory = tf.concat([inputs, memory], 1)
-
-            # Passes down the multi-head attention layer
             att_memory, _ = self.attn(memory, concat_memory, concat_memory)
-
-            # Passes down the first normalization layer
             norm_memory = self.before_norm(att_memory + memory)
-
-            # Makes a copy to feed the linear layers
             linear_memory = norm_memory
 
-            # For every linear layer
             for l in self.linear:
-                # Passes down the layer
                 linear_memory = l(linear_memory)
 
-            # Calculates the final memory from the network with another normalization layer
             memory = self.after_norm(norm_memory + linear_memory)
 
         return memory
 
     def call(self, inputs, states):
-        """Method that holds vital information whenever this class is called.
-
-        Args:
-            inputs (tf.Tensor): An input tensor.
-            states (list): A list holding previous states and memories.
-
-        Returns:
-            Output states as well as current state and memory.
-
-        """
-
-        # Gathering previous hidden and memory states
+        # Gathers previous hidden and memory states
         h_prev, m_prev = states
 
-        # Projecting the inputs to the same size as the memory
+        # Projects the inputs to the same size as the memory
         inputs = tf.expand_dims(self.projector(inputs), 1)
 
-        # Reshaping the previous hidden state tensor
-        h_prev = tf.reshape(
-            h_prev, [h_prev.shape[0], self.n_slots, self.slot_size])
+        # Reshapes the previous hidden state and memory tensors
+        h_prev = tf.reshape(h_prev, [h_prev.shape[0], self.n_slots, self.slot_size])
+        m_prev = tf.reshape(m_prev, [m_prev.shape[0], self.n_slots, self.slot_size])
 
-        # Reshaping the previous memory tensor
-        m_prev = tf.reshape(
-            m_prev, [m_prev.shape[0], self.n_slots, self.slot_size])
-
-        # Copying the inputs for the forget and input gates
+        # Copies the inputs for the forget and input gates
         inputs_f = inputs
         inputs_i = inputs
 
-        # Splitting up the kernel into forget and input gates kernels
+        # Splits up the kernel into forget and input gates kernels
+        # Also calculates the forget and input gates kernel outputs
         k_f, k_i = tf.split(self.kernel, 2, axis=1)
-
-        # Calculating the forget and input gates kernel outputs
         x_f = tf.tensordot(inputs_f, k_f, axes=[[-1], [0]])
         x_i = tf.tensordot(inputs_i, k_i, axes=[[-1], [0]])
 
-        # Splitting up the recurrent kernel into forget and input gates kernels
+        # Splits up the recurrent kernel into forget and input gates kernels
+        # Also calculates the forget and input gates recurrent kernel outputs
         rk_f, rk_i = tf.split(self.recurrent_kernel, 2, axis=1)
-
-        # Calculating the forget and input gates recurrent kernel outputs
         x_f += tf.tensordot(h_prev, rk_f, axes=[[-1], [0]])
         x_i += tf.tensordot(h_prev, rk_i, axes=[[-1], [0]])
 
-        # Splitting up the bias into forget and input gates biases
+        # Splits up the bias into forget and input gates biases
+        # Also adds the forget and input gates bias outputs
         b_f, b_i = tf.split(self.bias, 2, axis=0)
-
-        # Adding the forget and input gate bias
         x_f = tf.nn.bias_add(x_f, b_f)
         x_i = tf.nn.bias_add(x_i, b_i)
 
-        # Calculating the attention mechanism over the previous memory
+        # Calculates the attention mechanism over the previous memory
+        # Also calculates current memory and hidden states
         att_m = self._attend_over_memory(inputs, m_prev)
-
-        # Calculating current memory state
-        m = self.recurrent_activation(
-            x_f + self.forget_bias) * m_prev + self.recurrent_activation(x_i) * self.activation(att_m)
-
-        # Calculating current hidden state
+        m = self.recurrent_activation(x_f + self.forget_bias) * m_prev + self.recurrent_activation(x_i) * self.activation(att_m)
         h = self.activation(m)
 
-        # Reshaping both the current hidden and memory states to their correct output size
+        # Reshapes both current hidden and memory states to their correct output size
         h = tf.reshape(h, [h.shape[0], self.units])
         m = tf.reshape(m, [m.shape[0], self.units])
 
         return h, [h, m]
 
     def get_initial_state(self, batch_size):
-        """Gets the cell initial state by creating an identity matrix.
-
-        Args:
-            batch_size (int): Size of the batch.
-
-        """
-
-        # Creates an identity matrix
         states = tf.eye(self.n_slots, batch_shape=[batch_size])
 
-        # If the slot size is bigger than number of slots
         if self.slot_size > self.n_slots:
-            # Calculates its difference
             diff = self.slot_size - self.n_slots
-
-            # Creates a new tensor for padding
             padding = tf.zeros((batch_size, self.n_slots, diff))
-
-            # Concatenates the initial states with the padding
             states = tf.concat([states, padding], -1)
 
-        # If the slot size is smaller than number of slots
         elif self.slot_size < self.n_slots:
-            # Just gather the tensor until the desired size
             states = states[:, :, :self.slot_size]
 
-        # Reshapes to a flatten output
         states = tf.reshape(states, (states.shape[0], -1))
 
         return states, states
@@ -302,18 +248,18 @@ class RMCCell(keras.layers.AbstractRNNCell):
             "n_layers": self.n_layers,
             "units": self.units,
             "n_gates": self.n_gates,
-            "activation": activations.serialize(self.activation),
-            "recurrent_activation": activations.serialize(self.recurrent_activation),
+            "activation": tf.keras.activations.serialize(self.activation),
+            "recurrent_activation": tf.keras.activations.serialize(self.recurrent_activation),
             "forget_bias": self.forget_bias,
-            "kernel_initializer": initializers.serialize(self.kernel_initializer),
-            "recurrent_initializer": initializers.serialize(self.recurrent_initializer),
-            "bias_initializer": initializers.serialize(self.bias_initializer),
-            "kernel_regularizer": regularizers.serialize(self.kernel_regularizer),
-            "recurrent_regularizer": regularizers.serialize(self.recurrent_regularizer),
-            "bias_regularizer": regularizers.serialize(self.bias_regularizer),
-            "kernel_constraint": constraints.serialize(self.kernel_constraint),
-            "recurrent_constraint": constraints.serialize(self.recurrent_constraint),
-            "bias_constraint": constraints.serialize(self.bias_constraint),
+            "kernel_initializer": tf.keras.initializers.serialize(self.kernel_initializer),
+            "recurrent_initializer": tf.keras.initializers.serialize(self.recurrent_initializer),
+            "bias_initializer": tf.keras.initializers.serialize(self.bias_initializer),
+            "kernel_regularizer": tf.keras.regularizers.serialize(self.kernel_regularizer),
+            "recurrent_regularizer": tf.keras.regularizers.serialize(self.recurrent_regularizer),
+            "bias_regularizer": tf.keras.regularizers.serialize(self.bias_regularizer),
+            "kernel_constraint": tf.keras.constraints.serialize(self.kernel_constraint),
+            "recurrent_constraint": tf.keras.constraints.serialize(self.recurrent_constraint),
+            "bias_constraint": tf.keras.constraints.serialize(self.bias_constraint),
         }
         base_config = super().get_config()
         return {**base_config, **config}
