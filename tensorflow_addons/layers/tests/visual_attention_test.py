c176a1402e4fd43ffc6ccfde7830cbf4d5252a34
vinayak19th <vinayak1998th@gmail.com>
2021-03-23 21:17:36 +0530

    Added visual attention

--- tensorflow_addons/layers/tests/visual_attention_test.py
+++ tensorflow_addons/layers/tests/visual_attention_test.py
@@ -13,46 +13,53 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for """
-from tensorflow_addons.layers.visual_attention import PixelAttention2D, ChannelAttention2D
+from tensorflow_addons.layers.visual_attention import (
+    PixelAttention2D,
+    ChannelAttention2D,
+)
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input,Conv2D
+from tensorflow.keras.layers import Input, Conv2D
 import tensorflow as tf
-import pytest
-from tensorflow_addons.utils import test_utils
 
 
 def pixel_attention_test():
-    inp = Input(shape=[None,None,1])
-    conv1 = Conv2D(16,3,padding="same")(inp)
+    inp = Input(shape=[None, None, 1])
+    conv1 = Conv2D(16, 3, padding="same")(inp)
     pa = PixelAttention2D(16)(conv1)
-    model = Model(inputs=inp,outputs=pa)
+    model = Model(inputs=inp, outputs=pa)
     return model
 
+
 def channel_attention_test():
-    inp = Input(shape=[None,None,1])
-    conv1 = Conv2D(16,3,padding="same")(inp)
+    inp = Input(shape=[None, None, 1])
+    conv1 = Conv2D(16, 3, padding="same")(inp)
     ca = ChannelAttention2D(16)(conv1)
-    model = Model(inputs=inp,outputs=ca)
+    model = Model(inputs=inp, outputs=ca)
     return model
-    
+
+
 def tests():
-    image = tf.constant([[  0,   0,   0,   0,   0,   0,   4,   0],
-                      [  0,   0,  31, 147, 179,  82,   2,   0],
-                      [  1,   0,  48, 174, 111, 206,  58,   0],
-                      [  1,   0,   0,   0,   0, 179,  85,   0],
-                      [  1,  58, 128, 174, 186, 234,  64,   0],
-                      [  5, 192, 232, 206, 164, 112, 151,   2],
-                      [  3,  30,  61,  30,   0,   0,  11,   1],
-                      [  0,   0,   0,   0,   0,   0,   0,   0]])
-    #Batch and Channel`
-    image = tf.expand_dims(image,-1)
-    image = tf.expand_dims(image,0)
-    print("input shape = ",image.shape)
+    image = tf.constant(
+        [
+            [0, 0, 0, 0, 0, 0, 4, 0],
+            [0, 0, 31, 147, 179, 82, 2, 0],
+            [1, 0, 48, 174, 111, 206, 58, 0],
+            [1, 0, 0, 0, 0, 179, 85, 0],
+            [1, 58, 128, 174, 186, 234, 64, 0],
+            [5, 192, 232, 206, 164, 112, 151, 2],
+            [3, 30, 61, 30, 0, 0, 11, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+    # Batch and Channel`
+    image = tf.expand_dims(image, -1)
+    image = tf.expand_dims(image, 0)
+    print("input shape = ", image.shape)
     model_pa = pixel_attention_test()
     output = model_pa.predict(image)
-    print("output shape pixel attention = ",output.shape)
+    print("output shape pixel attention = ", output.shape)
     del model_pa
     model_ca = channel_attention_test()
     output = model_ca.predict(image)
-    print("output shape channel attention = ",output.shape)
+    print("output shape channel attention = ", output.shape)
     del model_ca
--- tensorflow_addons/layers/visual_attention.py
+++ tensorflow_addons/layers/visual_attention.py
@@ -18,101 +18,105 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, GlobalAveragePooling2D
 
+
 @tf.keras.utils.register_keras_serializable(package="Addons")
 class ChannelAttention2D(tf.keras.layers.Layer):
-  """Implements Channel Attention ( Sanghyun Woo et al) for convolutional networks in tensorflow
-  Inputs need to be Conv2D feature maps.
-  The layer implements the following:
-      1. Average Pooling to create `[1,1,C]` vectors
-      2. Conv2D with k=1 for fully connected features and relu ac
-      3. Sigmoid activation to create attention maps
+    """Implements Channel Attention ( Sanghyun Woo et al) for convolutional networks in tensorflow
+    Inputs need to be Conv2D feature maps.
+    The layer implements the following:
+        1. Average Pooling to create `[1,1,C]` vectors
+        2. Conv2D with k=1 for fully connected features and relu ac
+        3. Sigmoid activation to create attention maps
 
-  Args:
-      * nf [int]: number of filters or channels
-      * Sdims : Spacial dimensions of input
-  Call Arguments:
-      * Feature maps : Conv2D feature maps of the shape `[batch,W,H,C]`.
-  Output;
-      Attention activated Conv2D features of shape `[batch,W,H,C]`.
+    Args:
+        * nf [int]: number of filters or channels
+        * Sdims : Spacial dimensions of input
+    Call Arguments:
+        * Feature maps : Conv2D feature maps of the shape `[batch,W,H,C]`.
+    Output;
+        Attention activated Conv2D features of shape `[batch,W,H,C]`.
 
-  Here is a code example for using `ChannelAttention2D` in a CNN:
-  ```python
-  inp = Input(shape=(1920,1080,3))
-  cnn_layer = Conv2D(32,3,,activation='relu', padding='same')(inp)
+    Here is a code example for using `ChannelAttention2D` in a CNN:
+    ```python
+    inp = Input(shape=(1920,1080,3))
+    cnn_layer = Conv2D(32,3,,activation='relu', padding='same')(inp)
 
-  # Using the .shape[-1] to simplify network modifications. Can directly input number of channels as well
-  attention_cnn = ChannelAttention2D(cnn_layer.shape[-1],cnn_layer.shape[1:-1])(cnn_layer)
+    # Using the .shape[-1] to simplify network modifications. Can directly input number of channels as well
+    attention_cnn = ChannelAttention2D(cnn_layer.shape[-1],cnn_layer.shape[1:-1])(cnn_layer)
 
-  #ADD DNN layers .....
-  ```
-  """
-  def __init__(self,nf,r=4,**kwargs):
-    super().__init__(**kwargs)
-    self.nf = nf
-    self.conv1 = Conv2D(filters=nf/r ,kernel_size=1,use_bias=True)
-    self.conv2 = Conv2D(filters=nf ,kernel_size=1,use_bias=True)
-    self.pool = GlobalAveragePooling2D()
-    
-  @tf.function
-  def call(self,x):
-    y = self.pool(x)
-    y = tf.expand_dims(y,1)
-    y = tf.expand_dims(y,1)
-    y = self.conv1(y)
-    y = tf.nn.relu(y)
-    y = self.conv2(y)
-    y = tf.nn.sigmoid(y)
-    y = tf.multiply(x,y)
-    return y
+    #ADD DNN layers .....
+    ```
+    """
 
-  def get_config(self):
-    config = super().get_config()
-    config.update({"Att_filters":self.nf})
-    return config
+    def __init__(self, nf, r=4, **kwargs):
+        super().__init__(**kwargs)
+        self.nf = nf
+        self.conv1 = Conv2D(filters=nf / r, kernel_size=1, use_bias=True)
+        self.conv2 = Conv2D(filters=nf, kernel_size=1, use_bias=True)
+        self.pool = GlobalAveragePooling2D()
+
+    @tf.function
+    def call(self, x):
+        y = self.pool(x)
+        y = tf.expand_dims(y, 1)
+        y = tf.expand_dims(y, 1)
+        y = self.conv1(y)
+        y = tf.nn.relu(y)
+        y = self.conv2(y)
+        y = tf.nn.sigmoid(y)
+        y = tf.multiply(x, y)
+        return y
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"Att_filters": self.nf})
+        return config
+
 
 @tf.keras.utils.register_keras_serializable(package="Addons")
 class PixelAttention2D(tf.keras.layers.Layer):
-  """Implements Pixel Attention ( Hengyuan Zhao et al) for convolutional networks in tensorflow
-  Inputs need to be Conv2D feature maps.
-  The layer implements the following:
-  1. Conv2D with k=1 for fully connected features
-  2. Sigmoid activation to create attention maps
-  3. tf.multiply to create attention activated outputs
-  4. Conv2D with k=1 for fully connected features
+    """Implements Pixel Attention ( Hengyuan Zhao et al) for convolutional networks in tensorflow
+    Inputs need to be Conv2D feature maps.
+    The layer implements the following:
+    1. Conv2D with k=1 for fully connected features
+    2. Sigmoid activation to create attention maps
+    3. tf.multiply to create attention activated outputs
+    4. Conv2D with k=1 for fully connected features
 
-  Args:
-  * nf [int]: number of filters or channels
-  * name : Name of layer
-  Call Arguments:
-  * Feature maps : Conv2D feature maps of the shape `[batch,W,H,C]`.
-  Output;
-  Attention activated Conv2D features of shape `[batch,W,H,C]`.
+    Args:
+    * nf [int]: number of filters or channels
+    * name : Name of layer
+    Call Arguments:
+    * Feature maps : Conv2D feature maps of the shape `[batch,W,H,C]`.
+    Output;
+    Attention activated Conv2D features of shape `[batch,W,H,C]`.
 
-  Here is a code example for using `PixelAttention2D` in a CNN:
-  ```python
-  inp = Input(shape=(1920,1080,3))
-  cnn_layer = Conv2D(32,3,,activation='relu', padding='same')(inp)
+    Here is a code example for using `PixelAttention2D` in a CNN:
+    ```python
+    inp = Input(shape=(1920,1080,3))
+    cnn_layer = Conv2D(32,3,,activation='relu', padding='same')(inp)
 
-  # Using the .shape[-1] to simplify network modifications. Can directly input number of channels as well
-  attention_cnn = PixelAttention(cnn_layer.shape[-1])(cnn_layer)
+    # Using the .shape[-1] to simplify network modifications. Can directly input number of channels as well
+    attention_cnn = PixelAttention(cnn_layer.shape[-1])(cnn_layer)
 
-  #ADD DNN layers .....
-  ```
-  """
-  def __init__(self, nf,**kwargs):
-    super().__init__(**kwargs)
-    self.nf = nf
-    self.conv1 = Conv2D(filters=nf,kernel_size=1)
+    #ADD DNN layers .....
+    ```
+    """
 
-  @tf.function
-  def call(self,x):
-    y = self.conv1(x)
-    self.sig = tf.keras.activations.sigmoid(y)
-    out = tf.math.multiply(x,y)
-    out = self.conv1(out)
-    return out
+    def __init__(self, nf, **kwargs):
+        super().__init__(**kwargs)
+        self.nf = nf
+        self.conv1 = Conv2D(filters=nf, kernel_size=1)
 
-  def get_config(self):
-    config = super().get_config()
-    config.update({"Att_filters":self.nf})
-    return config
+    @tf.function
+    def call(self, x):
+        y = self.conv1(x)
+        self.sig = tf.keras.activations.sigmoid(y)
+        out = tf.math.multiply(x, y)
+        out = self.conv1(out)
+        return out
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"Att_filters": self.nf})
+        return config
