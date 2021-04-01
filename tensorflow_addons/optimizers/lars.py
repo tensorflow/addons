d12a2f8e6452bb9a70d6552845eb2491af33a2eb
fsx950223 <fsx950223@outlook.com>
2021-04-01 16:08:40 +0800

    add lars

---
+++ tensorflow_addons/optimizers/lars.py
@@ -0,0 +1,153 @@
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""LARS optimizer."""
+
+import tensorflow as tf
+from tensorflow_addons.utils.types import FloatTensorLike
+
+from typeguard import typechecked
+from typing import Union, Callable, List
+
+
+@tf.keras.utils.register_keras_serializable(package="Addons")
+class LARS(tf.keras.optimizers.Optimizer):
+    """Layer-wise Adaptive Rate Scaling for large batch training.
+    Introduced by "Large Batch Training of Convolutional Networks" by Y. You,
+    I. Gitman, and B. Ginsburg. (https://arxiv.org/abs/1708.03888)
+    Implements the LARS learning rate scheme presented in the paper above. This
+    optimizer is useful when scaling the batch size to up to 32K without
+    significant performance degradation. It is recommended to use the optimizer
+    in conjunction with:
+        - Gradual learning rate warm-up
+        - Linear learning rate scaling
+        - Poly rule learning rate decay
+    Note, LARS scaling is currently only enabled for dense tensors.
+    Args:
+        lr: A `Tensor` or floating point value. The base learning rate.
+        momentum: A floating point value. Momentum hyperparameter.
+        weight_decay: A floating point value. Weight decay hyperparameter.
+        eeta: LARS coefficient as used in the paper. Dfault set to LARS
+            coefficient from the paper. (eeta / weight_decay) determines the
+            highest scaling factor in LARS.
+        epsilon: Optional epsilon parameter to be set in models that have very
+            small gradients. Default set to 0.0.
+        nesterov: when set to True, nesterov momentum will be enabled
+    """
+
+    @typechecked
+    def __init__(self,
+                 learning_rate: Union[FloatTensorLike, Callable] = 0.001,
+                 momentum: FloatTensorLike = 0.9,
+                 weight_decay: FloatTensorLike = 0.0001,
+                 eeta: FloatTensorLike = 0.001,
+                 epsilon: FloatTensorLike = 0.0,
+                 nesterov: bool = False,
+                 skip_list: List[str] = None,
+                 name: str = "LARS",
+                 **kwargs):
+
+        if momentum < 0.0:
+            raise ValueError("momentum should be positive: %s" % momentum)
+        if weight_decay < 0.0:
+            raise ValueError("weight_decay is not positive: %s" % weight_decay)
+        super().__init__(name, **kwargs)
+        self._set_hyper("learning_rate", kwargs.get("lr", learning_rate))
+        self._set_hyper("momentum", momentum)
+        self._set_hyper("weight_decay", weight_decay)
+        self._set_hyper("eeta", eeta)
+        self._epsilon = epsilon or tf.keras.backend.epsilon()
+        self._nesterov = nesterov
+        self._skip_list = skip_list
+
+    def _create_slots(self, var_list):
+        for v in var_list:
+            self.add_slot(v, "momentum")
+
+    def _compute_lr(self, grad, var, coefficients):
+        scaled_lr = coefficients["lr_t"]
+        if self._skip_list is None or not any(v in var.name
+                                              for v in self._skip_list):
+            w_norm = tf.linalg.norm(var, ord=2)
+            g_norm = tf.linalg.norm(grad, ord=2)
+            trust_ratio = tf.where(
+                w_norm > 0,
+                tf.where(
+                    g_norm > 0,
+                    (coefficients["eeta_t"] * w_norm /
+                    (g_norm + coefficients["weight_decay_t"] * w_norm + self._epsilon)), 1.0),
+                1.0)
+            scaled_lr = coefficients["lr_t"] * trust_ratio
+            # Add the weight regularization gradient
+            grad = grad + coefficients["weight_decay_t"] * var
+        return scaled_lr, grad
+
+    def _prepare_local(self, var_device, var_dtype, apply_state):
+        super()._prepare_local(var_device, var_dtype, apply_state)
+        apply_state[(var_device, var_dtype)].update(
+            {
+                "momentum_t": tf.identity(
+                    self._get_hyper("momentum", var_dtype)
+                ),
+                "weight_decay_t": tf.identity(
+                    self._get_hyper("weight_decay", var_dtype)
+                ),
+                "eeta_t": tf.identity(
+                    self._get_hyper("eeta", var_dtype)
+                ),
+            }
+        )
+
+    def _resource_apply_dense(self, grad, var, apply_state):
+        var_device, var_dtype = var.device, var.dtype
+        coefficients = (apply_state or {}).get(
+            (var_device, var_dtype)
+        ) or self._fallback_apply_state(var_device, var_dtype)
+        scaled_lr, grad = self._compute_lr(grad, var, coefficients)
+        mom = self.get_slot(var, "momentum")
+        return tf.raw_ops.ResourceApplyMomentum(
+                var=var.handle,
+                accum=mom.handle,
+                lr=tf.cast(1.0, var.dtype),
+                grad=grad * scaled_lr,
+                momentum=coefficients["momentum_t"],
+                use_locking=self._use_locking,
+                use_nesterov=self._nesterov)
+
+    def _resource_apply_sparse(self, grad, var, indices, apply_state):
+        var_device, var_dtype = var.device, var.dtype
+        coefficients = (apply_state or {}).get(
+            (var_device, var_dtype)
+        ) or self._fallback_apply_state(var_device, var_dtype)
+        mom = self.get_slot(var, "momentum")
+        return tf.raw_ops.ResourceSparseApplyMomentum(
+                var=var.handle,
+                accum=mom.handle,
+                lr=tf.cast(coefficients["lr_t"], grad.dtype),
+                grad=grad,
+                indices=indices,
+                momentum=tf.cast(coefficients["momentum_t"], grad.dtype),
+                use_locking=self._use_locking,
+                use_nesterov=self._nesterov)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"learning_rate": self._serialize_hyperparameter("learning_rate"),
+                       "momentum": self._serialize_hyperparameter("momentum"),
+                       "weight_decay": self._serialize_hyperparameter("weight_decay"),
+                       "epsilon": self._epsilon,
+                       "eeta": self._serialize_hyperparameter("eeta"),
+                       "nesterov": self._nesterov,
+                       "skip_list": self._skip_list})
+        return config
---
+++ tensorflow_addons/optimizers/tests/lars_test.py
@@ -0,0 +1,110 @@
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for LARS Optimizer."""
+
+
+import numpy as np
+import pytest
+
+import tensorflow as tf
+
+from tensorflow_addons.optimizers import lars
+from tensorflow_addons.utils import test_utils
+
+
+@pytest.mark.usefixtures("maybe_run_functions_eagerly")
+def testLARSGradientOneStep():
+    for dtype in [tf.float32, tf.float64]:
+        shape = [3, 3]
+        var_np = np.ones(shape)
+        grad_np = np.ones(shape)
+        lr_np = 0.1
+        m_np = 0.9
+        wd_np = 0.1
+        ep_np = 1e-5
+        eeta = 0.1
+        vel_np = np.zeros(shape)
+
+        var = tf.Variable(var_np, dtype=dtype)
+        grad = tf.Variable(grad_np, dtype=dtype)
+        opt = lars.LARS(
+            learning_rate=lr_np,
+            momentum=m_np,
+            weight_decay=wd_np,
+            eeta=eeta,
+            epsilon=ep_np)
+
+        test_utils.assert_allclose_according_to_type(var_np, var)
+
+        opt.apply_gradients([(grad, var)])
+
+        w_norm = np.linalg.norm(var_np.flatten(), ord=2)
+        g_norm = np.linalg.norm(grad_np.flatten(), ord=2)
+        trust_ratio = eeta * w_norm / (g_norm + wd_np * w_norm + ep_np)
+        scaled_lr = lr_np * trust_ratio
+        grad_np = grad_np + wd_np * var_np
+
+        vel_np = m_np * vel_np + scaled_lr * grad_np
+        var_np -= vel_np
+
+        test_utils.assert_allclose_according_to_type(var_np, var)
+        test_utils.assert_allclose_according_to_type(vel_np, opt.get_slot(var, 'momentum'))
+
+
+@pytest.mark.usefixtures("maybe_run_functions_eagerly")
+def testLARSGradientMultiStep():
+    for dtype in [tf.float32, tf.float64]:
+        shape = [3, 3]
+        var_np = np.ones(shape)
+        grad_np = np.ones(shape)
+        lr_np = 0.1
+        m_np = 0.9
+        wd_np = 0.1
+        ep_np = 1e-5
+        eeta = 0.1
+        vel_np = np.zeros(shape)
+
+        var = tf.Variable(var_np, dtype=dtype)
+        grad = tf.Variable(grad_np, dtype=dtype)
+        opt = lars.LARS(
+            learning_rate=lr_np,
+            momentum=m_np,
+            eeta=eeta,
+            weight_decay=wd_np,
+            epsilon=ep_np)
+
+        test_utils.assert_allclose_according_to_type(var_np, var)
+
+        for _ in range(10):
+            opt.apply_gradients([(grad, var)])
+
+            w_norm = np.linalg.norm(var_np.flatten(), ord=2)
+            g_norm = np.linalg.norm(grad_np.flatten(), ord=2)
+            trust_ratio = eeta * w_norm / (g_norm + wd_np * w_norm + ep_np)
+            scaled_lr = lr_np * trust_ratio
+            grad_np = grad_np + wd_np * var_np
+
+            vel_np = m_np * vel_np + scaled_lr * grad_np
+            var_np -= vel_np
+
+            test_utils.assert_allclose_according_to_type(var_np, var)
+            test_utils.assert_allclose_according_to_type(vel_np, opt.get_slot(var, 'momentum'))
+
+
+def test_serialization():
+    optimizer = lars.LARS(1e-4)
+    config = tf.keras.optimizers.serialize(optimizer)
+    new_optimizer = tf.keras.optimizers.deserialize(config)
+    assert new_optimizer.get_config() == optimizer.get_config()
