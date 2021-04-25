7717da516c51aae49afdd67115a8ae372002888f
Rehan Guha <rehanguha29@gmail.com>
2021-04-25 15:50:48 +0530

    Changed docsting style

--- tensorflow_addons/optimizers/tests/cocob_test.py
+++ tensorflow_addons/optimizers/tests/cocob_test.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""
-Tests for COntinuos COin Betting (COCOB) Backprop optimizer
-"""
+"""Tests for COntinuos COin Betting (COCOB) Backprop optimizer"""
 
 import numpy as np
 import tensorflow as tf
