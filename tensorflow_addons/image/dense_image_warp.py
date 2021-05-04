4c3853e4c23f64813771c780b07d3edd32ed984c
Ali Salehi <AliSaaalehi@gmail.com>
2021-05-04 17:38:21 -0500

    Update dense_image_warp.py
    By simply negating the flow vectors we will not get the same result as warping based on optical flow.
    Your test cases might be too simple to capture this issue.

--- tensorflow_addons/image/dense_image_warp.py
+++ tensorflow_addons/image/dense_image_warp.py
@@ -178,9 +178,7 @@ def dense_image_warp(
 
     NOTE: The definition of the flow field above is different from that
     of optical flow. This function expects the negative forward flow from
-    output image to source image. Given two images `I_1` and `I_2` and the
-    optical flow `F_12` from `I_1` to `I_2`, the image `I_1` can be
-    reconstructed by `I_1_rec = dense_image_warp(I_2, -F_12)`.
+    output image to source image.
 
     Args:
       image: 4-D float `Tensor` with shape `[batch, height, width, channels]`.
