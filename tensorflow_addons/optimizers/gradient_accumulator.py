b31c896152c08e52932ee3e1d5b71ae32186bc24
Stefan Falk <stefan.falk@itranslate.com>
2021-07-20 09:34:44 +0200

    Fixing code mess up

--- tensorflow_addons/optimizers/gradient_accumulator.py
+++ tensorflow_addons/optimizers/gradient_accumulator.py
@@ -12,25 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from typing import List, Union, Dict, Hashable
+from typing import Union, Dict, Hashable, List
 
 import tensorflow as tf
+
 from tensorflow_addons.utils import types
-from typeguard import typechecked
 
 
 class AccumulationGradientTransformer:
-
     _accu_gradients: Union[Dict[Hashable, tf.Variable], None] = None
 
-    def __init__(self, optimizer: types.Optimizer, accu_steps: types.TensorLike, trainable_variables):
+    def __init__(
+        self,
+        optimizer: types.Optimizer,
+        accu_steps: types.TensorLike,
+        trainable_variables,
+    ):
         self.optimizer = optimizer
         self.accu_steps = accu_steps
-        self.step = tf.Variable(0, dtype=tf.int64, name='ga_step')
+        self.step = tf.Variable(0, dtype=tf.int64, name="ga_step")
         self._accu_gradients: Union[List[tf.Variable], None] = None
         policy = tf.keras.mixed_precision.global_policy()
         self.variable_dtype = policy.variable_dtype
-        self._accu_gradients = {var.ref(): self.optimizer.add_slot(var, "ga") for var in trainable_variables}
+        self._accu_gradients = {
+            var.ref(): self.optimizer.add_slot(var, "ga") for var in trainable_variables
+        }
 
     def __call__(self, grads_and_vars, *args, **kwargs):
 
@@ -39,26 +45,36 @@ class AccumulationGradientTransformer:
         step_inc_op = self.step.assign_add(1, read_value=False)
 
         with tf.control_dependencies([step_inc_op]):
-            can_apply = tf.cast(self.step % self.accu_steps == 0, dtype=self.variable_dtype)
-            accumulate = tf.cast(self.step % tf.constant(self.accu_steps + 1, dtype=self.step.dtype) != 0, dtype=self.variable_dtype)
+            can_apply = tf.cast(
+                self.step % self.accu_steps == 0, dtype=self.variable_dtype
+            )
+            accumulate = tf.cast(
+                self.step % (self.accu_steps + 1) != 0, dtype=self.variable_dtype
+            )
 
         accum_ops = list()
         for grad, var in grads_and_vars:
+
+            # Get the accumulated gradient
             grad_accum = accu_gradients[var.ref()] * accumulate
+
             if isinstance(grad, tf.IndexedSlices):
                 added = tf.IndexedSlices(
-                    values=grad.values + tf.gather_nd(grad_accum, grad.indices[..., None]),
+                    values=grad.values + tf.gather_nd(grad_accum, grad.indices),
                     indices=grad.indices,
-                    dense_shape=grad.dense_shape
+                    dense_shape=grad.dense_shape,
                 )
                 accu_op = accu_gradients[var.ref()].scatter_update(added)
             else:
-                accu_op = accu_gradients[var.ref()].assign(grad + grad_accum, read_value=False)
+                accu_op = accu_gradients[var.ref()].assign(
+                    grad + grad_accum, read_value=False
+                )
 
             accum_ops.append(accu_op)
 
         iter_dec_op = self.optimizer.iterations.assign_add(
-            -1 * tf.cast(can_apply, dtype=self.optimizer.iterations.dtype), read_value=False
+            -1 * tf.cast(can_apply, dtype=self.optimizer.iterations.dtype),
+            read_value=False,
         )
 
         with tf.control_dependencies(accum_ops + [iter_dec_op]):
@@ -67,11 +83,8 @@ class AccumulationGradientTransformer:
 
 
 def GradientAccumulator(
-    optimizer: types.Optimizer,
-    accum_steps: int = 2,
-    trainable_variables=None
+    optimizer: types.Optimizer, accu_steps: int = 2, trainable_variables=None
 ) -> types.Optimizer:
-
     if trainable_variables is None:
         trainable_variables = list()
 
@@ -81,8 +94,8 @@ def GradientAccumulator(
     optimizer.gradient_transformers.append(
         AccumulationGradientTransformer(
             optimizer=optimizer,
-            accu_steps=accum_steps,
-            trainable_variables=trainable_variables
+            accu_steps=accu_steps,
+            trainable_variables=trainable_variables,
         )
     )
 
