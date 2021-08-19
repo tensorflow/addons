cf6b0d2f54e640f2144399def176c45f65570d12
Lukas Geiger <lukas.geiger94@gmail.com>
2021-08-19 12:31:55 +0100

    Remove compatibility code for TF < 2.4

--- tensorflow_addons/callbacks/average_model_checkpoint.py
+++ tensorflow_addons/callbacks/average_model_checkpoint.py
@@ -63,9 +63,8 @@ class AverageModelCheckpoint(tf.keras.callbacks.ModelCheckpoint):
 
     def _get_optimizer(self):
         optimizer = self.model.optimizer
-        # TODO(fsx950223): change _optimizer to inner_optimizer after tf2.3 is not support
         if type(optimizer).__name__ in ["LossScaleOptimizer", "LossScaleOptimizerV1"]:
-            optimizer = optimizer._optimizer
+            optimizer = optimizer.inner_optimizer
 
         return optimizer
 
