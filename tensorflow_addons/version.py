3d226019c03384c5ed9061285c37667e2ab22a89
Sean Morgan <seanmorgan@outlook.com>
2021-08-19 08:45:26 -0400

    Update version.py

--- tensorflow_addons/version.py
+++ tensorflow_addons/version.py
@@ -28,7 +28,7 @@ _PATCH_VERSION = "0"
 # stable release (indicated by `_VERSION_SUFFIX = ''`). Outside the context of a
 # release branch, the current version is by default assumed to be a
 # 'development' version, labeled 'dev'.
-_VERSION_SUFFIX = "dev"
+_VERSION_SUFFIX = ""
 
 # Example, '0.1.0-dev'
 __version__ = ".".join([_MAJOR_VERSION, _MINOR_VERSION, _PATCH_VERSION])
