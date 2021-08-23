df4432eb0f8087374586ccdb47f0a9a5b3a423f5
juntang-zhuang <j.zhuang@yale.edu>
2021-08-23 15:10:27 +0000

    remove_future_import

--- tensorflow_addons/optimizers/tests/adabelief_test.py
+++ tensorflow_addons/optimizers/tests/adabelief_test.py
@@ -14,10 +14,6 @@
 # ==============================================================================
 """Tests for AdaBelief optimizer."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 import pytest
 
