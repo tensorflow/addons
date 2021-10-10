a8d688ad57b46a89d525e57765cb458d19f3e871
Sean Morgan <seanmorgan@outlook.com>
2021-10-10 15:40:24 -0700

    * test build on tf2.7rc0

--- tools/run_gpu_tests.sh
+++ tools/run_gpu_tests.sh
@@ -6,7 +6,7 @@ export DOCKER_BUILDKIT=1
 docker build \
        -f tools/docker/build_wheel.Dockerfile \
        --target tfa_gpu_tests \
-       --build-arg TF_VERSION=2.6.0 \
+       --build-arg TF_VERSION=2.7.0rc0 \
        --build-arg PY_VERSION=3.6 \
        -t tfa_gpu_tests ./
 docker run --rm -t --gpus=all tfa_gpu_tests
