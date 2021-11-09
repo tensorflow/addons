36317936567cb32091af054cbf67f460921f46cb
Sean Morgan <seanmorgan@outlook.com>
2021-11-09 15:58:04 -0800

    Update build_dev_container.sh

--- tools/build_dev_container.sh
+++ tools/build_dev_container.sh
@@ -6,5 +6,6 @@ docker build \
     -f tools/docker/dev_container.Dockerfile \
     --build-arg TF_VERSION=2.7.0 \
     --build-arg TF_PACKAGE=tensorflow-cpu \
+    --no-cache \
     --target dev_container_cpu \
     -t tfaddons/dev_container:latest-cpu ./
