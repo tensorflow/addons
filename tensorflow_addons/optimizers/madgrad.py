6acca21b47b420292ebaef45a1088f1b610d3f21
DarshanDeshpande <darshan1504@gmail.com>
2021-11-10 10:42:34 +0530

    Update madgrad

--- tensorflow_addons/optimizers/madgrad.py
+++ tensorflow_addons/optimizers/madgrad.py
@@ -15,17 +15,12 @@
 """Madgrad optimizer implementation."""
 # pylint: disable=g-classes-have-attributes
 
-from tensorflow.python.framework import ops
-from tensorflow.python.keras import backend_config
-from tensorflow.python.keras.optimizer_v2 import optimizer_v2
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import math_ops
-from tensorflow.python.ops import state_ops
 import tensorflow as tf
+from tensorflow.python.keras import backend_config
 
 
 @tf.keras.utils.register_keras_serializable(package="Addons")
-class MadGrad(optimizer_v2.OptimizerV2):
+class MadGrad(tf.keras.optimizers.Optimizer):
     r"""Optimizer that implements the MADGRAD algorithm.
 
     Args:
@@ -83,11 +78,11 @@ class MadGrad(optimizer_v2.OptimizerV2):
             self.add_slot(var, "x_0")
 
     def _prepare_local(self, var_device, var_dtype, apply_state):
-        momentum = array_ops.identity(self._get_hyper("momentum", var_dtype))
-        weight_decay = array_ops.identity(self._get_hyper("weight_decay", var_dtype))
+        momentum = tf.identity(self._get_hyper("momentum", var_dtype))
+        weight_decay = tf.identity(self._get_hyper("weight_decay", var_dtype))
 
         apply_state[(var_device, var_dtype)] = dict(
-            epsilon=ops.convert_to_tensor_v2_with_dispatch(self.epsilon, var_dtype),
+            epsilon=tf.convert_to_tensor(self.epsilon, var_dtype),
             momentum=momentum,
             weight_decay=weight_decay,
         )
@@ -102,29 +97,25 @@ class MadGrad(optimizer_v2.OptimizerV2):
         sk = self.get_slot(var, "sk")
         x_0 = self.get_slot(var, "x_0")
 
-        decay = math_ops.cast(self._get_hyper("weight_decay"), var_dtype)
+        decay = tf.cast(self._get_hyper("weight_decay"), var_dtype)
         lr_t = self._decayed_lr(var_dtype)
 
         grad = tf.cond(
-            math_ops.greater(decay, 0),
-            lambda: grad + decay * var,
-            lambda: grad,
+            tf.math.greater(decay, 0), lambda: grad + decay * var, lambda: grad,
         )
 
-        local_step = math_ops.cast(self.iterations + 1, var_dtype)
-        lamb = lr_t * math_ops.pow(local_step, 0.5)
+        local_step = tf.cast(self.iterations + 1, var_dtype)
+        lamb = lr_t * tf.math.pow(local_step, 0.5)
         if self._first_step:
-            x_0 = state_ops.assign(x_0, var, use_locking=self._use_locking)
+            x_0 = x_0.assign(var, use_locking=self._use_locking)
             self._first_step = False
 
-        sk_plus_1 = state_ops.assign_add(sk, lamb * grad, use_locking=self._use_locking)
-        vk_plus_1 = state_ops.assign_add(
-            vk, lamb * (grad * grad), use_locking=self._use_locking
-        )
+        sk_plus_1 = sk.assign_add(lamb * grad, use_locking=self._use_locking)
+        vk_plus_1 = vk.assign_add(lamb * (grad * grad), use_locking=self._use_locking)
 
         z_k_plus_1 = (
             x_0
-            - (1 / (math_ops.pow(vk_plus_1, (1.0 / 3.0)) + coefficients["epsilon"]))
+            - (1 / (tf.math.pow(vk_plus_1, (1.0 / 3.0)) + coefficients["epsilon"]))
             * sk_plus_1
         )
 
@@ -132,7 +123,7 @@ class MadGrad(optimizer_v2.OptimizerV2):
             coefficients["momentum"] * z_k_plus_1
         )
 
-        return state_ops.assign(var, var_t, use_locking=self._use_locking).op
+        return var.assign(var_t, use_locking=self._use_locking).op
 
     def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
         var_device, var_dtype = var.device, var.dtype.base_dtype
@@ -144,20 +135,20 @@ class MadGrad(optimizer_v2.OptimizerV2):
         sk = self.get_slot(var, "sk")
         x_0 = self.get_slot(var, "x_0")
 
-        decay = math_ops.cast(self._get_hyper("weight_decay"), var_dtype)
+        decay = tf.cast(self._get_hyper("weight_decay"), var_dtype)
         lr_t = self._decayed_lr(var_dtype)
 
         grad = tf.cond(
-            math_ops.greater(decay, 0),
-            lambda: grad + decay * array_ops.gather(var, indices),
+            tf.math.greater(decay, 0),
+            lambda: grad + decay * tf.gather(var, indices),
             lambda: grad,
         )
-        local_step = math_ops.cast(self.iterations + 1, var_dtype)
+        local_step = tf.cast(self.iterations + 1, var_dtype)
 
-        lamb = lr_t * math_ops.pow(local_step, 0.5)
+        lamb = lr_t * tf.math.pow(local_step, 0.5)
 
         if self._first_step:
-            x_0 = state_ops.assign(x_0, var, use_locking=self._use_locking)
+            x_0 = x_0.assign(var, use_locking=self._use_locking)
             self._first_step = False
 
         sk_plus_1 = self._resource_scatter_add(sk, indices, lamb * grad)
@@ -165,14 +156,14 @@ class MadGrad(optimizer_v2.OptimizerV2):
 
         z_k_plus_1 = (
             x_0
-            - (1 / (math_ops.pow(vk_plus_1, (1.0 / 3.0)) + coefficients["epsilon"]))
+            - (1 / (tf.math.pow(vk_plus_1, (1.0 / 3.0)) + coefficients["epsilon"]))
             * sk_plus_1
         )
 
         var_t = (1 - coefficients["momentum"]) * var + (
             coefficients["momentum"] * z_k_plus_1
         )
-        return state_ops.assign(var, var_t, use_locking=self._use_locking).op
+        return var.assign(var_t, use_locking=self._use_locking).op
 
     def get_config(self):
         config = super(MadGrad, self).get_config()
