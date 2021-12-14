26cf240c310153abb7246b0bd158551b41f3e6e4
Sean Morgan <seanmorgan@outlook.com>
2021-12-13 20:44:20 -0800

    * Use new manylinux policy

--- tools/releases/tf_auditwheel_patch.sh
+++ tools/releases/tf_auditwheel_patch.sh
@@ -16,7 +16,7 @@
 
 set -e -x
 
-SITE_PKG_LOCATION="/usr/local/lib/python3.10"
+SITE_PKG_LOCATION=$(python -c "import site; print(site.getsitepackages()[0])")
 TF_SHARED_LIBRARY_NAME=$(grep -r TF_SHARED_LIBRARY_NAME .bazelrc | awk -F= '{print$2}')
-POLICY_JSON="${SITE_PKG_LOCATION}/auditwheel/policy/policy.json"
+POLICY_JSON="${SITE_PKG_LOCATION}/auditwheel/policy/manylinux-policy.json"
 sed -i "s/libresolv.so.2\"/libresolv.so.2\", $TF_SHARED_LIBRARY_NAME/g" $POLICY_JSON
