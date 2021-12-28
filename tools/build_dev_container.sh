d19c3efa5e4b9924e5f1e0b603fe0a22b8efcbfe
Sean Morgan <seanmorgan@outlook.com>
2021-12-27 19:51:40 -0800

    Utilize SIG Build Docker Images (#2515)
    * Utilize SIG Build build image
    Co-authored-by: bhack <bhack@users.noreply.github.com>

--- tools/build_dev_container.sh
+++ tools/build_dev_container.sh
@@ -5,7 +5,8 @@ set -x -e
 docker build \
     -f tools/docker/dev_container.Dockerfile \
     --build-arg TF_VERSION=2.7.0 \
-    --build-arg TF_PACKAGE=tensorflow-cpu \
+    --build-arg TF_PACKAGE=tensorflow \
+    --build-arg PY_VERSION=$PY_VERSION \
     --no-cache \
-    --target dev_container_cpu \
-    -t tfaddons/dev_container:latest-cpu ./
+    --target dev_container \
+    -t tfaddons/dev_container:latest-gpu ./
