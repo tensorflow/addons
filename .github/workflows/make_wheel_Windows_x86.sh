07903908b77d0e5d96842175ae8ee80a661d4eb8
Sean Morgan <seanmorgan@outlook.com>
2022-04-24 22:00:20 -0700

    Better diff

--- .github/workflows/make_wheel_Windows_x86.sh
+++ .github/workflows/make_wheel_Windows_x86.sh
@@ -1,13 +1,12 @@
 set -e -x
 
 export TF_NEED_CUDA=0
+export PYTHON_BIN_PATH=$(which python)
 export BAZEL_VC="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/"
 
 # Install Deps
 python --version
 python -m pip install --default-timeout=1000 wheel setuptools tensorflow==$TF_VERSION
-
-export PYTHON_BIN_PATH=$(which python)
 
 # Test
 bash ./tools/testing/build_and_run_tests.sh
