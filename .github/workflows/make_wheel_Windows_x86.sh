d5683c536707e9bba6ec0b6954ed4c63fee37d3c
Sean Morgan <seanmorgan@outlook.com>
2022-04-24 20:52:48 -0700

    Rename variable

--- .github/workflows/make_wheel_Windows_x86.sh
+++ .github/workflows/make_wheel_Windows_x86.sh
@@ -7,7 +7,7 @@ export BAZEL_VC="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/
 python --version
 python -m pip install --default-timeout=1000 wheel setuptools tensorflow==$TF_VERSION
 
-export _PYTHON_BIN_PATH=$(which python)
+export PYTHON_BIN_PATH=$(which python)
 
 # Test
 bash ./tools/testing/build_and_run_tests.sh
