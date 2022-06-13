6b8028591af781b6d66b7067b4fbe25a9f19aea6
Sean Morgan <seanmorgan@outlook.com>
2022-06-13 14:32:31 -0700

    bump 0.17.1 version (#2727)

--- tensorflow_addons/version.py
+++ tensorflow_addons/version.py
@@ -21,7 +21,7 @@ EXCLUSIVE_MAX_TF_VERSION = "2.10.0"
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = "0"
 _MINOR_VERSION = "17"
-_PATCH_VERSION = "0"
+_PATCH_VERSION = "1"
 
 # When building releases, we can update this value on the release branch to
 # reflect the current release candidate ('rc0', 'rc1') or, finally, the official
