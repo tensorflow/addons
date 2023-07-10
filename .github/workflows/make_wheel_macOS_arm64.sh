b828f7958e899c8fd88d3c413f0087d0b42987f7
Sean Morgan <seanmrgn@amazon.com>
2023-07-10 11:19:01 -0700

    Debug

--- .github/workflows/make_wheel_macOS_arm64.sh
+++ .github/workflows/make_wheel_macOS_arm64.sh
@@ -7,7 +7,7 @@ python -m pip install --default-timeout=1000 delocate==0.10.3 wheel setuptools t
 
 python configure.py
 # Setting DYLD_LIBRARY_PATH to help delocate finding tensorflow after the rpath invalidation
-# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$(python -c 'import configure; print(configure.get_tf_shared_lib_dir())')
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$(python -c 'import configure; print(configure.get_tf_shared_lib_dir())')
 
 # For dynamic linking, we want the ARM version of TensorFlow.
 # Since we cannot run it on x86 so we need to force pip to install it regardless
@@ -29,5 +29,5 @@ bazel build \
   build_pip_pkg
 
 bazel-bin/build_pip_pkg artifacts "--plat-name macosx_11_0_arm64 $NIGHTLY_FLAG"
-delocate-wheel -w wheelhouse -v artifacts/*.whl
+delocate-wheel -w wheelhouse -v --ignore-missing-dependencies artifacts/*.whl
 
